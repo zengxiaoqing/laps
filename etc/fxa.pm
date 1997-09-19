6a9310d334c748bcb0637a304f80c95af3b07c6e
jedwards <>
1997-09-19 15:02:42 +0000

    moved fxa log filename to fxa.pm now creates parent directory if not present

--- etc/fxa.pm
+++ etc/fxa.pm
@@ -18,3 +18,26 @@ sub Get_env'fxa{
 }
 
 1;
+
+sub Set_logdir'fxa{
+    
+    if( -d $ENV{LOG_DIR}){
+	local($yymmdd) = `date -u +%y%m%d`;
+	chomp($yymmdd);
+	$LAPS_LOG_PATH= "$ENV{LOG_DIR}/$yymmdd";
+
+	if(! -d $LAPS_LOG_PATH){
+	    mkdir $LAPS_LOG_PATH, 0777 || 
+                die "Could not create Log directory $LAPS_LOG_PATH";
+	}
+	$LAPS_LOG_PATH .= "/laps";
+	if(! -d $LAPS_LOG_PATH){
+	    mkdir $LAPS_LOG_PATH, 0777 || 
+                die "Could not create Log directory $LAPS_LOG_PATH";
+	}
+    }else{
+	die "Could not find log dir $ENV{LOG_DIR}";
+    }
+	
+}
+1;
