aebdb21b24cc87f0aabe015c48b56f34b8ccd6a9
jedwards <>
1997-09-19 15:10:59 +0000

    Corrected path error for log files

--- etc/fxa.pm
+++ etc/fxa.pm
@@ -1,6 +1,6 @@
 package fxa;
 
-sub Get_env'fxa{
+sub Get_env'fxa{ #'
     local($fxa_env_file) = "/awips/fxa/.environs";
     open(FXA,"$fxa_env_file") || return 0;
 
@@ -24,7 +24,7 @@ sub Set_logdir'fxa{
     if( -d $ENV{LOG_DIR}){
 	local($yymmdd) = `date -u +%y%m%d`;
 	chomp($yymmdd);
-	$LAPS_LOG_PATH= "$ENV{LOG_DIR}/$yymmdd";
+	local($LAPS_LOG_PATH)= "$ENV{LOG_DIR}/$yymmdd";
 
 	if(! -d $LAPS_LOG_PATH){
 	    mkdir $LAPS_LOG_PATH, 0777 || 
@@ -39,5 +39,6 @@ sub Set_logdir'fxa{
 	die "Could not find log dir $ENV{LOG_DIR}";
     }
 	
-}
+    return $LAPS_LOG_PATH;
+} 
 1;
