98965b3161b31255100b9ae800307477b04e49c5
Steve Albers <Steve.Albers@noaa.gov>
1998-01-12 23:39:20 +0000

    time: 2330 UTC changes: New ingest_vad source code directory added.

---
+++ src/ingest/radar/vad/ingest_vad.f
@@ -0,0 +1,119 @@
+
+!     Steve Albers      Dec-1997        
+
+!     Input file 
+      character*70 filename_in
+      character*9 a9_time
+      character*150 dir_in
+      character*255 c_filespec
+      integer max_files
+      parameter(max_files = 1000)
+      character*255 c_fnames(max_files)
+      integer i4times(max_files)
+
+!     Output file
+      character*31    ext
+      integer*4       len_dir
+
+      character*40 c_vars_req
+      character*100 c_values_req
+
+      call get_systime(i4time_sys,a9_time,istatus)
+      if(istatus .ne. 1)go to 999
+
+      call get_grid_dim_xy(NX_L,NY_L,istatus)
+      if (istatus .ne. 1) then
+          write (6,*) 'Error getting horizontal domain dimensions'
+          go to 999
+      endif
+ 
+      call get_laps_cycle_time(ilaps_cycle_time,istatus)
+      if(istatus .eq. 1)then
+          write(6,*)' ilaps_cycle_time = ',ilaps_cycle_time
+      else
+          write(6,*)' Error getting laps_cycle_time'
+          stop
+      endif
+
+      lag_time_report = 4 * 3600
+
+!     Open output PIN file
+      ext = 'pro' ! 'vad'
+      call open_lapsprd_file_append(2,i4time_sys,ext(1:3),istatus)
+
+!     Get List of input /public NetCDF files
+      call get_vad_parms(dir_in,istatus)
+      if(istatus .eq. 1)then
+          write(6,*)'dir_in = ',dir_in
+      else
+          write(6,*)' Error getting dir_in'
+          stop   
+      endif
+
+      call s_len(dir_in,len_dir_in)
+      c_filespec = dir_in(1:len_dir_in)//'/'//'*0030r'
+      call get_file_times(c_filespec,max_files,c_fnames
+     1                      ,i4times,i_nbr_files_ret,istatus)
+
+!     Loop through /public NetCDF files and choose ones in time window
+      write(6,*)' # of files on /public = ',i_nbr_files_ret
+      do i = 1,i_nbr_files_ret
+          call make_fnam_lp(i4times(i),a9_time,istatus)
+          filename_in = dir_in(1:len_dir_in)//'/'//a9_time//'0030r'
+
+!         Test whether we want the NetCDF file for this time
+          i4time_file_earliest = i4time_sys - (ilaps_cycle_time / 2)
+          i4time_file_latest =   i4time_sys + (ilaps_cycle_time / 2) 
+     1                                      + lag_time_report
+          
+          if(i4times(i) .lt. i4time_file_earliest)then
+              write(6,*)' File is too early ',a9_time,i
+          elseif(i4times(i) .gt. i4time_file_latest)then
+              write(6,*)' File is too late ',a9_time,i
+          else
+              write(6,*)' File is in time window ',a9_time,i
+              write(6,*)' Input file ',filename_in
+
+!             Read from the NetCDF pirep file and write to the opened PIN file
+              call get_vad_data(i4time_sys,ilaps_cycle_time
+     1                                      ,NX_L,NY_L
+     1                                      ,filename_in,istatus)
+          endif
+      enddo
+
+ 990  close(2) ! Output VAD file
+
+ 999  continue
+
+      write(6,*)' End of vad ingest program'
+
+      end
+ 
+ 
+       subroutine get_vad_parms(path_to_vad,istatus)
+
+       character*150 path_to_vad,filename
+       namelist /vad_nl/ path_to_vad
+ 
+       character*150 static_dir,filename
+ 
+       call get_directory('nest7grid',static_dir,len_dir)
+
+       filename = static_dir(1:len_dir)//'/vad.nl'
+ 
+       open(1,file=filename,status='old',err=900)
+       read(1,vad_nl,err=901)
+       close(1)
+
+       istatus = 1
+       return
+
+  900  print*,'error opening file ',filename
+       istatus = 0
+       return
+
+  901  print*,'error reading vad_nl in ',filename
+       istatus = 0
+       return
+
+       end
---
+++ src/ingest/radar/vad/ingest_vad_sub.f
@@ -0,0 +1,373 @@
+
+      subroutine get_vad_data(i4time_sys,ilaps_cycle_time
+     1                                    ,NX_L,NY_L
+     1                                    ,filename,istatus)
+
+      character*70 filename
+
+!.............................................................................
+
+      include 'netcdf.inc'
+      integer maxLevels, recNum,nf_fid, nf_vid, nf_status
+C
+C  Open netcdf File for reading
+C
+      nf_status = NF_OPEN(filename,NF_NOWRITE,nf_fid)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'NF_OPEN',filename
+      endif
+C
+C  Fill all dimension values
+C
+C
+C Get size of maxLevels
+C
+      nf_status = NF_INQ_DIMID(nf_fid,'maxLevels',nf_vid)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'dim maxLevels'
+      endif
+      nf_status = NF_INQ_DIMLEN(nf_fid,nf_vid,maxLevels)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'dim maxLevels'
+      endif
+C
+C Get size of recNum
+C
+      nf_status = NF_INQ_DIMID(nf_fid,'recNum',nf_vid)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'dim recNum'
+      endif
+      nf_status = NF_INQ_DIMLEN(nf_fid,nf_vid,recNum)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'dim recNum'
+      endif
+      call main_sub(nf_fid, maxLevels, recNum,
+!.............................................................................
+     1              i4time_sys,ilaps_cycle_time,NX_L,NY_L,istatus)
+!.............................................................................
+
+      return
+      end
+C
+C
+      subroutine main_sub(nf_fid, maxLevels, recNum,
+!.............................................................................
+     1              i4time_sys,ilaps_cycle_time,NX_L,NY_L,istatus)
+
+      real*4 lat_a(NX_L,NY_L)
+      real*4 lon_a(NX_L,NY_L)
+      real*4 topo_a(NX_L,NY_L)
+
+      character*9 a9time_ob
+      character*4 c4_staname
+
+!.............................................................................
+
+      include 'netcdf.inc'
+      integer maxLevels, recNum,nf_fid, nf_vid, nf_status
+      integer numLevels(recNum), windDir( maxLevels, recNum)
+      real obHeight( maxLevels, recNum), staElev(recNum),
+     +     staLat(recNum), staLon(recNum), windSpeed( maxLevels,
+     +     recNum)
+      double precision timeObs(recNum)
+      character*4 staName(recNum)
+      character*350 rawMsg(recNum)
+      character rmsError( maxLevels, recNum)
+
+!............................................................................
+
+      call read_netcdf(nf_fid, maxLevels, recNum, numLevels, windDir, 
+     +     obHeight, staElev, staLat, staLon, windSpeed, timeObs, 
+     +     rawMsg, rmsError, staName)
+C
+C The netcdf variables are filled - your code goes here
+C
+!............................................................................
+
+      call get_domain_perimeter(NX_L,NY_L,'nest7grid',lat_a,lon_a, 
+     1            topo_a,1.0,rnorth,south,east,west,istatus)
+      if(istatus .ne. 1)then
+          write(6,*)' Error in get_laps_perimeter'
+          return
+      endif
+
+      num_vad = recNum
+      write(6,*)' # of vad = ',recNum
+
+      do ista = 1,num_vad
+
+          write(6,*)
+          write(6,*)' vad #',ista
+
+!         write(6,*)' location = '
+!    1             ,stalat(ista),stalon(ista),staelev(ista)
+
+          if(stalat(ista) .le. rnorth .and. stalat(ista) .ge. south 
+     1                                .and.
+     1       stalon(ista) .ge. west   .and. stalon(ista) .le. east      
+     1                                                             )then
+              continue
+          else ! Outside lat/lon perimeter - reject
+              write(6,*)' lat/lon - reject'       
+!    1                 ,stalat(ista),stalon(ista)
+              goto 900
+          endif
+
+
+          if(abs(timeObs(ista))      .lt. 3d9)then
+              call c_time2fname(nint(timeObs(ista)),a9time_ob)
+          else
+              write(6,*)' Bad observation time - reject',timeObs(ista)
+              goto 900
+          endif
+
+          call cv_asc_i4time(a9time_ob,i4time_ob)
+          i4_resid = abs(i4time_ob - i4time_sys)
+          if(i4_resid .gt. (ilaps_cycle_time / 2) )then ! outside time window
+              write(6,*)' time - reject '
+     1           ,a9time_ob,i4_resid,ilaps_cycle_time / 2
+              goto 900        
+          endif
+
+          write(6,1)a9time_ob
+ 1        format(' Ob Time:',1x,a9)
+
+          id_num = 0
+
+          c4_staname = 'K'//staname(ista)(1:3)
+
+          write(6,401)id_num,numlevels(ista),
+     1                stalat(ista),stalon(ista),staelev(ista),
+     1                c4_staname,a9time_ob
+          write(2,401)id_num,numlevels(ista),
+     1                stalat(ista),stalon(ista),staelev(ista),
+     1                c4_staname,a9time_ob
+401       format(i12,i12,f11.3,f15.3,f15.0,5x,a4,5x,a9)
+
+!         Set the rms error
+          rms = 10.
+          if(rmserror(ilvl,ista)(1:1) .eq. 'A')rms = 1.03
+          if(rmserror(ilvl,ista)(1:1) .eq. 'B')rms = 2.06
+          if(rmserror(ilvl,ista)(1:1) .eq. 'C')rms = 3.09
+          if(rmserror(ilvl,ista)(1:1) .eq. 'D')rms = 4.12
+          if(rmserror(ilvl,ista)(1:1) .eq. 'E')rms = 5.14
+          if(rmserror(ilvl,ista)(1:1) .eq. 'F')rms = 6.17
+          if(rmserror(ilvl,ista)(1:1) .eq. 'G')rms = 7.20
+
+          do ilvl = 1,numlevels(ista)
+              write(2,301,err=303)obheight(ilvl,ista),
+     1                            float(winddir(ilvl,ista)), 
+     1                            windspeed(ilvl,ista),rms       
+              write(6,301,err=303)obheight(ilvl,ista),
+     1                            float(winddir(ilvl,ista)), 
+     1                            windspeed(ilvl,ista),rms       
+301           format(1x,f6.0,f6.0,2f6.1)
+303           continue
+          enddo ! i
+
+ 900  enddo ! i
+
+!............................................................................
+
+      return
+      end
+C
+C  Subroutine to read the file "VAD Wind data : selected by receipt time : time range from 883602000 to 883603800" 
+C
+      subroutine read_netcdf(nf_fid, maxLevels, recNum, numLevels, 
+     +     windDir, obHeight, staElev, staLat, staLon, windSpeed, 
+     +     timeObs, rawMsg, rmsError, staName)
+C
+      include 'netcdf.inc'
+      integer maxLevels, recNum,nf_fid, nf_vid, nf_status
+      integer numLevels(recNum), windDir( maxLevels, recNum)
+      real obHeight( maxLevels, recNum), staElev(recNum),
+     +     staLat(recNum), staLon(recNum), windSpeed( maxLevels,
+     +     recNum)
+      double precision timeObs(recNum)
+      character*4 staName(recNum)
+      character*350 rawMsg(recNum)
+      character rmsError( maxLevels, recNum)
+
+
+C   Variables of type REAL
+C
+C     Variable        NETCDF Long Name
+C      obHeight     "Observation height above MSL"
+C
+        nf_status = NF_INQ_VARID(nf_fid,'obHeight',nf_vid)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var obHeight'
+      endif
+        nf_status = NF_GET_VAR_REAL(nf_fid,nf_vid,obHeight)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var obHeight'
+      endif
+C
+C     Variable        NETCDF Long Name
+C      staElev      "Elevation of station above MSL"
+C
+        nf_status = NF_INQ_VARID(nf_fid,'staElev',nf_vid)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var staElev'
+      endif
+        nf_status = NF_GET_VAR_REAL(nf_fid,nf_vid,staElev)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var staElev'
+      endif
+C
+C     Variable        NETCDF Long Name
+C      staLat       "Station latitude"
+C
+        nf_status = NF_INQ_VARID(nf_fid,'staLat',nf_vid)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var staLat'
+      endif
+        nf_status = NF_GET_VAR_REAL(nf_fid,nf_vid,staLat)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var staLat'
+      endif
+C
+C     Variable        NETCDF Long Name
+C      staLon       "Station longitude"
+C
+        nf_status = NF_INQ_VARID(nf_fid,'staLon',nf_vid)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var staLon'
+      endif
+        nf_status = NF_GET_VAR_REAL(nf_fid,nf_vid,staLon)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var staLon'
+      endif
+C
+C     Variable        NETCDF Long Name
+C      windSpeed    "Wind speed"
+C
+        nf_status = NF_INQ_VARID(nf_fid,'windSpeed',nf_vid)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var windSpeed'
+      endif
+        nf_status = NF_GET_VAR_REAL(nf_fid,nf_vid,windSpeed)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var windSpeed'
+      endif
+
+C   Variables of type INT
+C
+C
+C     Variable        NETCDF Long Name
+C      numLevels    "Number of observations for station"
+C
+        nf_status = NF_INQ_VARID(nf_fid,'numLevels',nf_vid)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var numLevels'
+      endif
+        nf_status = NF_GET_VAR_INT(nf_fid,nf_vid,numLevels)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var numLevels'
+      endif
+C
+C     Variable        NETCDF Long Name
+C      windDir      "Wind direction"
+C
+        nf_status = NF_INQ_VARID(nf_fid,'windDir',nf_vid)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var windDir'
+      endif
+        nf_status = NF_GET_VAR_INT(nf_fid,nf_vid,windDir)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var windDir'
+      endif
+
+C   Variables of type DOUBLE
+C
+C
+C     Variable        NETCDF Long Name
+C      timeObs      "Time of observation"
+C
+        nf_status = NF_INQ_VARID(nf_fid,'timeObs',nf_vid)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var timeObs'
+      endif
+        nf_status = NF_GET_VAR_DOUBLE(nf_fid,nf_vid,timeObs)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var timeObs'
+      endif
+
+
+C   Variables of type CHAR
+C
+C
+C     Variable        NETCDF Long Name
+C      rawMsg       "Receipt format VAD Winds message"
+C
+        nf_status = NF_INQ_VARID(nf_fid,'rawMsg',nf_vid)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var rawMsg'
+      endif
+        nf_status = NF_GET_VAR_TEXT(nf_fid,nf_vid,rawMsg)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var rawMsg'
+      endif
+C
+C     Variable        NETCDF Long Name
+C      rmsError     "RMS vector wind error"
+C
+        nf_status = NF_INQ_VARID(nf_fid,'rmsError',nf_vid)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var rmsError'
+      endif
+        nf_status = NF_GET_VAR_TEXT(nf_fid,nf_vid,rmsError)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var rmsError'
+      endif
+C
+C     Variable        NETCDF Long Name
+C      staName      "Three letter WMO site identifier"
+C
+        nf_status = NF_INQ_VARID(nf_fid,'staName',nf_vid)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var staName'
+      endif
+        nf_status = NF_GET_VAR_TEXT(nf_fid,nf_vid,staName)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'in var staName'
+      endif
+
+      nf_status = nf_close(nf_fid)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'nf_close'
+      endif
+
+      return
+      end
+
+
