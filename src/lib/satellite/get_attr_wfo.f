326a2b22090e62fbf5944ac033b8ce5c7c7f690e
smart <>
1998-03-16 11:32:34 +0000

    New routine to extract lat/lon nav info from wfo satellite netcdf files.

---
+++ src/lib/satellite/get_attr_wfo.f
@@ -0,0 +1,60 @@
+      subroutine get_attribute_wfo(filename,rlat00,rlon00,istatus)
+C
+C  Open netcdf File for reading
+C
+      character filename*(*)
+      integer   istatus
+      real      rlat00
+      real      rlon00
+
+      include 'netcdf.inc'
+
+      lenf=index(filename,' ')-1
+      nf_status = NF_OPEN(filename,NF_NOWRITE,nf_fid)
+
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'NF_OPEN ',filename(1:lenf)
+        istatus=-1
+        goto 100
+      endif
+C
+C Get lat00
+C
+      nf_status = NF_INQ_ATTID(nf_fid,nf_attid,'lat00',nf_attnum)
+      if(nf_status.ne.NF_NOERR)	then
+         print*, NF_STRERROR(nf_status)
+         print*, 'lat00 varid'
+         istatus=-1
+         goto 100
+      endif
+
+      nf_status = NF_GET_ATT_REAL(nf_fid,nf_attid,'lat00',rlat00)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'lat00'
+         istatus=-1
+         goto 100
+      endif
+C
+C Get lon00
+C
+      nf_status = NF_INQ_ATTID(nf_fid,nf_attid,'lon00',nf_attnum)
+      if(nf_status.ne.NF_NOERR) then
+         print*, NF_STRERROR(nf_status)
+         print*, 'lon00 varid'
+         istatus=-1
+         goto 100
+      endif
+
+      nf_status = NF_GET_ATT_REAL(nf_fid,nf_attid,'lon00',rlon00)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'lon00'
+        istatus=-1
+        goto 100
+      endif
+C
+      istatus = 0
+      return
+      end
