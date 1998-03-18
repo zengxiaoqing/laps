96bc603703fea6aca97ba3632c1ae1526fe7314a
smart <>
1998-03-18 14:27:15 +0000

    added x/y (pixel/line) resolution (km).

--- src/lib/satellite/get_attr_wfo.f
+++ src/lib/satellite/get_attr_wfo.f
@@ -1,4 +1,5 @@
-      subroutine get_attribute_wfo(filename,rlat00,rlon00,istatus)
+      subroutine get_attribute_wfo(filename,rlat00,rlon00,resx,
+     &resy,istatus)
 C
 C  Open netcdf File for reading
 C
@@ -6,6 +7,7 @@ C
       integer   istatus
       real      rlat00
       real      rlon00
+      real      resx,resy
 
       include 'netcdf.inc'
 
@@ -24,7 +26,7 @@ C
       nf_status = NF_INQ_ATTID(nf_fid,nf_attid,'lat00',nf_attnum)
       if(nf_status.ne.NF_NOERR)	then
          print*, NF_STRERROR(nf_status)
-         print*, 'lat00 varid'
+         print*, 'lat00 attribute id'
          istatus=-1
          goto 100
       endif
@@ -42,7 +44,7 @@ C
       nf_status = NF_INQ_ATTID(nf_fid,nf_attid,'lon00',nf_attnum)
       if(nf_status.ne.NF_NOERR) then
          print*, NF_STRERROR(nf_status)
-         print*, 'lon00 varid'
+         print*, 'lon00 attribute id'
          istatus=-1
          goto 100
       endif
@@ -51,6 +53,42 @@ C
       if(nf_status.ne.NF_NOERR) then
         print *, NF_STRERROR(nf_status)
         print *,'lon00'
+        istatus=-1
+        goto 100
+      endif
+C
+C Get resx
+C
+      nf_status = NF_INQ_ATTID(nf_fid,nf_attid,'dxKm',nf_attnum)
+      if(nf_status.ne.NF_NOERR) then
+         print*, NF_STRERROR(nf_status)
+         print*, 'dxKm attribute id'
+         istatus=-1
+         goto 100
+      endif
+
+      nf_status = NF_GET_ATT_REAL(nf_fid,nf_attid,'dxKm',resx)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'dxKm'
+        istatus=-1
+        goto 100
+      endif
+C
+C Get resx
+C
+      nf_status = NF_INQ_ATTID(nf_fid,nf_attid,'dyKm',nf_attnum)
+      if(nf_status.ne.NF_NOERR) then
+         print*, NF_STRERROR(nf_status)
+         print*, 'dyKm attribute id'
+         istatus=-1
+         goto 100
+      endif
+
+      nf_status = NF_GET_ATT_REAL(nf_fid,nf_attid,'dyKm',resy)
+      if(nf_status.ne.NF_NOERR) then
+        print *, NF_STRERROR(nf_status)
+        print *,'dyKm'
         istatus=-1
         goto 100
       endif
