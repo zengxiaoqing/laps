596e7093573a8776b35dfc3e1ec0e2eabcbab0e4
jedwards <>
1998-05-12 18:16:45 +0000

    Pressure Should increase from the bottom

--- src/lib/get_pres_3d.f
+++ src/lib/get_pres_3d.f
@@ -1,26 +1,25 @@
       subroutine get_pres_3d(i4time,ni,nj,nk,pres_3d,istatus)
-      include 'lapsparms.cmn'
-       
+      integer ni,nj,nk       
       real*4 pres_3d(ni,nj,nk)      
+      include 'lapsparms.cmn'
 
-      if(iflag_lapsparms_cmn .ne. 1)then
-         write(6,*)' get_pres_3d: calling get_laps_config'
+      write(6,*)' get_pres_3d: calling get_laps_config'
 
-         call get_laps_config('nest7grid',istatus)
-         if(istatus .ne. 1 .or. iflag_lapsparms_cmn .ne. 1)then
-            write(6,*)' Error detected in calling get_laps_config'
-            istatus = 0
-            return
-         else
-            write(6,*)' Success in calling get_laps_config'
-         endif
-
+      call get_laps_config('nest7grid',istatus)
+      if(istatus .ne. 1 .or. iflag_lapsparms_cmn .ne. 1)then
+         write(6,*)' Error detected in calling get_laps_config'
+         istatus = 0
+         return
+      else
+         write(6,*)' Success in calling get_laps_config'
       endif
+
+
 
       call upcase(vertical_grid,vertical_grid)
       if(vertical_grid.eq.'PRESSURE') then
          do k = 1,nk
-            pressure = PRESSURE_BOTTOM_L+(k-1)*PRESSURE_INTERVAL_L
+            pressure = PRESSURE_BOTTOM_L-(k-1)*PRESSURE_INTERVAL_L
             do j = 1,nj
                do i = 1,ni
                   pres_3d(i,j,k) = pressure
