5811741861d6a4d78e8f9184dfe2ae6d47fa2b7b
smart <>
1998-12-11 15:52:38 +0000

    source used in vrc_driver allowing automatic map transformation from native wsi/conus grid to laps. New namelist for vrc only has wait parameters.

--- src/lib/radar/wsi_ingest/genllijlut.f
+++ src/lib/radar/wsi_ingest/genllijlut.f
@@ -29,7 +29,7 @@ cdis
 cdis 
 cdis 
 cdis 
-      subroutine gen_llij_lut_wsi(irad,imax,jmax,lat,lon
+      subroutine gen_rirj_ce(imax,jmax,lat,lon,ri,rj
      +    ,istatus)
 c
 c
@@ -51,7 +51,6 @@ c
        real*4 dx,dy
        real*4 rla1,rlo1
        real*4 rla2,rlo2
-       real*4 rlatin,rlap
        real*4 rlat,rlon
        real*4 rlatc,rlonc
        real*4 dlat,dlon
@@ -61,7 +60,6 @@ c
        integer i,j
        integer n,n1
        integer nx,ny,nz
-       integer irad
        integer istatus
        integer nlines
        integer nelems
@@ -77,11 +75,10 @@ c
 c ***************************************************************************
 c
       istatus=1
-      call get_wsi_parms_vrc(irad,nlines,nelems,
-     +dx,dy,rla1,rlo1,rla2,rlo2,rlat,rlon,rlatin,
-     +istatus)
-
-      write(6,*)'Parameters from vrc_nl namelist'
+c
+c need to get latest filetime from appropriate data subdirectory
+c
+      write(6,*)'NAV Parameters'
       write(6,*)'dx     ',dx
       write(6,*)'dy     ',dy
       write(6,*)'nelems ',nelems
@@ -92,10 +89,7 @@ c
       write(6,*)'rlo2   ',rlo2
       write(6,*)'rlon   ',rlon
       write(6,*)'rlat   ',rlat
-      write(6,*)'rlatin ',rlatin
 c
-      dlon = 0.019119
-      dlat = 0.017966
       rlatc = rlat
       rlonc = rlon
       nx = nelems
@@ -111,8 +105,6 @@ c  Build ri/rj look up for laps domain
 c
       call latlon_2_ceij(imax*jmax,lat,lon,ri,rj)
 
-
-
       write(6,*)'ri/rj corners for domain: '
       write(6,*)'--------------------------'
       write(6,*)'(SW) ',ri(1,1),rj(1,1)
@@ -124,6 +116,162 @@ c
 c
 c check for laps domain outside data domain
 c
+       call check_domain_vrc(imax,jmax,ri,rj,nx,ny)
+
+c
+c output
+c
+      write(6,*)'ri/rj corners for domain after check: '
+      write(*,*)'--------------------------------------'
+      write(6,*)'(SW) ',ri(1,1),rj(1,1)
+      write(6,*)'(SE) ',ri(imax,1),rj(imax,1)
+      write(6,*)'(NW) ',ri(1,jmax),rj(1,jmax)
+      write(6,*)'(NE) ',ri(imax,jmax),rj(imax,jmax)
+      write(6,*)
+
+      if(.false.)then
+        do i = 1,imax,10
+        do j = 1,jmax,10
+           write(6,*)'i,j,ri,rj: ',i,j,ri(i,j),rj(i,j)
+        enddo
+        enddo
+      endif
+
+      goto 16
+
+901   write(6,*)'Error reading parm file ',file(1:n)
+
+16    write(6,*)'Finished in get_llij_lut_wsi'
+      return
+      end
+c
+c *******************************************************************
+c *******************************************************************
+c
+      subroutine gen_rirj_lam(imax,jmax,lat,lon,nelems,nlines,
+     &        rla1,rlo1,rla2,rlo2,rlatin,rlov,dx,dy,
+     &        ri,rj,istatus)
+c
+      implicit none
+
+      integer*4 imax,jmax
+
+      real*4    lat(imax,jmax)
+      real*4    lon(imax,jmax)
+      real*4    rla1,rlo1
+      real*4    rla2,rlo2
+      real*4    dx,dy
+      real*4    du,dv
+      real*4    u_orig,v_orig
+      real*4    rlatin,rlov
+      real*4    pi
+      real*4    u,v
+
+      real*4    ri1,rj1
+      real*4    ri2,rj2
+      real*4    ri3,rj3
+      real*4    ri4,rj4
+
+      real*4    lapterm
+      real*4    lovterm
+      real*4    latterm
+      real*4    lonterm
+      real*4    dxterm
+      real*4    dyterm
+
+      real*4    ri(imax,jmax)
+      real*4    rj(imax,jmax)
+
+      integer   i,j,n,nn
+      integer   n1,n2
+      integer   nlines,nelems
+      integer   istatus
+
+      character*200 table_path
+      character*200 file
+      character*255 path
+      character*200 cname
+      character*3   ctype
+c
+c =================================
+      pi = acos(-1.0)
+c
+      write(6,*)'NAV Parameters'
+      write(6,*)'dx     ',dx
+      write(6,*)'dy     ',dy
+      write(6,*)'nelems ',nelems
+      write(6,*)'nlines ',nlines
+      write(6,*)'La1   ',rla1
+      write(6,*)'Lo1   ',rlo1
+c     write(6,*)'La2   ',rla2
+c     write(6,*)'Lo2   ',rlo2
+      write(6,*)'Lov   ',rlov
+      write(6,*)'Latin ',rlatin
+
+c compute lat lons  fsl conus grid (or read fm disk)
+c determine ri/rj pair for the four laps domain corners.
+c compute i,j  in lambert grid for desired points.
+c get delta u and delta v in the lambert grid
+
+      dxterm = dx/1000.
+      dyterm = dy/1000.
+      call getdudv_lam(rlov,rlatin,dxterm,dyterm,
+     &       rla1,rlo1,du,dv,u_orig,v_orig)
+      lapterm=(rlatin*pi)/180.
+      lovterm=(rlov*pi)/180.
+
+      do j = 1, jmax
+      do i = 1, imax
+         latterm=(lat(i,j)*pi)/180.
+         lonterm=(lon(i,j)*pi)/180.
+         call getuv_lam (lapterm,lovterm,
+     &       latterm,lonterm,u,v)
+         call uv_ij (nlines,u_orig,v_orig,du,dv,
+     &       u,v,ri(i,j),rj(i,j))
+      enddo
+      enddo
+
+      call check_domain_vrc(imax,jmax,ri,rj,nelems,nlines)
+c
+      if(.false.)then
+         do i = 1,imax,10
+         do j = 1,jmax,10
+            write(6,*)'i,j,ri,rj: ',i,j,ri(i,j),rj(i,j)
+         enddo
+         enddo
+      endif
+
+c
+      write(6,*)'Lambert-CONUS (WFO) corners for domain: '
+      write(6,*)'----------------------------------------'
+      write(6,*)'(SW) ',ri(1,1),rj(1,1)
+      write(6,*)'(SE) ',ri(imax,1),rj(imax,1)
+      write(6,*)'(NW) ',ri(1,jmax),rj(1,jmax)
+      write(6,*)'(NE) ',ri(imax,jmax),rj(imax,jmax)
+      write(6,*)
+
+      goto 900
+
+901   write(6,*)'Error openning file ',file(1:n)
+
+900   return
+      end
+
+      subroutine check_domain_vrc(imax,jmax,ri,rj,nx,ny)
+c
+c check for laps domain outside data domain
+c
+       implicit none
+
+       integer imax,jmax
+       integer i_out,j_out
+       integer i,j
+       integer nx,ny
+       integer istatus
+
+       real*4  ri(imax,jmax),rj(imax,jmax)
+       real*4  r_missing_data
+
        call get_r_missing_data(r_missing_data,istatus)
        i_out=0
        j_out=0
@@ -158,213 +306,9 @@ c
        endif
        if(i_out.gt.0.or.j_out.gt.0)then
           print*,'Check yor laps domain (nest7grid.parms) settings'
-          print*,'if you do not want partial coverage'
+          print*,'if you do not want partial coverage of domain'
        endif
-c
-c output
-c
-      write(6,*)'ri/rj corners for domain after check: '
-      write(*,*)'--------------------------------------'
-      write(6,*)'(SW) ',ri(1,1),rj(1,1)
-      write(6,*)'(SE) ',ri(imax,1),rj(imax,1)
-      write(6,*)'(NW) ',ri(1,jmax),rj(1,jmax)
-      write(6,*)'(NE) ',ri(imax,jmax),rj(imax,jmax)
-      write(6,*)
 
-      if(.false.)then
-        do i = 1,imax,10
-        do j = 1,jmax,10
-           write(6,*)'i,j,ri,rj: ',i,j,ri(i,j),rj(i,j)
-        enddo
-        enddo
-      endif
+       return
+       end
 
-      call get_directory('static',table_path,n1)
-      table_path = table_path(1:n1)//'vrc/wsi_llij_lut_wsi.lut'
-      write(6,*)'Write lat/lon to i/j look up table'
-      n1=index(table_path,' ')
-      write(6,*)table_path(1:n1)
-
-      call write_table (table_path,imax,jmax,lat,lon,ri,rj,istatus)
-      if(istatus .ne. 1)then
-         write(6,*)'Error writing look-up table'
-         goto 900
-      endif
-
-      goto 16
-
-900   write(6,*)'Error writting table ',file(1:n)
-      goto 16
-
-901   write(6,*)'Error reading parm file ',file(1:n)
-
-16    write(6,*)'Finished in get_llij_lut_wsi'
-      return
-      end
-c
-c *******************************************************************
-c *******************************************************************
-c
-      subroutine gen_vrc_wfo_cdf_lut(irad,imax,jmax,lat,lon,istatus)
-c
-      implicit none
-
-      integer*4 imax,jmax
-
-      real*4    lat(imax,jmax)
-      real*4    lon(imax,jmax)
-      real*4    rla1,rlo1
-      real*4    rla2,rlo2
-      real*4    rlat,rlon
-      real*4    dx,dy
-      real*4    du,dv
-      real*4    u_orig,v_orig
-      real*4    rlatin
-      real*4    pi
-      real*4    u,v
-
-      real*4    ri1,rj1
-      real*4    ri2,rj2
-      real*4    ri3,rj3
-      real*4    ri4,rj4
-
-      real*4    lapterm
-      real*4    lovterm
-      real*4    latterm
-      real*4    lonterm
-      real*4    dxterm
-      real*4    dyterm
-
-      real*4    ri(imax,jmax)
-      real*4    rj(imax,jmax)
-
-      integer   i,j,n,nn
-      integer   n1,n2
-      integer   nlines,nelems
-      integer   irad
-      integer   istatus
-
-      character*200 table_path
-      character*200 file
-      character*255 path
-      character*200 cname
-      character*3   ctype
-c
-c =================================
-      pi = acos(-1.0)
-c
-
-      call get_wsi_parms_vrc(irad,nlines,nelems,
-     +dx,dy,rla1,rlo1,rla2,rlo2,rlat,rlon,rlatin,
-     +istatus)
-
-      write(6,*)'Parameters from vrc_nl namelist'
-      write(6,*)'dx     ',dx
-      write(6,*)'dy     ',dy
-      write(6,*)'nelems ',nelems
-      write(6,*)'nlines ',nlines
-      write(6,*)'rla1   ',rla1
-      write(6,*)'rlo1   ',rlo1
-      write(6,*)'rla2   ',rla2
-      write(6,*)'rlo2   ',rlo2
-      write(6,*)'rlon   ',rlon
-      write(6,*)'rlat   ',rlat
-      write(6,*)'rlatin ',rlatin
-
-c compute lat lons  fsl conus grid (or read fm disk)
-c determine ri/rj pair for the four laps domain corners.
-c compute i,j  in lambert grid for desired points.
-c get delta u and delta v in the lambert grid
-
-         dxterm = dx/1000.
-         dyterm = dy/1000.
-         call getdudv_lam(rlon,rlat,dxterm,dyterm,
-     &       rla1,rlo1,du,dv,u_orig,v_orig)
-c
-c compute corner point ri/rj pairs used to determine # of i and j
-c
-         write(6,*)'Compute corner points'
-
-         lapterm=(rlat*pi)/180.
-         lovterm=(rlon*pi)/180.
-         latterm=(lat(1,1)*pi)/180.
-         lonterm=(lon(1,1)*pi)/180.
-
-         call getuv_lam (lapterm,lovterm,
-     &       latterm,lonterm,u,v)
-            call uv_ij (nlines,u_orig,v_orig,du,dv,
-     &       u,v,ri1,rj1)
-
-         latterm=(lat(imax,1)*pi)/180.
-         lonterm=(lon(imax,1)*pi)/180.
-
-         call getuv_lam (lapterm,lovterm,
-     &       latterm,lonterm,u,v)
-            call uv_ij (nlines,u_orig,v_orig,du,dv,
-     &       u,v,ri2,rj2)
-
-         latterm=(lat(1,jmax)*pi)/180.
-         lonterm=(lon(1,jmax)*pi)/180.
-
-         call getuv_lam (lapterm,lovterm,
-     &       latterm,lonterm,u,v)
-            call uv_ij (nlines,u_orig,v_orig,du,dv,
-     &       u,v,ri3,rj3)
-
-         latterm=(lat(imax,jmax)*pi)/180.
-         lonterm=(lon(imax,jmax)*pi)/180.
-
-         call getuv_lam (lapterm,lovterm,
-     &       latterm,lonterm,u,v)
-            call uv_ij (nlines,u_orig,v_orig,du,dv,
-     &       u,v,ri4,rj4)
-c
-         write(6,*)'WFO/WSI ri/rj corners for domain: '
-ccc     &laps_domain_file
-         write(6,*)'ri1/rj1 (SW) ',ri1,rj1
-         write(6,*)'ri2/rj2 (SE) ',ri2,rj2
-         write(6,*)'ri3/rj3 (NW) ',ri3,rj3
-         write(6,*)'ri4/rj4 (NE) ',ri4,rj4
-         write(6,*)
-c
-c first get uv in lambert grid
-c
-         do j = 1, jmax
-         do i = 1, imax
-
-            latterm=(lat(i,j)*pi)/180.
-            lonterm=(lon(i,j)*pi)/180.
-
-            call getuv_lam (lapterm,lovterm,
-     &       latterm,lonterm,u,v)
-            call uv_ij (nlines,u_orig,v_orig,du,dv,
-     &       u,v,ri(i,j),rj(i,j))
-
-         enddo
-         enddo
-c
-         do i = 1,imax,10
-         do j = 1,jmax,10
-
-            write(6,*)'i,j,ri,rj: ',i,j,ri(i,j),rj(i,j)
-
-         enddo
-         enddo
-         call get_directory('static',table_path,n1)
-         table_path = table_path(1:n1)//'vrc/wsi_llij_lut_wfo.lut'
-         write(6,*)'Write lat/lon to i/j look up table'
-         n1=index(table_path,' ')
-         write(6,*)table_path(1:n1)
-
-         call write_table (table_path,imax,jmax,lat,lon,ri,rj,istatus)
-         if(istatus .ne. 1)then
-            write(6,*)'Error writing look-up table'
-            goto 900
-         endif
-c
-         goto 900
-
-901      write(6,*)'Error openning file ',file(1:n)
-
-900      return
-         end
--- src/lib/radar/wsi_ingest/get_nl_parms.f
+++ src/lib/radar/wsi_ingest/get_nl_parms.f
@@ -1,19 +1,16 @@
       subroutine get_ln3_parameters(msng_radar,ickint,
-     +itotwait,iageth,istart,jstart,iend,jend,istatus)
+     +itotwait,iageth,istatus)
 c
       implicit none
 
       integer    istatus
       integer    len_dir
-      integer    istart,jstart
-      integer    iend,jend
       integer    ickint,itotwait,iageth
       integer    msng_radar
 
       character  nest7grid*150
 
-      namelist /ln3_nl/ msng_radar,ickint,itotwait,
-     + iageth,istart,jstart,iend,jend
+      namelist /ln3_nl/msng_radar,ickint,itotwait,iageth
 
       istatus = 1
 
@@ -39,56 +36,31 @@ c
 c
 c------------------------------------------------------
 c
-      subroutine get_wsi_parms_vrc(irad,lines,elems,
-     +dx,dy,rla1,rlo1,rla2,rlo2,rlat,rlon,rlatin,istatus)
+      subroutine read_vrc_nl(iwsimsng,icheckint,iwaittime,
+     +iagethresh,istatus)
 
-      integer nrad_types
-      parameter (nrad_types=2)
-
-      integer nlines(nrad_types)
-      integer nelems(nrad_types)
-      integer irad
-      integer lines,elems
+      integer imsngrad
+      integer icheckint
+      integer iwaittime
+      integer iagethresh
       integer istatus
-
-      real*4  resx(nrad_types)
-      real*4  resy(nrad_types)
-      real*4  radla1(nrad_types)
-      real*4  radlo1(nrad_types)
-      real*4  radla2(nrad_types)
-      real*4  radlo2(nrad_types)
-      real*4  radlat(nrad_types)
-      real*4  radlon(nrad_types)
-      real*4  radlatin(nrad_types)
-
-      real*4  rla1,rlo1,rlat,rlon,rlatin
-      real*4  dx,dy
 
       character nest7grid*150
 
-      namelist /vrc_nl/nlines,nelems,resx,resy,radla1,
-     +radlo1,radla2,radlo2,radlat,radlon,radlatin
+      namelist /vrc_nl/iwsimsng,icheckint,iwaittime,iagethresh
 
-      istatus = 0
+      istatus = 1
 
       call get_directory('nest7grid',nest7grid,len_dir)
+      if(nest7grid(len_dir:len_dir).ne.'/') then
+        len_dir=len_dir+1
+        nest7grid(len_dir:len_dir)='/'
+      endif
 
-      nest7grid = nest7grid(1:len_dir)//'/vrc.nl'
+      nest7grid = nest7grid(1:len_dir)//'vrc.nl'
 
       open(1,file=nest7grid,status='old',err=900)
       read(1,vrc_nl,err=901)
-
-      lines=nlines(irad)
-      elems=nelems(irad)
-      dx=resx(irad)
-      dy=resy(irad)
-      rla1=radla1(irad)
-      rlo1=radlo1(irad)
-      rla2=radla2(irad)
-      rlo2=radlo2(irad)
-      rlat=radlat(irad)
-      rlon=radlon(irad)
-      rlatin=radlatin(irad)
 
       close(1)
       istatus = 1
