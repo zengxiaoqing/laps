72f2c21290b25e2a440315389a6538a54a19bc0d
Daniel Birkenheuer <daniel.l.birkenheuer@noaa.gov>
1999-05-27 21:42:11 +0000

    Modified elabling the function to extend below -47C down to -132C with reasonable accuracy.

--- src/lib/mthermo/eslo.f
+++ src/lib/mthermo/eslo.f
@@ -29,23 +29,41 @@ cdis
 cdis 
 cdis 
 cdis 
-	function eslo(t)
+      function eslo(t)
 
-c	baker, schlatter  17-may-1982	  original version.
+      implicit none
 
-c   this function returns the saturation vapor pressure over liquid
-c   water eslo (millibars) given the temperature t (celsius). the
-c   formula is due to lowe, paul r.,1977: an approximating polynomial
-c   for the computation of saturation vapor pressure, journal of applied
-c   meteorology, vol 16, no. 1 (january), pp. 100-103.
-c   the polynomial coefficients are a0 through a6.
+      real eslo
+      real a0,a1,a2,a3,a4,a5,a6
+      real t
+      real esice ! function
 
-	data a0,a1,a2,a3,a4,a5,a6
-     1  /6.107799961,     4.436518521e-01, 1.428945805e-02,
-     2   2.650648471e-04, 3.031240396e-06, 2.034080948e-08,
-     3	 6.136820929e-11/
-	es = a0+t*(a1+t*(a2+t*(a3+t*(a4+t*(a5+a6*t)))))
-	if (es.lt.0.) es = 0.
-	eslo = es
-	return
-	end
+c     baker, schlatter  17-may-1982	  original version.
+c     added functionality to extend to temps below -47C (by approx)
+c     D. Birkenheuer 5/19/99.  approx based on formula via Pielke's book
+c     tuned by Dan to match Lowe's function 0>t>-47c.
+
+c     this function returns the saturation vapor pressure over liquid
+c     water eslo (millibars) given the temperature t (celsius). the
+c     formula is due to lowe, paul r.,1977: an approximating polynomial
+c     for the computation of saturation vapor pressure, journal of applied
+c     meteorology, vol 16, no. 1 (january), pp. 100-103.
+c     the polynomial coefficients are a0 through a6.
+
+      data a0,a1,a2,a3,a4,a5,a6
+     1     /6.107799961,     4.436518521e-01, 1.428945805e-02,
+     2     2.650648471e-04, 3.031240396e-06, 2.034080948e-08,
+     3     6.136820929e-11/
+
+
+      if (t.gt.-46.) then ! Lowe's function
+         eslo = a0+t*(a1+t*(a2+t*(a3+t*(a4+t*(a5+a6*t)))))
+         if (eslo.lt.0.) eslo = 0.
+      elseif (t.gt.-132.) then ! Pielke's tuned function (liquid data unknown)
+         eslo = 6.10813510 * exp (t*17.66795997 / 
+     1        (t+273.15-29.96392251) )
+      else ! adjust to ice, liquid data unknown
+         eslo = esice(t)
+      endif
+      return
+      end
