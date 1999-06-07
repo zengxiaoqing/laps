5b9752ede9453cff06155589ba4f3b67ea2e23ef
Daniel Birkenheuer <daniel.l.birkenheuer@noaa.gov>
1999-06-07 19:57:56 +0000

    Vsn 1.16 includes RAOB time discrinimation, improved outlier techniques for determining scaling, and specific RAOB designation (disallows SATSND).
    Main customer: AFWA

--- src/humid/raob_step.f
+++ src/humid/raob_step.f
@@ -38,32 +38,32 @@ cdis
 
 c       this module modifies the input data array of specific humidity
 c       with available raob data
-c       this module will us .snd data if it is available.
-c       in addition there is a way to turn off this module in the presense
+c       this module will use .snd data if it is available.
+c       in addition there is a way to turn off this module in the presence
 c       of .snd data.   simply modify the static file
 c       ../static/raob_moisture_switch.txt
-c       by putting a 0 in the first recort.  the advantage of this approach
+c       by putting a 0 in the first record.  the advantage of this approach
 c       is that it is external to the program and will not require a
 c       recompile.
 c
-c       this software was developed by dan birkenheuer, june 1996
+c
 c
 c       theory:
 c
 c       this code utilizes the second pass of a barnes analysis.
 c       in this form, a background is already available i.e., from the
 c       model forecast that the moisture analysis obtains from .lga
+c       or in 4dda mode from a model run
 c
-c       the second pass barnes takes the background and adds to it the
-c       weighted differences between the background and the raob observations
-c       the difference "effects" are distributed over the entire domain
-c       with the respective weights being a function of the distance from
-c       the observation.
+c       the second pass barnes scales the background to the raob data
+c       the scaling approach prevents illogical adjustments in large
+c       domains, common for AFWA application.
 c
 c       this is a "one-pass" analysis.
 c
-c       lambda in this code is hardwired to 600km.  see barnes for the
-c       significance of lambda.
+c       the radius of influence of a raob is 600km.  At that distance,
+c       the weight associated with a raob is 0.5.  it falls off rapidly
+c       as distance increases
 c
 c       a note on the look_back_time parameter:
 c
@@ -72,9 +72,9 @@ c       only at the time of observation (not in the event of no obs)
 c       hence this is really not required in the current way the "system"
 c       as a whole is being coded as a group.  in the "group" approach,
 c       a .snd file will be created each laps time and will automatically
-c       contain the raob data for a predescribed "look back time".  typically
-c       this will be 3 hours.  if there is no data, the .snd file will exist
-c       and will be empty.
+c       contain the raob data for a predescribed "look back time".  
+c       typically this will be 3 hours.  if there is no data, the .snd file 
+c       will exist and will be empty.
 c
 c       if for some reason this group approach for lookback time is ever
 c       dropped or modified, this code contains a switch to accommodate that
@@ -92,11 +92,12 @@ c       unable to find .snd data for the current time.
 c     include 'lapsparms.for'
 c     include 'parmtrs.inc'
 
-c     parameterize number of levels and soundings allowed
+c     parameters for sounding max and mins
 
-      integer snd_levels,snd_total
-      parameter (snd_levels = 300)
-      parameter (snd_total = 1000)
+      integer snd_lev, snd_tot
+      parameter (snd_lev = 300)
+      parameter (snd_tot = 1000)
+
 
 
 c     input parameters
@@ -107,10 +108,10 @@ c     input parameters
 
 c  dynamic dependent parameters                     
 
-      real  q_r(kk,snd_total)
-      real diff(kk,snd_total)
-      real scale(kk,snd_total)
-      real weight (ii,jj,snd_total)
+      real  q_r(kk, snd_tot)
+      real diff(kk, snd_tot)
+      real scale(kk, snd_tot)
+      real weight (ii,jj, snd_tot)
       integer mask(ii,jj)
 
 c  normal internal parameters
@@ -118,17 +119,22 @@ c  normal internal parameters
       integer i,j,k,ks,is
       integer look_back_time
       integer raob_i4time       ! actual time of raob data used
+      integer raob_i4time_chk   ! used to validate actual raob
       integer numoffiles, istatus, maxfiles
       character*256 c_filenames(200),pathname_in
       character*9 filename
       character*200 fname
       integer idx, len
-      real lat_r(snd_total), lon_r(snd_total)
-      integer  i_r(snd_total), j_r(snd_total)
-      integer lev_r(snd_total), dummy, isound
+      real lat_r( snd_tot), lon_r( snd_tot)
+      integer  i_r( snd_tot), j_r( snd_tot)
+      integer lev_r( snd_tot), dummy, isound
       real rdummy
-      real p_r(snd_levels,snd_total), td_r(snd_levels,snd_total), 
-     1     t_r(snd_levels,snd_total)
+      character*5 cdummy
+      integer idummy
+      character*9 r_filename(ii*jj)
+      character*8 snd_type(ii*jj)
+      real p_r(snd_lev, snd_tot), td_r(snd_lev, snd_tot), 
+     1     t_r(snd_lev, snd_tot)
       real temt, temtd, ssh2
       real r, rmin
       real d2r,pi
@@ -136,14 +142,14 @@ c  normal internal parameters
       real wt_ck(ii,jj) ! weight check to test for ample RAOB data
       real rspacing_dum
       real rmd
-      real x(ii*jj)
+      real x(snd_tot)
       integer x_sum
       real ave(kk),adev(kk),sdev(kk),var(kk),skew(kk),curt(kk)
 
         
 c *** begin routine
 
-      write(6,*) 'Checking for data zeros, 4/14/99'
+      write(6,*) '1.16 Iterated sigma computation, timecheck, 6.4.99'
 
       pi = acos(-1.0)
       d2r = pi/180.
@@ -154,6 +160,7 @@ c *** begin routine
             wt_ck(i,j) = 0.0
          enddo
       enddo
+
       call get_r_missing_data(rmd, istatus)   
 
 c     *** read in the raob data
@@ -199,11 +206,19 @@ c     +++ validate the raob time
 c     +++ read raob file
       call get_directory('snd',fname,len)
 
-      do isound = 1,snd_total
+      open (12, file = fname(1:len)//filename//'.snd',
+     1     form='formatted',status='old',err=18)
+
+      isound = 0  ! initialize isound (enable time rejection) 5/24/99 db
+
+      do is = 1, snd_tot
+
+         isound = isound + 1
                 
-         open (12, file = fname(1:len)//filename//'.snd',
-     1        form='formatted',status='old',err=18)
- 15      read (12,*,end=16) dummy, idx, lat_r(isound), lon_r (isound)
+
+ 15      read (12,511,end=16) idummy,idx, lat_r(isound),lon_r (isound),
+     1        rdummy, cdummy,r_filename(isound),snd_type(isound)
+ 511     format(i12,i12,f11.4,f15.4,f15.0,1x,a5,3x,a9,1x,a8)
          print*, idx
          lev_r(isound) = 1
 
@@ -230,11 +245,36 @@ c     +++ read raob file
 
             endif
 
-         enddo
+         enddo !  i
 
          lev_r(isound) = lev_r(isound) - 1
+c     reject on time condition (one hour lookback)
+         if(r_filename(isound) .ne. filename) then ! examine closer
+            call i4time_fname_lp (r_filename(isound), raob_i4time_chk, 
+     1           istatus)
+            if (abs(raob_i4time - raob_i4time_chk) .gt. 3600) then ! over 1hr
+               write(6,*) 'rejecting on time bounds', r_filename(isound)
+               isound = isound -1 !reject -- out of time bounds
+            else
+               write(6,*) 'accepting.. ', r_filename(isound)
+c     test for non-RAOB type sounding
+               if (snd_type(isound) .ne. "RAOB    ") then ! reject (wrong type)
+                  write(6,*) 'rejecting ', snd_type(isound)
+                  isound = isound -1
+               endif
+            endif
+         else                   ! accept implicitly
+            write(6,*) 'accepting.. time exact ', snd_type(isound)
+c     test for non-RAOB type sounding
+            if (snd_type(isound) .ne. "RAOB    ") then ! reject (wrong type)
+               write(6,*) 'rejecting ', snd_type(isound)
+               isound = isound -1
+            endif
 
-      enddo
+         endif
+
+
+      enddo  ! dummy is loop
 
  16   close (12)
 
@@ -249,6 +289,8 @@ c     +++ read raob file
 
 c     +++ compute the nearest i,j for all sounding locations
 c     also compute weighting function for all locations for given raob
+c     this approach used to accommodate any map projection or sized 
+c     domain
 
 
       do k = 1,isound
@@ -273,6 +315,7 @@ c     also compute weighting function for all locations for given raob
 
                weight(i,j,k) = exp ( -1.*(r * 6419)**2/519370.)
                wt_ck(i,j) = wt_ck(i,j) + weight(i,j,k)
+
                call check_nan(weight(i,j,k),istatus)
                if(istatus.eq.0) then ! nan detected bail
                   write(6,*) 'nan detected in reading raobs (weight)'
@@ -330,10 +373,8 @@ c     now check for enough raob data
 c     for afwa and other very large domains where cutoff is encountered
 c     it is necessarey to get rid of discontinuities a bit.  
 c     this is skipped for the moment given the test for data in the
-c     domain... not enough... too bad
-
-
-
+c     domain... so far this step seems not to be needed.
+c
 c      do k = 1, isound
 c         do j =1,jj
 c            do i =1,ii
@@ -374,7 +415,8 @@ c     pressure levels
                   write(6,*) 'nan detected in processing raobs (q_r)'
                   write(6,*) 'k, is ', k,is
                   return
-               endif        
+               endif 
+       
             endif
 
          enddo                  ! k
@@ -407,53 +449,79 @@ c *** difference the raob data at each gridpoint location and height
 
       enddo
 
-c     compute level mean and sigma
+c     compute level mean and sigma  modified from original... now examines
+c     bias data (diff variable)
 
       write(6,*) 'compting layer statistics'
       
-      do k = 1,kk
+      do k = 1,kk               ! do for each layer
          x_sum = 0
-         do i = 1,ii
-            do j = 1,jj
-               if(data(i,j,k) .ne. rmd .or. data(i,j,k).gt. 0.0 ) then
-                  x_sum=x_sum +1
-                  x(x_sum) = data (i,j,k)
-               endif
-            enddo
-         enddo
+         do is = 1,isound       ! do for all differences each layer
+            if(diff(k,is) .ne. rmd ) then
+               x_sum=x_sum +1
+               x(x_sum) = diff(k,is)
+            endif
 
-         call moment_b (x,x_sum,ave(k),adev(k),sdev(k),
+c     all data assembled for given layer, compute stats for that layer.
+
+         enddo                  ! is
+      
+
+ 220     call moment_b (x,x_sum,ave(k),adev(k),sdev(k),
      1        var(k),skew(k),curt(k), istatus)
 
-         if (istatus.ne.1) then
+         if(istatus.ne.1) go to 222
+
+c     this loop put in to recursivly recompute sigma after changing
+c     the population.  here use 3*sigma criteria  (normal full population)
+         
+         do is = 1, x_sum
+            if(x(is).gt.ave(k)+3.*sdev(k) .or. 
+     1           x(is).lt.ave(k)-3.*sdev(k)) then ! reject
+               do i = is,x_sum-1 ! discard bad x(is), shift
+                  x(is) = x(is + 1)
+               enddo
+               x_sum = x_sum -1  !decrement x_sum
+               go to 220        !recompute mean
+            endif
+         enddo
+
+
+
+ 222     if (istatus.ne.1) then
             write(6,*) 'not enough data for computing moments level ', k
+            write(6,*) 'this will result in NO raob use for this level'
             ave(k) = rmd
             sdev(k) = rmd
          else
-            write (6,*) k,x_sum,ave(k),sdev(k)
+            write (6,*) x_sum,ave(k),sdev(k),' level ',k
          endif
 
-      enddo
+      enddo                     ! k
 
 c     compare computed diff values with QC thresholding... assign rmd if bad
-
-      do is=1,isound
+c     here use 2*sigma cutoff for addnl constraint.
+      
+      do is = 1,isound
          do k = 1,kk
-
+            
             if(sdev(k).ne.rmd) then
  
-               if(abs(diff(k,is)) .le. 2.*sdev(k) ) then
+               if   (
+     1              diff(k,is) .le. ave(k)+2.*sdev(k) 
+     1              .and. 
+     1              diff (k,is) .ge. ave(k)-2.*sdev(k)
+     1              ) then      ! in accept range
                   write(6,*) 'accepting ',k,is,diff(k,is)
-               elseif (diff(k,is) .lt. 0.0 
-     1                 .and. abs(diff(k,is)) .le. 3.5*sdev(k) ) then
-                  write (6,*) 'accepting extreme negative value', 
-     1                 k,is,diff(k,is)
                else
-                  write(6,*) 'tossing ',k,is, diff(k,is)
+                  write(6,*) 'tossing (exceeds limits)',k,is, diff(k,is)
                   diff(k,is) = rmd
                endif
+            else
+               write(6,*) 'tossing level, too few stats level ',k
+               diff(k,is) = rmd
             endif
-  
+            
          enddo
       enddo
 
@@ -472,9 +540,9 @@ c     *** analyze the field (barnes second scaling pass to background)
 
                do is = 1,isound
 
-                  if (diff(k,is).eq.rmd) then !dont modify
+                  if (diff(k,is).eq.rmd) then ! dont modify
                      continue
-                  elseif(data(i,j,k).eq.rmd) then !dont modify
+                  elseif(data(i,j,k).eq.rmd) then ! dont modify
                      continue
                   else
                      tot_scale_weight = weight(i,j,is)*scale(k,is) 
@@ -485,28 +553,33 @@ c     *** analyze the field (barnes second scaling pass to background)
 
                enddo
 
-               if (
+               if   (
      1              (data(i,j,k) .ne. rmd)
      1              .and.
      1              (tot_weight.ne.0.0) 
      1              .and.
      1              (counter.ne.0.0)
-     1              )then
+     1              )   then
 
 c     compute the appropriate scale factor to apply (weighted average)
 
                   scale_used = tot_scale_weight/tot_weight
 
-c     compute the weight average for this location, this introduces possiblity for
-c     circles, but on the other reduces them due to exaggerated saturations going
-c     into regions that are near saturation already
+c     compute the weight average for this location, this introduces 
+c     possiblity for circles, but on the other hand reduces them due to
+c     exaggerated saturations going into regions that are near saturation
+c     already
 
                   weight_avg = tot_weight/counter
 
 c     apply the computed weights and averages to the data point in question
+c     the first approach (commented out) was dropped after the second 
+c     method was found to work well after assurance of a critical mass of
+c     data points.
 
 c             data(i,j,k) = data(i,j,k)*(scale_used-1.)*weight_avg 
 c     1                 + data (i,j,k)
+
                   data(i,j,k) = data(i,j,k)*(scale_used-1.)
      1                 + data (i,j,k)
 
@@ -532,18 +605,6 @@ c     if conditions are not met, data is unchanged
          enddo
       enddo
 
-c      do k = 1, isound
-c         do j =1,jj
-c            do i =1,ii
-c               mask(i,j) = 0
-c            enddo
-c         enddo
-c         mask(i_r(k), j_r(k)) = 1
-c      enddo
-c      do k = 1,kk
-c         call slv_laplc (data(1,1,k),mask,ii,jj)
-c      enddo
-
 c     *** return modified data array
 
       write(6,*) 'end RAOB step'
@@ -554,14 +615,8 @@ c     *** return modified data array
       return
 
  24   write(6,*)  'raob moisture switch missing!, ignoring raobs'
-      return
 
-c     old notes kept here for reference.
-c     at this time, these have no bearing on the current algorithm
-c     Dan
-c     lam = 600 km
-c     2*lam = 1200km , c = ln(2)*lam**2/4/pi**2 = 25282km**2
-c     denominator = 4*c = 101131.
+      return
 
       end
 
