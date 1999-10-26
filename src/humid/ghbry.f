ef304365eac8f91b636f8598ad71db150e82cc18
Daniel Birkenheuer <daniel.l.birkenheuer@noaa.gov>
1999-10-26 16:58:32 +0000

    updated routine and checked for possible problems PRIMARY PROJECT: Lockheed Missles and Space Systems

--- src/humid/ghbry.f
+++ src/humid/ghbry.f
@@ -29,157 +29,131 @@ cdis
 cdis
 cdis
 cdis
-        subroutine ghbry (i4time,plevel,pb,lt1dat,htby,
-     1                      ii,jj,kk,istatus)
+      subroutine ghbry (i4time,plevel,pb,lt1dat,htby,
+     1     ii,jj,kk,istatus)
+    
+c     routine to return boundary layer depth in meters
+c     currently this is fixed at 100 meters.  logically, this
+c     should be determined by some type of model
+     
+      implicit none
 
-
-c       $log: ghbry.for,v $
-c revision 1.2  1995/09/13  21:35:21  birk
-c added disclaimer to files
-c
-c revision 1.1  1994/04/25  15:12:05  birk
-c initial revision
-c
-
-c       routine to return boundary layer depth in meters
-c       currently this is fixed at 100 meters.  logically, this
-c       should be determined by some type of model
-
-
-        implicit none
-
-c        include 'parmtrs.inc'
-
-c input parameters
-
+c     input parameters
+      
       integer i4time
       integer ii,jj,kk,istatus
       real plevel (kk)
-      real pb (ii,jj)  ! surface station pressure
+      real pb (ii,jj)           ! surface station pressure
       real lt1dat(ii,jj,kk)
       real htby (ii,jj)
+      
+c     dynamic allocation 
+      
+      real n2(ii,jj,kk)         ! brunt-vaisala frequency
+      
+c     internal variables
 
-c dynamic allocation 
-
-        real*4 n2(ii,jj,kk) ! brunt-vaisala frequency
-
-c internal variables
-
-        real*4 x1,x2,y1,y2 ! brackets for zero brunt-vaisala freq. interp
-        real*4 g ! gravity
-        real*4 r ! gas constant of dry air
-        real*4 kkk ! (r/cp) where cp is the specific heat
-        real*4 y1min
-
-        data g / 980.665/   !  cm/s**2  (cgs units)
-        data r / 2.8704e6/  !  erg/g/k  (cgs units)
-        data kkk / 0.2857/    ! (dimensionless)
-
-        logical*4 test  ! to test whether we are above the surface or not
-
-c       note: in the computation of n2 the units of pressure cancel since
-c       the pressure occurs both in the numerator and denominator....therefore
-c       mb are sufficient units for pressure and the relationship
+      integer i,j,k
+      real x1,x2,y1,y2          ! brackets for zero brunt-vaisala freq. interp
+      real g                    ! gravity
+      real r                    ! gas constant of dry air
+      real kkk                  ! (r/cp) where cp is the specific heat
+      save g,r,kkk              ! inserted save to be safe, probably not needed
+      real y1min
+      
+      data g / 980.665/         !  cm/s**2  (cgs units)
+      data r / 2.8704e6/        !  erg/g/k  (cgs units)
+      data kkk / 0.2857/        ! (dimensionless)
+      
+      logical test              ! to test above the surface or not
+      
+c     note: in the computation of n2 the units of pressure cancel since
+c     the pressure occurs both in the numerator and denominator....therefore
+c     mb are sufficient units for pressure and the relationship
 c     1  mb = 1000 cgs units does not have to be applied.
+      
 
-        integer*4 i,j,k
+c     ------------begin exe last revised 10/26/99 db
+      
+      do j = 1,jj
+         do i = 1,ii
+            
+            htby(i,j) = pb(i,j) !put surface pressure in as first guess of
+c     boundary level
+            
+         enddo
+      enddo
 
-        do j = 1,jj
-        do i = 1,ii
+c     section is for better boundary layer analysis
+      
+      do j = 1,jj
+         do i = 1,ii
+            do k = 2,kk-1
+               
+               n2(i,j,k) = (g/lt1dat(i,j,k) )**2 * (plevel(k)/r) *
+     1              (
+     1              kkk*lt1dat(i,j,k)/plevel(k)
+     1              -  ( lt1dat(i,j,k+1)-lt1dat(i,j,k-1) )
+     1              / ( plevel(k+1) - plevel(k-1) )
+     1              )
+               
+               
+            enddo               ! k
+         enddo
+      enddo
+ 
+c     now decide where the "height of the boundary layer" is
 
-        htby(i,j) = pb(i,j)  !put surface pressure in as first guess of
-c               boundary level
+      y1min = 0.0
+      do j = 1,jj
+         do i = 1,ii
+            
+            test = .false.
+            do k = 1,kk
+               
+               if (pb(i,j) .ge. plevel(k) 
+     1              .and. n2(i,j,k) .lt. 0.0) then
+                  test = .true.
+                  x1 = plevel(k)
+                  y1 = n2(i,j,k)
+                  y1min = min(y1,y1min)
+               endif
+               
+               if (test .and. pb(i,j) .ge. plevel(k) 
+     1              .and. n2(i,j,k) .ge. 0.0) then
+                  test = .false.
+                  x2 = plevel(k)
+                  y2 = n2(i,j,k)
+c     bail out of loop here to not affect regions above 1st inversion
+                  go to 111
+               endif
+               
+            enddo
+c     divert code here to not fall into section 111
+            go to 112
+ 111        continue
+            
+            if (y1.ne.y2)   then
+               
+c     interpolate in height space
+               call interp( 0.,y1,y2,log(x1),log(x2),htby(i,j) )
+               htby(i,j) = exp(htby(i,j))
+               
+            endif
+            
+ 112        continue
+            
+         enddo
+ 
+         y1 = 0.0
 
-        enddo
-        enddo
-
-
-c       istatus = 1
-c       if(1.eq.1) then
-c       return
-c       endif
-
-c       code here currently under test march 13, 1992  dan b.
-c       section is for better boundary layer analysis
-
-        do j = 1,jj
-        do i = 1,ii
-        do k = 2,kk-1
-
-        n2(i,j,k) = (g/lt1dat(i,j,k) )**2 * (plevel(k)/r) *
-     1  (
-     1  kkk*lt1dat(i,j,k)/plevel(k)
-     1  -  ( lt1dat(i,j,k+1)-lt1dat(i,j,k-1) )
-     1  / ( plevel(k+1) - plevel(k-1) )
-     1  )
-
-
-        enddo ! k
-        enddo
-        enddo
-
-
-
-
-c        now decide where the "height of the boundary layer" is
-
-
-        y1min = 0.0
-        do j = 1,jj
-        do i = 1,ii
-
-        test = .false.
-        do k = 1,kk
-
-        if (pb(i,j) .ge. plevel(k) .and. n2(i,j,k) .lt. 0.0) then
-        test = .true.
-        x1 = plevel(k)
-        y1 = n2(i,j,k)
-        y1min = min(y1,y1min)
-        endif
-
-        if (test .and. pb(i,j) .ge. plevel(k) .and. n2(i,j,k) .ge. 0.0) 
-     1then
-        test = .false.
-        x2 = plevel(k)
-        y2 = n2(i,j,k)
-c       bail out of loop here to not affect regions above 1st inversion
-        go to 111
-        endif
-
-
-        enddo
-c       divert code here to not fall into section 111
-        go to 112
-111     continue
-
-
-        if (y1.ne.y2)   then
-
-c       interpolate in height space
-                call interp( 0.,y1,y2,log(x1),log(x2),htby(i,j) )
-                htby(i,j) = exp(htby(i,j))
-
-
-        endif
-
-112     continue
-
-        enddo
-
-c       print*, j,y1min,y1
-c       assign y1 zero for monitoring purposes only when using above type
-c       statement
-        y1 = 0.0
-        enddo
-
-c       write out the pbl pressure top
-
-        call gen_bl_file (i4time,htby,ii,jj,istatus)
-
-        if (istatus.eq.0) print*, 'error in gen_bl_file routine'
-
-
-
-        return
-        end
+      enddo
+      
+c     write out the pbl pressure top
+      
+      call gen_bl_file (i4time,htby,ii,jj,istatus)
+      
+      if (istatus.eq.0) print*, 'Error in gen_bl_file routine'
+      
+      return
+      end
