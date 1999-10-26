c080c8d76a80a7185fec346f0839073511c4a062
Daniel Birkenheuer <daniel.l.birkenheuer@noaa.gov>
1999-10-26 17:07:31 +0000

    updated routine, no major revisions PRIMARY PROJECT: Lockheed Missles and Space

--- src/humid/gen_bl_file.f
+++ src/humid/gen_bl_file.f
@@ -29,70 +29,58 @@ cdis
 cdis
 cdis
 cdis
-        subroutine gen_bl_file (i4time,pb,igrid,jgrid,istatus)
+      subroutine gen_bl_file (i4time,pb,igrid,jgrid,istatus)
 
-c       $log: gen_bl_file.for,v $
-c revision 1.3  1995/09/13  21:35:19  birk
-c added disclaimer to files
-c
-c revision 1.2  1994/04/25  17:05:38  birk
-c reactivated this module that writes output for lsx (surface analysis)
-c the current version should be fully portable between platforms.  the
-c file that is written is in ascii
-c
-c revision 1.1  1994/04/25  15:17:46  birk
-c initial revision
-c
-
-c       this routine generates the boundary layer output file determined
-c       for the sh mixing algorithm and subsequently for the retroactive
-c       inclusion in the surface analysis.
-
-c       inputs:
-
-c       i4time
-c       pb ! top of boundary layer pressure (mb)
-
-c       outputs:   none!  this routine generates a file only where
-c       filename (ascii time stamp for first record)
-c       pb in a simple file (binary write)
-
-        implicit none
-
+c     this routine generates the boundary layer output file determined
+c     for the sh mixing algorithm and subsequently for the retroactive
+c     inclusion in the surface analysis.
+      
+c     inputs:
+      
+c     i4time
+c     pb ! top of boundary layer pressure (mb)
+      
+c     outputs:   none!  this routine generates a file only where
+c     filename (ascii time stamp for first record)
+c     pb in a simple file (binary write)
+      
+      implicit none
+      
       integer igrid,jgrid
+      real pb(igrid,jgrid)
+      integer i4time
+      integer istatus, len
+      character*9 filename
+      character*200 pbfile
 
-        real pb(igrid,jgrid)
-        integer i4time
-        integer istatus, len
-        character*9 filename
-        character*200 pbfile
+c     ----- begin exe  last modified 10/26/99 db
+      
+c     set istatus failure mode
+      istatus = 0
 
-
-c       set istatus failure mode
-        istatus = 0
-        call get_directory('lpbl',pbfile,len)
-        pbfile = pbfile(1:len)//'pbl_depth.lpbl'
-
-        call make_fnam_lp (i4time,filename,istatus)
-
-        if (istatus.eq.1) then
-        istatus = 0
-        else
-        print*, 'error in routine make_fnam_lp'
-        istatus = 0
-        return
-        endif
-
-        open (unit=22, file=pbfile, form ='formatted')
-
-        write (22,5) filename,i4time
-        write (22,6) pb
-5       format (1x,a9,i12)
-6       format (1x, 7f10.3)
-
-        close (22)
-
-        istatus = 1
-
-        return
-        end
+      call get_directory('lpbl',pbfile,len)
+      pbfile = pbfile(1:len)//'pbl_depth.lpbl'
+      
+      call make_fnam_lp (i4time,filename,istatus)
+      
+      if (istatus.eq.1) then
+         istatus = 0
+      else
+         print*, 'Error in routine make_fnam_lp'
+         istatus = 0
+         return
+      endif
+      
+      open (unit=22, file=pbfile, form ='formatted')
+      
+      write (22,5) filename,i4time
+      write (22,6) pb
+ 5    format (1x,a9,i12)
+ 6    format (1x, 7f10.3)
+      
+      close (22)
+      
+      istatus = 1
+      
+      return
+      end
