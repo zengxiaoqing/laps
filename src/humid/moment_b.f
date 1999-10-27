83b5ac83aff259ae59e75bc57b075dc01a42e413
Daniel Birkenheuer <daniel.l.birkenheuer@noaa.gov>
1999-10-27 20:43:07 +0000

    added FSL disclaimer to new code

--- src/humid/moment_b.f
+++ src/humid/moment_b.f
@@ -1,3 +1,34 @@
+cdis    forecast systems laboratory
+cdis    noaa/oar/erl/fsl
+cdis    325 broadway
+cdis    boulder, co     80303
+cdis
+cdis    forecast research division
+cdis    local analysis and prediction branch
+cdis    laps
+cdis
+cdis    this software and its documentation are in the public domain and
+cdis    are furnished "as is."  the united states government, its
+cdis    instrumentalities, officers, employees, and agents make no
+cdis    warranty, express or implied, as to the usefulness of the software
+cdis    and documentation for any purpose.  they assume no responsibility
+cdis    (1) for the use of the software and documentation; or (2) to provide
+cdis     technical support to users.
+cdis
+cdis    permission to use, copy, modify, and distribute this software is
+cdis    hereby granted, provided that the entire disclaimer notice appears
+cdis    in all copies.  all modifications to this software must be clearly
+cdis    documented, and are solely the responsibility of the agent making
+cdis    the modifications.  if significant modifications or enhancements
+cdis    are made to this software, the fsl software policy manager
+cdis    (softwaremgr@fsl.noaa.gov) should be notified.
+cdis
+cdis
+cdis
+cdis
+cdis
+cdis
+cdis
       subroutine moment_b(data,n,ave,adev,sdev,var,skew,curt,istatus)
 
 c alternative moment program adapted from numerical recipies not to comflict
