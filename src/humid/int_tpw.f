cd11729593056bc44f26b851a3ab96d16cbc1547
Daniel Birkenheuer <daniel.l.birkenheuer@noaa.gov>
2000-11-21 21:18:55 +0000

    allowed for variable pressure level spacing

--- src/humid/int_tpw.f
+++ src/humid/int_tpw.f
@@ -29,73 +29,71 @@ cdis
 cdis
 cdis
 cdis
-        subroutine  int_tpw(data,kstart,qs,ps,plevel,tpw,ii,jj,kk)
+      subroutine  int_tpw(data,kstart,qs,ps,plevel,tpw,ii,jj,kk)
 
-c       $log: int_tpw.for,v $
-c revision 1.3  1995/09/13  21:35:23  birk
-c added disclaimer to files
-c
-c revision 1.2  1994/07/22  22:02:22  birk
-c made code compatible with lapsparms.inc
-c
-c revision 1.1  1994/04/25  15:01:49  birk
-c initial revision
-c
-
-c       subroutine to integrate the total precipitable water from the
-c       specific humidity data.  consolodating this process, since it is
-c       done numerous times in the code
-
-c       birkenheuer  feb 9 1993
-
-        implicit none
-
-c        include 'lapsparms.for'
-c        include 'parmtrs.inc'
-
-c parameter variables
-
+c     $log: int_tpw.for,v $
+c     revision 1.3  1995/09/13  21:35:23  birk
+c     added disclaimer to files
+c     
+c     revision 1.2  1994/07/22  22:02:22  birk
+c     made code compatible with lapsparms.inc
+c     
+c     revision 1.1  1994/04/25  15:01:49  birk
+c     initial revision
+c     
+      
+c     subroutine to integrate the total precipitable water from the
+c     specific humidity data.  consolodating this process, since it is
+c     done numerous times in the code
+      
+c     birkenheuer  feb 9 1993
+      
+      implicit none
+      
+c     include 'lapsparms.for'
+c     include 'parmtrs.inc'
+      
+c     parameter variables
+      
       integer ii,jj,kk
-        real data(ii,jj,kk)
-        integer kstart (ii,jj)
-        real qs(ii,jj)
-        real ps(ii,jj)
-        real plevel (kk)
-        real tpw (ii,jj)
-
-
-c variables requireing dynamic allocation
-
-c               NONE
-
-c internal variables
-
-        real delta_p
-        integer i,j,k
-
-c       set delta_p
-
-        delta_p = plevel(1)-plevel(2)
-
-c       integrate the tpw field
-
-        do j = 1,jj
-        do i = 1,ii
-c compute the dewpoints
-c integrate
-        tpw(i,j) = 0.0
-        do k = kstart(i,j),kk-1
-        tpw(i,j) = tpw(i,j) + ( data(i,j,k)+data(i,j,k+1) )/2. * delta_p
-        enddo
-        tpw(i,j) = tpw(i,j) *1.e3  !  change to g/kg mb
-        tpw(i,j) = tpw(i,j)
-     1  + ( qs(i,j) + data(i,j,kstart(i,j)) *1.e3 ) /2.
-     1  * (ps(i,j)-plevel(kstart(i,j)) )
-        tpw(i,j) = tpw(i,j) / 100. / 9.8
-
-        enddo
-        enddo
-
-        return
-
-        end
+      real data(ii,jj,kk)
+      integer kstart (ii,jj)
+      real qs(ii,jj)
+      real ps(ii,jj)
+      real plevel (kk)
+      real tpw (ii,jj)
+      
+      
+c     variables requireing dynamic allocation
+      
+c     NONE
+      
+c     internal variables
+      
+      integer i,j,k
+      
+c     integrate the tpw field
+      
+      do j = 1,jj
+         do i = 1,ii
+c     integrate q
+            tpw(i,j) = 0.0
+            do k = kstart(i,j),kk-1
+               tpw(i,j) = tpw(i,j) + ( data(i,j,k)+data(i,j,k+1) )/2. 
+     1              * (plevel(k)-plevel(k+1))
+            enddo
+c     change units of q to g/kg
+            tpw(i,j) = tpw(i,j) *1.e3 
+c     add surface layer (already in g/kg)
+            tpw(i,j) = tpw(i,j)
+     1           + ( qs(i,j) + data(i,j,kstart(i,j)) *1.e3 ) /2.
+     1           * (ps(i,j)-plevel(kstart(i,j)) )
+c     comvert g/kg to cm
+            tpw(i,j) = tpw(i,j) / 100. / 9.8
+            
+         enddo
+      enddo
+      
+      return
+      
+      end
