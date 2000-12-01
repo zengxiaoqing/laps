0af9fec7ae6edf4063299608f728ed814eeec3fa
Daniel Birkenheuer <daniel.l.birkenheuer@noaa.gov>
2000-12-01 16:56:50 +0000

    installed new FSL disclaimer in all humidity software

--- src/humid/lsin.f
+++ src/humid/lsin.f
@@ -1,27 +1,40 @@
-cdis    forecast systems laboratory
-cdis    noaa/oar/erl/fsl
-cdis    325 broadway
-cdis    boulder, co     80303
-cdis
-cdis    forecast research division
-cdis    local analysis and prediction branch
-cdis    laps
-cdis
-cdis    this software and its documentation are in the public domain and
-cdis    are furnished "as is."  the united states government, its
-cdis    instrumentalities, officers, employees, and agents make no
-cdis    warranty, express or implied, as to the usefulness of the software
-cdis    and documentation for any purpose.  they assume no responsibility
-cdis    (1) for the use of the software and documentation; or (2) to provide
-cdis     technical support to users.
-cdis
-cdis    permission to use, copy, modify, and distribute this software is
-cdis    hereby granted, provided that the entire disclaimer notice appears
-cdis    in all copies.  all modifications to this software must be clearly
-cdis    documented, and are solely the responsibility of the agent making
-cdis    the modifications.  if significant modifications or enhancements
-cdis    are made to this software, the fsl software policy manager
+cdis   
+cdis    Open Source License/Disclaimer, Forecast Systems Laboratory
+cdis    NOAA/OAR/FSL, 325 Broadway Boulder, CO 80305
+cdis    
+cdis    This software is distributed under the Open Source Definition,
+cdis    which may be found at http://www.opensource.org/osd.html.
+cdis    
+cdis    In particular, redistribution and use in source and binary forms,
+cdis    with or without modification, are permitted provided that the
+cdis    following conditions are met:
+cdis    
+cdis    - Redistributions of source code must retain this notice, this
+cdis    list of conditions and the following disclaimer.
+cdis    
+cdis    - Redistributions in binary form must provide access to this
+cdis    notice, this list of conditions and the following disclaimer, and
+cdis    the underlying source code.
+cdis    
+cdis    - All modifications to this software must be clearly documented,
+cdis    and are solely the responsibility of the agent making the
+cdis    modifications.
+cdis    
+cdis    - If significant modifications or enhancements are made to this
+cdis    software, the FSL Software Policy Manager
 cdis    (softwaremgr@fsl.noaa.gov) should be notified.
+cdis    
+cdis    THIS SOFTWARE AND ITS DOCUMENTATION ARE IN THE PUBLIC DOMAIN
+cdis    AND ARE FURNISHED "AS IS."  THE AUTHORS, THE UNITED STATES
+cdis    GOVERNMENT, ITS INSTRUMENTALITIES, OFFICERS, EMPLOYEES, AND
+cdis    AGENTS MAKE NO WARRANTY, EXPRESS OR IMPLIED, AS TO THE USEFULNESS
+cdis    OF THE SOFTWARE AND DOCUMENTATION FOR ANY PURPOSE.  THEY ASSUME
+cdis    NO RESPONSIBILITY (1) FOR THE USE OF THE SOFTWARE AND
+cdis    DOCUMENTATION; OR (2) TO PROVIDE TECHNICAL SUPPORT TO USERS.
+cdis   
+cdis cdis
+cdis
+cdis
 cdis
 
       subroutine lsin (i4time,plevel,lt1dat,data,cg,tpw,bias_one,
