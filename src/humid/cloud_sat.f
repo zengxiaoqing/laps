40db6c672feb148beb570721ee3010312bfe4246
Daniel Birkenheuer <daniel.l.birkenheuer@noaa.gov>
2001-03-02 15:43:20 +0000

    Routine consolidates the cloud saturation function (relationship between cloud fraction and specific humidity.  Needed for variational module.
    Project:  GOES, WIAP
    
    Developed: 3/2/2001

---
+++ src/humid/cloud_sat.f
@@ -0,0 +1,72 @@
+cdis   
+cdis    Open Source License/Disclaimer, Forecast Systems Laboratory
+cdis    NOAA/OAR/FSL, 325 Broadway Boulder, CO 80305
+cdis    
+cdis    This software is distributed under the Open Source Definition,
+cdis    which may be found at http://www.opensource.org/osd.html.
+cdis    
+cdis    In particular, redistribution and use in source and binary forms,
+cdis    with or without modification, are permitted provided that the
+cdis    following conditions are met:
+cdis    
+cdis    - Redistributions of source code must retain this notice, this
+cdis    list of conditions and the following disclaimer.
+cdis    
+cdis    - Redistributions in binary form must provide access to this
+cdis    notice, this list of conditions and the following disclaimer, and
+cdis    the underlying source code.
+cdis    
+cdis    - All modifications to this software must be clearly documented,
+cdis    and are solely the responsibility of the agent making the
+cdis    modifications.
+cdis    
+cdis    - If significant modifications or enhancements are made to this
+cdis    software, the FSL Software Policy Manager
+cdis    (softwaremgr@fsl.noaa.gov) should be notified.
+cdis    
+cdis    THIS SOFTWARE AND ITS DOCUMENTATION ARE IN THE PUBLIC DOMAIN
+cdis    AND ARE FURNISHED "AS IS."  THE AUTHORS, THE UNITED STATES
+cdis    GOVERNMENT, ITS INSTRUMENTALITIES, OFFICERS, EMPLOYEES, AND
+cdis    AGENTS MAKE NO WARRANTY, EXPRESS OR IMPLIED, AS TO THE USEFULNESS
+cdis    OF THE SOFTWARE AND DOCUMENTATION FOR ANY PURPOSE.  THEY ASSUME
+cdis    NO RESPONSIBILITY (1) FOR THE USE OF THE SOFTWARE AND
+cdis    DOCUMENTATION; OR (2) TO PROVIDE TECHNICAL SUPPORT TO USERS.
+cdis   
+cdis
+cdis
+cdis
+cdis
+cdis
+cdis
+cdis
+cdis
+cdis
+cdis
+
+      subroutine cloud_sat (cg,sat,data)
+
+c     This routine is the basic cloud saturation model for lq3driver.x
+c     this routine is called from the main routine when performing post
+c     saturation computations.  It is now also used in the variational step
+
+c     Dan Birkenheuer  3/2/2001
+
+      real cg                   ! cloud fraction
+      real sat                  ! saturated ssh at location
+      real data                 ! pre_analyzed ssh at location
+
+
+
+      if(cg .gt. 0.6 .and. cg .lt. 1.0) then !cloudy
+         
+         
+         data = cg*sat + (1.- cg)*data
+         
+      elseif (cg.ge.1.0) then 
+         
+         data = sat
+         
+      endif
+
+      return
+      end
