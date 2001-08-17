72d18d877bf6e550ed238abb0d7d4adc06cd40c6
Steve Albers <Steve.Albers@noaa.gov>
2001-08-17 16:44:31 +0000

    cloud layer refinements from Yi-Chih Huang

--- src/ingest/sao/read_metar_cwb.f
+++ src/ingest/sao/read_metar_cwb.f
@@ -1,41 +1,3 @@
-cdis   
-cdis    Open Source License/Disclaimer, Forecast Systems Laboratory
-cdis    NOAA/OAR/FSL, 325 Broadway Boulder, CO 80305
-cdis    
-cdis    This software is distributed under the Open Source Definition,
-cdis    which may be found at http://www.opensource.org/osd.html.
-cdis    
-cdis    In particular, redistribution and use in source and binary forms,
-cdis    with or without modification, are permitted provided that the
-cdis    following conditions are met:
-cdis    
-cdis    - Redistributions of source code must retain this notice, this
-cdis    list of conditions and the following disclaimer.
-cdis    
-cdis    - Redistributions in binary form must provide access to this
-cdis    notice, this list of conditions and the following disclaimer, and
-cdis    the underlying source code.
-cdis    
-cdis    - All modifications to this software must be clearly documented,
-cdis    and are solely the responsibility of the agent making the
-cdis    modifications.
-cdis    
-cdis    - If significant modifications or enhancements are made to this
-cdis    software, the FSL Software Policy Manager
-cdis    (softwaremgr@fsl.noaa.gov) should be notified.
-cdis    
-cdis    THIS SOFTWARE AND ITS DOCUMENTATION ARE IN THE PUBLIC DOMAIN
-cdis    AND ARE FURNISHED "AS IS."  THE AUTHORS, THE UNITED STATES
-cdis    GOVERNMENT, ITS INSTRUMENTALITIES, OFFICERS, EMPLOYEES, AND
-cdis    AGENTS MAKE NO WARRANTY, EXPRESS OR IMPLIED, AS TO THE USEFULNESS
-cdis    OF THE SOFTWARE AND DOCUMENTATION FOR ANY PURPOSE.  THEY ASSUME
-cdis    NO RESPONSIBILITY (1) FOR THE USE OF THE SOFTWARE AND
-cdis    DOCUMENTATION; OR (2) TO PROVIDE TECHNICAL SUPPORT TO USERS.
-cdis   
-cdis
-cdis
-cdis   
-cdis
       subroutine read_metar_cwb(
      &     filename, maxSkyCover, recNum, altimeter,
      &     autoStationType, dewpoint, dpFromTenths, elevation,
@@ -174,12 +136,13 @@ c      ----------       examing data quality and changing units       ---------
 
          if ( skyLayerBase(i,j) .eq. -9999. )  then
                skyLayerBase(i,j)= badflag
+	       skyCover(i,j)= '   '
          elseif ( skyLayerBase(i,j) .eq. 0. )  then
                skyLayerBase(i,j)= 15.
-         elseif ( skyLayerBase(i,j) .gt. 100. )  then
-               skyLayerBase(i,j)= skyLayerBase(i,j) *300.       ! unit: m
+         elseif ( skyLayerBase(i,j) .eq. 999. )  then
+               skyLayerBase(i,j)= 30000.
          else
-               skyLayerBase(i,j)= skyLayerBase(i,j) *30.
+               skyLayerBase(i,j)= skyLayerBase(i,j) *30.        ! unit: m
          endif
 30    continue
 
