89d371ae1455069b4d71785f5be483361ebc577b
Steve Albers <Steve.Albers@noaa.gov>
2002-02-06 18:54:28 +0000

    count 'n_valid_radars' from 'vrc' radar inputs              set unused vertical levels in 'grid_ra_ref' to 'r_missing_data'

--- src/ingest/radar/mosaic/getrdrvrc.f
+++ src/ingest/radar/mosaic/getrdrvrc.f
@@ -1,6 +1,6 @@
       subroutine get_laps_rdr(nx_l,ny_l,nz_l,z,record,i_ra_count,
      & cfname,c_radar_id,rlat_radar,rlon_radar,rheight_radar,
-     & grid_ra_ref,istatus)
+     & n_valid_radars,grid_ra_ref,istatus)
 
       implicit none
 
@@ -10,11 +10,11 @@
       integer Nx, Ny, nc
       integer imax, jmax, kdim, kmax
       integer ref_fcinv( z, record)
-      integer i,j, i_ra_count
+      integer i,j, i_ra_count, n_valid_radars
       integer n_ref_grids(i_ra_count)
       integer istatus
 
-      real Dx, Dy, La1, Latin1, Latin2, Lo1, LoV, level
+      real Dx, Dy, La1, Latin1, Latin2, Lo1, LoV, level, r_missing_data       
       real ref(nx_l,ny_l,z,record)
       real grid_ra_ref(nx_l, ny_l, nz_l, i_ra_count) 
       real rlat_radar(i_ra_count)
@@ -41,6 +41,10 @@ c
       write(6,*)' Subroutine get_laps_rdr:'
 
       istatus = 0
+ 
+      n_valid_radars = 0
+
+      call get_r_missing_data(r_missing_data,istatus)
 
       do i=1,i_ra_count
 
@@ -59,7 +63,10 @@ c
             return
          else
             print*,'Success reading ',cfname(i)(1:nc)
+            n_valid_radars = n_valid_radars + 1
          endif
+
+         grid_ra_ref(:,:,:,i) = r_missing_data  ! Initialize this 3D radar ref
 
          call move(ref(1,1,1,1),grid_ra_ref(1,1,1,i),nx_l,ny_l)
 
