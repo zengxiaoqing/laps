91b5ce3a06ae7ff88ed9dbb8d623ad55b9d80576
Steve Albers <Steve.Albers@noaa.gov>
2002-02-26 00:05:03 +0000

    New routine to generate cloud soundings from CO2-Slicing produced              measurements of cloud-top pressure & cover (ctp files). In this              mode of operation (mode1) we will analyze these cloud soundings              along with the METAR & PIREP cloud soundings. Not yet functional.

---
+++ src/cloud/insert_co2ctp.f
@@ -0,0 +1,191 @@
+cdis   
+cdis    Open Source License/Disclaimer, Forecast Systems Laboratory
+cdis    NOAA/OAR/FSL, 325 Broadway Boulder, CO 80305
+cdis    
+cdis    This software is distributed under the Open Source Definition,
+cdis    which may be found at http://www.opensource.org/osd.html.
+cdis    
+cdis    In particular, redistribution and use in source and binary forms,
+cdis    with or without modification, are permitted provided that the
+cdis    following conditions are met:
+cdis    
+cdis    - Redistributions of source code must retain this notice, this
+cdis    list of conditions and the following disclaimer.
+cdis    
+cdis    - Redistributions in binary form must provide access to this
+cdis    notice, this list of conditions and the following disclaimer, and
+cdis    the underlying source code.
+cdis    
+cdis    - All modifications to this software must be clearly documented,
+cdis    and are solely the responsibility of the agent making the
+cdis    modifications.
+cdis    
+cdis    - If significant modifications or enhancements are made to this
+cdis    software, the FSL Software Policy Manager
+cdis    (softwaremgr@fsl.noaa.gov) should be notified.
+cdis    
+cdis    THIS SOFTWARE AND ITS DOCUMENTATION ARE IN THE PUBLIC DOMAIN
+cdis    AND ARE FURNISHED "AS IS."  THE AUTHORS, THE UNITED STATES
+cdis    GOVERNMENT, ITS INSTRUMENTALITIES, OFFICERS, EMPLOYEES, AND
+cdis    AGENTS MAKE NO WARRANTY, EXPRESS OR IMPLIED, AS TO THE USEFULNESS
+cdis    OF THE SOFTWARE AND DOCUMENTATION FOR ANY PURPOSE.  THEY ASSUME
+cdis    NO RESPONSIBILITY (1) FOR THE USE OF THE SOFTWARE AND
+cdis    DOCUMENTATION; OR (2) TO PROVIDE TECHNICAL SUPPORT TO USERS.
+cdis   
+cdis
+cdis
+cdis   
+cdis
+c
+c
+
+        subroutine insert_co2ctp(i4time,cld_hts                           ! I
+     1            ,imax,jmax,kmax,r_missing_data,l_use_co2                ! I
+     1            ,default_clear_cover                                    ! I
+     1            ,lat,lon,ix_low,ix_high,iy_low,iy_high,max_co2ctp       ! I
+     1            ,cld_snd,wt_snd,i_snd,j_snd,n_cld_snd,max_cld_snd       ! I/O
+     1            ,istatus)                                               ! O
+
+!       Input
+        real*4 lat(imax,jmax),lon(imax,jmax)
+        real*4 cld_hts(kmax)
+
+!       Arrays for cloud soundings
+        real*4 cld_snd(max_cld_snd,kmax)
+        real*4 wt_snd(max_cld_snd,kmax)
+        integer*4 i_snd(max_cld_snd)
+        integer*4 j_snd(max_cld_snd)
+
+        logical lstat_co2_a(imax,jmax)
+        logical l_use_co2
+
+!       Local
+        real*4 pct_pa(imax,jmax)
+        real*4 lca(imax,jmax)
+        real*4 cldtop_co2_pa_a(imax,jmax)
+        real*4 cloud_frac_co2_a(imax,jmax)
+
+        character*31 ext
+        character var*3,units*10
+        character*125 comment
+
+        write(6,*)' Subroutine insert_c02ctp...'
+
+!       Obtain NESDIS Cloud-top pressure
+        if(l_use_co2)then
+            i4_co2_window = 4000
+
+            write(6,*)' Getting NESDIS Cloud-top pressure'
+            ext = 'ctp'
+            var = 'PCT'
+            ilevel = 0
+            call get_laps_2dgrid(i4time,i4_co2_window,i4time_nearest
+     1                      ,EXT,var       
+     1                      ,units,comment,imax,jmax,pct_pa,ilevel
+     1                      ,istat_pct)       
+            if(abs(istat_pct) .ne. 1)then
+                write(6,*)' Note: cannot read NESDIS Cloud-top pressure'       
+            endif
+
+!           Obtain NESDIS Cloud-fraction
+            write(6,*)' Getting NESDIS Cloud-fraction'
+            ext = 'ctp'
+            var = 'LCA'
+            ilevel = 0
+            call get_laps_2dgrid(i4time,i4_co2_window,i4time_nearest
+     1                          ,EXT,var
+     1                          ,units,comment,imax,jmax,lca,ilevel
+     1                          ,istat_lca)       
+            if(abs(istat_lca) .ne. 1)then
+                write(6,*)' Note: cannot read NESDIS Cloud-fraction'
+            endif
+
+        endif ! l_use_co2
+
+!       Place CO2-Slicing Cloud-top pressure in sparse array
+
+        cloud_frac_co2_a = r_missing_data
+        cldtop_co2_pa_a  = r_missing_data
+        lstat_co2_a = .false.
+        icount = 0
+
+        if(abs(istat_pct) .eq. 1 .and. abs(istat_lca) .eq. 1 
+     1                           .and. l_use_co2                )then
+            write(6,*)' Extracting CO2-Slicing info from NESDIS data'
+            do j = 1,jmax
+            do i = 1,imax
+!               Test for partial cloudiness
+                if(lca(i,j) .gt. 0. .and. lca(i,j) .lt. 1.0)then ! use co2 data
+                    if(pct_pa(i,j) .lt. 100000.)then
+                        icount = icount + 1
+                        cloud_frac_co2_a(i,j) = lca(i,j)
+                        cldtop_co2_pa_a(i,j) = pct_pa(i,j) 
+                        lstat_co2_a(i,j) = .true.
+                    endif
+                endif
+            enddo ! i
+            enddo ! j
+
+            istat_co2 = 0 ! for now
+        else
+            istat_co2 = 0
+        endif
+
+!       Add to cloud sounding arrays
+
+        n_cloud = 0
+
+        do j = 1,jmax
+        do i = 1,imax
+            if(lstat_co2_a(i,j))then ! Create this cloud sounding
+!               ctop_m = height_of_pressure(cldtop_co2_pa_a(i,j))
+                cbase_m = ctop_m - 500.
+
+                cbuf_high = ctop_m + 500.
+
+                cover_rpt = cloud_frac_co2_a(i,j)
+
+                if(n_cloud .le. 10)then
+                    write(6,*)' Working with CO2 cloud top:'
+     1                       ,nint(cbase_m),nint(ctop_m),cover_rpt
+                endif
+
+                n_cloud = n_cloud + 1
+
+                do k=1,kmax
+                    cover = cover_rpt
+
+!                   Fill in Cloud Layer
+                    if(cld_hts(k) .ge. cbase_m .and.
+     1                 cld_hts(k) .lt. ctop_m                  )then
+                        call spread2(cld_snd,wt_snd,i_snd,j_snd
+     1                              ,n_cld_snd,max_cld_snd
+     1                                  ,nk,i,j,k,cover,1.)
+                        if(n_cloud .le. 10)then
+                            write(6,*)' Fill in k = ',k,cover,cld_hts(k)
+                        endif
+                    endif
+
+                    cover = default_clear_cover
+
+!                   Fill in clear buffer above cloud layer
+                    if(cld_hts(k) .ge. ctop_m .and.
+     1                 cld_hts(k) .lt. cbuf_high               )then      
+                        call spread2(cld_snd,wt_snd,i_snd,j_snd
+     1                              ,n_cld_snd,max_cld_snd
+     1                              ,kmax,i,j,k,cover,1.)
+                        if(n_cloud .le. 10)then
+                            write(6,*)' Fill in k = ',k,cover,cld_hts(k)       
+                        endif
+                    endif
+
+                enddo ! K cld_hts
+
+            endif ! CO2 measurement at this grid point
+        enddo ! i
+        enddo ! j
+
+        write(6,*)' Number of valid CO2-Slicing soundings = ',n_cloud
+
+        return
+        end
