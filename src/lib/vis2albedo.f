232c7d46d6db4d6a8369885d2f9a6acdf9476b74
Steve Albers <Steve.Albers@noaa.gov>
2002-03-06 19:56:46 +0000

    tweaked satellite albedos to be brighter at the high end

--- src/lib/vis2albedo.f
+++ src/lib/vis2albedo.f
@@ -119,7 +119,8 @@ c             end if
                    endif
 
 !                  Additional stretch
-                   call stretch2(0.0,1.0,.09,1.0,albedo_out(i,j))
+!                  call stretch2(0.0,1.0,.09,1.0,albedo_out(i,j))
+                   call stretch2(0.0,1.0,.09,1.1,albedo_out(i,j))
 c                                                               excesses
 c Accumulate extrema
 c
