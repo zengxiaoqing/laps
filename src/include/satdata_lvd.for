28e68fdffa73eb9025984c16056be652efdca093
smart <>
2003-03-30 17:08:48 +0000

    Modified the structures for gmssat/hko satellite data ingest. Simplified array initialization (ie., removed loops).

--- src/include/satdata_lvd.for
+++ src/include/satdata_lvd.for
@@ -8,15 +8,6 @@
       C_SAT_TYPES = '   '
       C_CHANNEL_TYPES = '   '
 
-c     do k=1,maxsat
-c     do j=1,maxtype
-c        C_SAT_TYPES(j,k)='   '
-c     do i=1,maxchannel
-c        C_CHANNEL_TYPES(i,j,k)='   '
-c     enddo
-c     enddo
-c     enddo
-
 c first satellite (goes08)
       C_SAT_TYPES(1,1)='gvr'
       C_SAT_TYPES(2,1)='wfo'
@@ -31,6 +22,7 @@ c third satllite (goes10)
       C_SAT_TYPES(2,3)='wfo'
 
 c forth satellite (gms)
+      C_SAT_TYPES(2,4)='hko'
       C_SAT_TYPES(3,4)='twn'
       C_SAT_TYPES(4,4)='gwc'
 c fifth satellite (goes12)
@@ -106,12 +98,17 @@ c     format type 4 (gwc)
       C_CHANNEL_TYPES(1,4,4)='vis'
       C_CHANNEL_TYPES(3,4,4)='wvp'
       C_CHANNEL_TYPES(4,4,4)='11u'
-      C_CHANNEL_TYPES(5,4,4)='12u'  !<-- end 4th type
+      C_CHANNEL_TYPES(5,4,4)='12u'  
+c     format type 2 (hko):  for HKO (JS  and PW Li 3-20-03)
+      C_CHANNEL_TYPES(1,2,4)='vis'
+      C_CHANNEL_TYPES(3,2,4)='wv '  !water vapor
+      C_CHANNEL_TYPES(4,2,4)='11u'  !ir1 
+      C_CHANNEL_TYPES(5,2,4)='12u' 
 c     format type 3 (twn):  for taiwan (JS  and BS Wang 6-7-01)
       C_CHANNEL_TYPES(1,3,4)='vis'
       C_CHANNEL_TYPES(3,3,4)='wv '  !water vapor
       C_CHANNEL_TYPES(4,3,4)='11u'  !ir1 
-      C_CHANNEL_TYPES(5,3,4)='12u'  !<-- end 4th type ... not known yet
+      C_CHANNEL_TYPES(5,3,4)='12u'  !<-- end 4th type 
 
 c goes12 (fifth satellite type)
 c     format type 1 (gvr)
@@ -119,7 +116,8 @@ c     format type 1 (gvr)
       C_CHANNEL_TYPES(2,1,5)='4u '
       C_CHANNEL_TYPES(3,1,5)='wv '
       C_CHANNEL_TYPES(4,1,5)='11u'
-      C_CHANNEL_TYPES(5,1,5)='12u'
+      C_CHANNEL_TYPES(5,1,5)='   '  !doesn't appear to be 12u for goes12
+      C_CHANNEL_TYPES(6,1,5)='13u'
 
 c     format type 2 (wfo)
       C_CHANNEL_TYPES(1,2,5)='vis'
@@ -154,18 +152,6 @@ c-----
       I_NSCYCLES=0
       I_NSINCS=0
 
-c     do j=1,maxsat
-c     do i=1,maxtype
-c        R_LATIN(i,j)=0.0
-c        R_LAP(i,j)=0.0
-c        R_LOV(i,j)=0.0
-c        I_EWCYCLES(i,j)=0
-c        I_EWINCS(i,j)=0
-c        I_NSCYCLES(i,j)=0
-c        I_NSINCS(i,j)=0
-c     enddo
-c     enddo
-
 c these values come from the netcdf files for type gvr (GVAR)
 c and satellites goes08 and goes10.  We'll also need them for
 c GOES12 and GOES09
@@ -190,46 +176,61 @@ c type gvr and gwc for goes08 and goes10. (Soon add goes09 and goes12)
 c                                          but only for gvr [no gwc])
       I_EWCYCLES(1,1)=2
       I_EWCYCLES(1,3)=2
+      I_EWCYCLES(1,5)=2
 
       I_EWCYCLES(4,1)=2
       I_EWCYCLES(4,3)=2
+      I_EWCYCLES(4,5)=2
 
       I_EWINCS(1,1)=3068
       I_EWINCS(1,3)=3025
+      I_EWINCS(1,5)=3096
       I_EWINCS(4,1)=3068
       I_EWINCS(4,3)=3068
+      I_EWINCS(4,5)=3096
+
 
       I_NSCYCLES(1,1)=4
       I_NSCYCLES(1,3)=4
+      I_NSCYCLES(1,5)=4
+
       I_NSCYCLES(4,1)=4
       I_NSCYCLES(4,3)=4
+      I_NSCYCLES(4,5)=4
 
       I_NSINCS(1,1)=3487
       I_NSINCS(1,3)=3299
+      I_NSINCS(1,5)=2997
       I_NSINCS(4,1)=3487
       I_NSINCS(4,3)=2795
+      I_NSINCS(4,5)=2997
 
       I_NWLINE_VIS(1,1)=3084
       I_NWLINE_VIS(1,3)=3060
+      I_NWLINE_VIS(1,5)=3080
       I_NWLINE_VIS(4,1)=2984
  
       I_NWLINE_IR(1,1)=3080
       I_NWLINE_IR(1,3)=3056
+      I_NWLINE_IR(1,5)=3080
       I_NWLINE_IR(4,1)=2984
 
       I_NWLINE_WV(1,1)=3080
       I_NWLINE_WV(1,3)=3056
+      I_NWLINE_WV(1,5)=3080
       I_NWLINE_WV(4,1)=2984
 c ------------------------------
       I_NWPIX_VIS(1,1)=9050
       I_NWPIX_VIS(1,3)=10696
+      I_NWPIX_VIS(1,5)=9050
       I_NWPIX_VIS(4,1)=9248
 
       I_NWPIX_IR(1,1)=9050
       I_NWPIX_IR(1,3)=10696
+      I_NWPIX_IR(1,5)=10696
       I_NWPIX_IR(4,1)=9248
 
       I_NWPIX_WV(1,1)=9050
-      I_NWPIX_WV(1,1)=13500
-      I_NWPIX_WV(1,1)=10696
-      I_NWPIX_WV(1,1)=9248
+      I_NWPIX_WV(1,3)=13500
+      I_NWPIX_WV(1,5)=9050
+      I_NWPIX_WV(4,1)=9248
