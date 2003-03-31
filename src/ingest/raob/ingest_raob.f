f8665b124dba77e04af4b762e942d804718c7fc8
Linda Wharton <Linda.Wharton@noaa.gov>
2003-03-31 18:52:27 +0000

    Added ingest of noaaport raob data to RSA. LW 3-31-03

--- src/ingest/raob/ingest_raob.f
+++ src/ingest/raob/ingest_raob.f
@@ -212,6 +212,11 @@
      1                ,filename_in,istatus)
 
               elseif(c8_raob_format(1:3) .eq. 'RSA')then
+                  call get_raob_data   (i4time_sys,ilaps_cycle_time
+     1                ,NX_L,NY_L
+     1                ,i4time_raob_earliest,i4time_raob_latest
+     1                ,filename_in,istatus)
+
 !                 call get_raob_data_rtamps(i4time_sys,ilaps_cycle_time       
 !    1                ,NX_L,NY_L
 !    1                ,i4time_raob_earliest,i4time_raob_latest
