cd291eace5721e256122797aef4f8e24aac94f21
smart <>
2003-05-06 12:50:29 +0000

    Increased max_files from 500 to 800.

--- src/ingest/satellite/lvd/lvd_driver_sub.f
+++ src/ingest/satellite/lvd/lvd_driver_sub.f
@@ -82,7 +82,7 @@ c                                     used for variable in include 'satellite_co
       integer   n_lvd_fields_max
       parameter (n_lvd_fields_max = 14)
       integer   max_files
-      parameter (max_files=500)
+      parameter (max_files=800)
  
       integer   len_lvd
 
