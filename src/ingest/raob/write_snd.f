2302f4525a37b0e4a440bc36e9bfc92d991fecbd
Steve Albers <Steve.Albers@noaa.gov>
2003-05-30 17:18:36 +0000

    increased 'c_line' to 150 characters              call 's_len2' to save space when character lines are written

--- src/ingest/raob/write_snd.f
+++ src/ingest/raob/write_snd.f
@@ -24,7 +24,7 @@
       integer iwmostanum(maxsnd),nlvl(maxsnd)
       real stalat(maxsnd,maxlvl),stalon(maxsnd,maxlvl),staelev(maxsnd)       
       character c5_staid(maxsnd)*5,a9time_ob(maxsnd,maxlvl)*9
-     1         ,c8_obstype(maxsnd)*8,c_line*132
+     1         ,c8_obstype(maxsnd)*8,c_line*150
 
       real height_m(maxsnd,maxlvl)
       real pressure_pa(maxsnd,maxlvl)
@@ -59,7 +59,9 @@
      1              ,temp_c(isnd,lvl)
      1              ,dewpoint_c(isnd,lvl)
      1              ,dir_deg(isnd,lvl),spd_mps(isnd,lvl)
-          write(lun_out,521)c_line
+
+          call s_len2(c_line,len_line)
+          write(lun_out,521)c_line(1:len_line)
   521     format(a)
 
           if(isnd .le. 100)then
@@ -69,7 +71,8 @@
      1              ,dir_deg(isnd,lvl),spd_mps(isnd,lvl)
      1              ,a9time_ob(isnd,lvl)
      1              ,stalat(isnd,lvl),stalon(isnd,lvl)
-              write(6,521)c_line
+              call s_len2(c_line,len_line)
+              write(6,521)c_line(1:len_line)
           endif
 
         enddo ! lvl
