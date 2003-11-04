be6633a0089424ccb3306d480c2e48a855bf4754
Steve Albers <Steve.Albers@noaa.gov>
2003-11-04 16:58:30 +0000

    turned off call to 'get_raob_data' for RSA-RTAMPS since we will              likely be writing a new routine for this - 'get_raob_data_rtamps'.

--- src/ingest/raob/ingest_raob.f
+++ src/ingest/raob/ingest_raob.f
@@ -217,10 +217,13 @@
               elseif(c8_raob_format(1:3) .eq. 'RSA')then
                   call open_ext(11,i4time_sys,'snd',istatus)
 
-                  call get_raob_data   (i4time_sys,ilaps_cycle_time
-     1                ,NX_L,NY_L
-     1                ,i4time_raob_earliest,i4time_raob_latest
-     1                ,filename_in,istatus)
+!                 call get_raob_data   (i4time_sys,ilaps_cycle_time
+!    1                ,NX_L,NY_L
+!    1                ,i4time_raob_earliest,i4time_raob_latest
+!    1                ,filename_in,istatus)
+
+                  write(6,*)
+     1                ' Subroutine get_raob_data_rtamps not yet written'       
 
 !                 call get_raob_data_rtamps(i4time_sys,ilaps_cycle_time       
 !    1                ,NX_L,NY_L
