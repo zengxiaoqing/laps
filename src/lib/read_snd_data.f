0fc13732404f6c4f87139edb6cf4d6d509d75b35
Steve Albers <Steve.Albers@noaa.gov>
2003-12-01 18:47:27 +0000

    added 'read_snd_data' return arguments for t,td to make this              routine more generic
                 moved subroutine 'read_sfc_snd' into this file - this version is
                 updated for the new argument list in 'read_snd_data'
    
                 added include of 'read_sfc.inc' for surface declarations

--- src/lib/read_snd_data.f
+++ src/lib/read_snd_data.f
@@ -3,14 +3,16 @@
      1                         ,MAX_PR,MAX_PR_LEVELS                   ! I
      1                         ,lat,lon,imax,jmax,kmax                 ! I
      1                         ,heights_3d                             ! I
+     1                         ,mode                                   ! I
      1                         ,n_profiles                             ! I/O
      1                         ,nlevels_obs_pr,lat_pr,lon_pr,elev_pr   ! O
      1                         ,c5_name,i4time_ob_pr,obstype           ! O
      1                         ,ob_pr_ht_obs,ob_pr_pr_obs              ! O
      1                         ,ob_pr_u_obs,ob_pr_v_obs                ! O
+     1                         ,ob_pr_t_obs,ob_pr_td_obs               ! O
      1                         ,istatus)                               ! O
 
-cdoc    Returns sounding data from the SND file
+cdoc    Returns sounding wind data from the SND file
 
 !       Profile Stuff
         real lat_pr(MAX_PR)
@@ -23,6 +25,8 @@ cdoc    Returns sounding data from the SND file
         real ob_pr_sp_obs(MAX_PR_LEVELS)                               ! L
         real ob_pr_u_obs(MAX_PR,MAX_PR_LEVELS)                         ! O
         real ob_pr_v_obs(MAX_PR,MAX_PR_LEVELS)                         ! O
+        real ob_pr_t_obs(MAX_PR,MAX_PR_LEVELS)                         ! O
+        real ob_pr_td_obs(MAX_PR,MAX_PR_LEVELS)                        ! O
 
         integer i4time_ob_pr(MAX_PR)
         integer nlevels_obs_pr(MAX_PR)
@@ -35,6 +39,18 @@ cdoc    Returns sounding data from the SND file
         real*4 lat(imax,jmax)
         real*4 lon(imax,jmax)
         real*4 heights_3d(imax,jmax,kmax)
+
+        logical l_good_level
+
+        IPR_TIMES_LVLS = MAX_PR*MAX_PR_LEVELS
+
+        write(6,*)' Subroutine read_snd_data: ',MAX_PR,MAX_PR_LEVELS
+     1                                         ,IPR_TIMES_LVLS
+
+        if(IPR_TIMES_LVLS .gt. 1000000)then
+            write(6,*)' Warning, large allocation for IPR_TIMES_LVLS' 
+     1               ,IPR_TIMES_LVLS
+        endif
 
         istatus = 0
 
@@ -111,9 +127,17 @@ cdoc    Returns sounding data from the SND file
               endif
           endif
 
- 505      if(ht_in .ne. r_missing_data .and.
-     1       di_in .ne. r_missing_data .and.
-     1       sp_in .ne. r_missing_data          )then           
+ 505      if(mode .eq. 1 .or. mode .eq. 2)then   ! key good levels to wind data
+              if(ht_in .ne. r_missing_data .and.
+     1           di_in .ne. r_missing_data .and.
+     1           sp_in .ne. r_missing_data          )then           
+                  l_good_level = .true.
+              else
+                  l_good_level = .false.
+              endif
+          endif ! mode
+
+          if(l_good_level)then           
 
               n_good_levels = n_good_levels + 1
 
@@ -147,6 +171,9 @@ cdoc    Returns sounding data from the SND file
 !    1        ,ob_snd_ht_obs(i_pr,level)
 !    1        ,ob_snd_t_obs(i_pr,level)
 !311          format(1x,i6,i4,5f8.1)
+
+              ob_pr_t_obs(i_pr,n_good_levels)  = t_in
+              ob_pr_td_obs(i_pr,n_good_levels) = td_in
 
           endif ! Good data at the level
 
@@ -355,3 +382,225 @@ c
 
       return
       end
+
+	subroutine read_sfc_snd(i4time,atime_s,n_obs_g,n_obs_b, ! regular SND
+     &       obstime,wmoid,stations,provider,wx_s,reptype,autostntype,
+     &       lat_s,lon_s,elev_s,t_s,td_s,rh_s,dd_s,ff_s,ddg_s,ffg_s,
+     &       alt_s,pstn_s,pmsl_s,delpch,delp,vis_s,solar_s,sfct,sfcm,
+     &       pcp1,pcp3,pcp6,pcp24,snow,kloud_s,max24t,min24t,t_ea,
+     &       td_ea,rh_ea,dd_ea,ff_ea,alt_ea,p_ea,vis_ea,solar_ea,
+     &       sfct_ea,sfcm_ea,pcp_ea,snow_ea,store_amt,store_hgt,maxsta,       
+     &       istatus)
+
+        include 'constants.inc'
+
+        include 'read_sfc.inc'
+
+!       Declarations for 'read_snd_data' call
+        integer MAX_PR,MAX_PR_LEVELS
+        parameter (MAX_PR = 100)
+        parameter (MAX_PR_LEVELS = 100)
+
+        real lat_pr(MAX_PR)
+        real lon_pr(MAX_PR)
+        real elev_pr(MAX_PR)
+
+        real ob_pr_ht_obs(MAX_PR,MAX_PR_LEVELS)                        ! O
+        real ob_pr_pr_obs(MAX_PR,MAX_PR_LEVELS)                        ! O
+        real ob_pr_u_obs(MAX_PR,MAX_PR_LEVELS)                         ! O
+        real ob_pr_v_obs(MAX_PR,MAX_PR_LEVELS)                         ! O
+        real ob_pr_t_obs(MAX_PR,MAX_PR_LEVELS)                         ! O
+        real ob_pr_td_obs(MAX_PR,MAX_PR_LEVELS)                        ! O
+
+        integer i4time_ob_pr(MAX_PR)
+        integer nlevels_obs_pr(MAX_PR)
+
+        character*5 c5_name(MAX_PR)
+        character*8 obstype(MAX_PR)
+        character*9 a9time_ob
+
+!       Other declarations
+        logical l_good_snd
+        character*3 ext
+
+        write(6,*)' Subroutine read_sfc_snd...'
+
+        call get_r_missing_data(r_missing_data,istatus)
+        call get_sfc_badflag(badflag,istatus)
+
+        n_good_snd = 0
+        ext = 'snd'
+        lun = 44
+        mode = 2
+
+        call read_snd_data(lun,i4time,ext                              ! I
+     1                         ,MAX_PR,MAX_PR_LEVELS                   ! I
+     1                         ,lat,lon,imax,jmax,kmax                 ! I
+     1                         ,heights_3d                             ! I
+     1                         ,mode                                   ! I
+     1                         ,n_profiles                             ! I/O
+     1                         ,nlevels_obs_pr,lat_pr,lon_pr,elev_pr   ! O
+     1                         ,c5_name,i4time_ob_pr,obstype           ! O
+     1                         ,ob_pr_ht_obs,ob_pr_pr_obs              ! O
+     1                         ,ob_pr_u_obs,ob_pr_v_obs                ! O
+     1                         ,ob_pr_t_obs,ob_pr_td_obs               ! O
+     1                         ,istatus)                               ! O
+        if(istatus .ne. 1)then
+            write(6,*)' Bad istatus from read_snd_data'
+            go to 999
+        endif
+
+        if(n_profiles .eq. 0)then
+            write(6,*)' No profiles available via read_snd_data'
+            go to 999
+        endif
+
+!       Append sounding obs to surface arrays
+        do i_pr = 1,n_profiles
+            l_good_snd = .false.
+
+            if(nlevels_obs_pr(i_pr) .gt. 0)then
+                rlevel_best_wind_diff = 99999.
+                rlevel_best_wind      = r_missing_data
+                ob_best_wind          = r_missing_data
+                ilevel_best_wind      = 0
+
+                rlevel_best_temp_diff = 99999.
+                rlevel_best_temp      = r_missing_data
+                ob_best_temp          = r_missing_data
+                ob_best_dwpt          = r_missing_data
+                ilevel_best_temp      = 0
+
+                do il = 1,nlevels_obs_pr(i_pr)
+                    rlevel_temp_diff = abs( 
+     1                   (ob_pr_ht_obs(i_pr,il)-elev_pr(i_pr)) - 2.0 )
+
+                    rlevel_wind_diff = abs( 
+     1                   (ob_pr_ht_obs(i_pr,il)-elev_pr(i_pr)) - 10.0 )
+
+                    if(rlevel_temp_diff .lt. rlevel_best_temp_diff)then
+                      if(ob_pr_t_obs(i_pr,il) .ne. r_missing_data)then
+                        rlevel_best_temp_diff = rlevel_temp_diff
+                        ilevel_best_temp      = il
+                        rlevel_best_temp      = ob_pr_ht_obs(i_pr,il)
+                      endif
+                    endif
+
+                    if(rlevel_wind_diff .lt. rlevel_best_wind_diff)then       
+                      if(ob_pr_u_obs(i_pr,il) .ne. r_missing_data
+     1             .and. ob_pr_v_obs(i_pr,il) .ne. r_missing_data)then
+                        rlevel_best_wind_diff = rlevel_wind_diff
+                        rlevel_best_wind      = ob_pr_ht_obs(i_pr,il)
+                        ilevel_best_wind      = il
+                      endif
+                    endif
+
+                enddo ! il
+
+                if(rlevel_best_wind_diff .le. 6.0 .or. 
+     1             rlevel_best_temp_diff .le. 2.0)then
+                    write(6,*)' Good Sounding Elev     ',i_pr
+     1                       ,c5_name(i_pr)
+     1                       ,rlevel_best_temp,rlevel_best_temp_diff       
+     1                       ,rlevel_best_wind,rlevel_best_wind_diff
+                    l_good_snd = .true.
+                    n_good_snd = n_good_snd + 1
+                elseif(i_pr .le. 100)then
+                    write(6,*)' Unusable Sounding Elev ',i_pr
+     1                       ,c5_name(i_pr)
+     1                       ,rlevel_best_temp,rlevel_best_temp_diff       
+     1                       ,rlevel_best_wind,rlevel_best_wind_diff
+                endif
+            endif
+
+!           if(.false.)then
+            if(l_good_snd)then ! This ob is good enough to append
+                n_obs_g = n_obs_g + 1
+                n_obs_b = n_obs_b + 1
+
+                lat_s(n_obs_b) = lat_pr(i_pr)
+                lon_s(n_obs_b) = lon_pr(i_pr)
+                elev_s(n_obs_b) = elev_pr(i_pr)
+                stations(n_obs_b) = c5_name(i_pr)
+                reptype(n_obs_b) = obstype(i_pr)
+
+!               Initialize values to badflag
+                t_s(n_obs_b) = badflag
+                td_s(n_obs_b) = badflag
+                rh_s(n_obs_b) = badflag
+                dd_s(n_obs_b) = badflag
+                ff_s(n_obs_b) = badflag
+                ddg_s(n_obs_b) = badflag
+                ffg_s(n_obs_b) = badflag
+                alt_s(n_obs_b) = badflag
+                pstn_s(n_obs_b) = badflag
+                pmsl_s(n_obs_b) = badflag
+                delpch(n_obs_b) = badflag
+                delp(n_obs_b) = badflag
+                vis_s(n_obs_b) = badflag
+                solar_s(n_obs_b) = badflag
+                sfct(n_obs_b) = badflag
+                sfcm(n_obs_b) = badflag
+                pcp1(n_obs_b) = badflag
+                pcp3(n_obs_b) = badflag
+                pcp6(n_obs_b) = badflag
+                pcp24(n_obs_b) = badflag
+                snow(n_obs_b) = badflag
+                kloud_s(n_obs_b) = badflag
+                max24t(n_obs_b) = badflag
+                min24t(n_obs_b) = badflag
+
+                t_ea(n_obs_b) = badflag
+                td_ea(n_obs_b) = badflag
+                rh_ea(n_obs_b) = badflag
+                dd_ea(n_obs_b) = badflag
+                ff_ea(n_obs_b) = badflag
+                alt_ea(n_obs_b) = badflag
+                p_ea(n_obs_b) = badflag
+                vis_ea(n_obs_b) = badflag
+                solar_ea(n_obs_b) = badflag
+                sfct_ea(n_obs_b) = badflag
+                sfcm_ea(n_obs_b) = badflag
+                pcp_ea(n_obs_b)  = badflag
+                snow_ea(n_obs_b) = badflag
+                store_amt(n_obs_b,:) = '     '
+                store_hgt(n_obs_b,:) = badflag
+
+!               Add temperature ob into arrays 
+                if(ob_pr_t_obs(i_pr,ilevel_best_temp) 
+     1                                         .ne. r_missing_data)then
+                    t_ob_c = ob_pr_t_obs(i_pr,ilevel_best_temp)     
+                    t_s(n_obs_b) = c_to_f(t_ob_c)
+                    write(6,*)' Good temp ob',t_s(n_obs_b)
+                endif      
+
+!               Add dewpoint ob into arrays 
+                if(ob_pr_td_obs(i_pr,ilevel_best_temp) 
+     1                                         .ne. r_missing_data)then       
+                    td_ob_c = ob_pr_td_obs(i_pr,ilevel_best_temp)     
+                    td_s(n_obs_b) = c_to_f(td_ob_c)
+                    write(6,*)' Good dwpt ob',td_s(n_obs_b)
+                endif      
+
+!               Add wind ob into arrays
+                if(ob_pr_u_obs(i_pr,ilevel_best_wind).ne.r_missing_data      
+     1       .and. ob_pr_v_obs(i_pr,ilevel_best_wind).ne.r_missing_data
+     1                                                         )then
+                    call uv_to_disp(ob_pr_u_obs(i_pr,ilevel_best_wind)
+     1                             ,ob_pr_v_obs(i_pr,ilevel_best_wind)
+     1                             ,dd_s(n_obs_b)
+     1                             ,speed_ms )
+                    ff_s(n_obs_b) = speed_ms / mspkt    
+                    write(6,*)' Good wind ob',dd_s(n_obs_b)
+     1                                       ,ff_s(n_obs_b)      
+                endif
+
+            endif ! good sounding
+
+        enddo ! i_pr
+
+ 999	write(6,*)' # of good soundings = ',n_good_snd,n_obs_g,n_obs_b       
+
+        return
+        end
+
