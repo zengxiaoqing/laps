deb53c3424ee8956103bdc1b90591b2fd6a47636
Steve Albers <Steve.Albers@noaa.gov>
2004-01-02 18:02:07 +0000

    pencilled in call to 'get_radiometer_data' and added a namelist              parameter for 'path_to_raw_radiometer'

--- src/ingest/raob/ingest_sounding.f
+++ src/ingest/raob/ingest_sounding.f
@@ -7,13 +7,17 @@
        character*150 path_to_raw_drpsnd
        character*150 path_to_raw_satsnd
        character*200 path_to_raw_tower
+       character*200 path_to_raw_radiometer
        character*8 c8_raob_format, c8_project
 
        call get_laps_config('nest7grid',istatus)
 
        call get_snd_parms(path_to_raw_raob,path_to_local_raob
      1                   ,path_to_raw_drpsnd
-     1                   ,path_to_raw_satsnd,path_to_raw_tower,istatus)       
+     1                   ,path_to_raw_satsnd
+     1                   ,path_to_raw_tower
+     1                   ,path_to_raw_radiometer
+     1                   ,istatus)       
        if(istatus .ne. 1)goto999
 
        call get_c8_project(c8_project,istatus)
@@ -21,6 +25,15 @@
           write (6,*) 'Error getting c8_project'
           go to 999
        endif
+
+       call get_grid_dim_xy(ni,nj,istatus)
+       if (istatus .ne. 1) then
+           write (6,*) 'Error getting horizontal domain dimensions'
+           stop
+       endif
+
+       call get_laps_cycle_time(laps_cycle_time,istatus)
+       if(istatus .ne. 1)stop
 
        lun_out = 21
 
@@ -63,16 +76,7 @@
            itime_before = 900
            itime_after = 900
 
-           call get_grid_dim_xy(ni,nj,istatus)
-           if (istatus .ne. 1) then
-               write (6,*) 'Error getting horizontal domain dimensions'
-               stop
-           endif
-
            call get_max_stations(maxsta, istatus)
-           if(istatus .ne. 1)stop
-
-           call get_laps_cycle_time(laps_cycle_time,istatus)
            if(istatus .ne. 1)stop
 
            call tower_driver_sub(ni,nj,lun_out
@@ -89,6 +93,11 @@
        write(6,*)' Call ingest_satsnd'
        call ingest_satsnd(path_to_raw_satsnd)
 
+       write(6,*)
+       write(6,*)' Call get_radiometer_data (under construction)'
+!      call get_radiometer_data(path_to_raw_radiometer,laps_cycle_time       
+!     1                        ,ni,nj,lun_out)
+
        close(lun_out) ! Output SND file
 
  999   end
@@ -97,17 +106,20 @@
        subroutine get_snd_parms(path_to_raw_raob,path_to_local_raob
      1                         ,path_to_raw_drpsnd
      1                         ,path_to_raw_satsnd
-     1                         ,path_to_raw_tower,istatus)
+     1                         ,path_to_raw_tower
+     1                         ,path_to_raw_radiometer
+     1                         ,istatus)
 
        character*150 path_to_raw_raob,path_to_local_raob
      1              ,path_to_raw_drpsnd
      1              ,path_to_raw_satsnd
+     1              ,path_to_raw_radiometer
 
        character*200 path_to_raw_tower
 
        namelist /snd_nl/ path_to_raw_raob,path_to_local_raob
      1                  ,path_to_raw_satsnd,path_to_raw_tower
-     1                  ,path_to_raw_drpsnd
+     1                  ,path_to_raw_drpsnd,path_to_raw_radiometer       
  
        character*150 static_dir,filename
  
