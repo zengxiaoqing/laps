549b6d682d051b446a5fd2d69aa04cc656e174f2
Steve Albers <Steve.Albers@noaa.gov>
2004-01-06 18:31:54 +0000

    commented out call to 'get_laps_config'
                 added call to 'get_systime'
    
                 pass 'lun_out' into subroutine 'ingest_raob'
    
                 turned on call to 'get_radiometer_data'
    
                 New subroutine 'convert_array' to simplify QC and units conversion
    
                 New subroutine 'get_nlevels_snd'

--- src/ingest/raob/ingest_sounding.f
+++ src/ingest/raob/ingest_sounding.f
@@ -9,22 +9,10 @@
        character*200 path_to_raw_tower
        character*200 path_to_raw_radiometer
        character*8 c8_raob_format, c8_project
+       character*9 a9_time
+       character*13 filename13,cvt_i4time_wfo_fname13
 
-       call get_laps_config('nest7grid',istatus)
-
-       call get_snd_parms(path_to_raw_raob,path_to_local_raob
-     1                   ,path_to_raw_drpsnd
-     1                   ,path_to_raw_satsnd
-     1                   ,path_to_raw_tower
-     1                   ,path_to_raw_radiometer
-     1                   ,istatus)       
-       if(istatus .ne. 1)goto999
-
-       call get_c8_project(c8_project,istatus)
-       if (istatus .ne. 1) then
-          write (6,*) 'Error getting c8_project'
-          go to 999
-       endif
+!      call get_laps_config('nest7grid',istatus)
 
        call get_grid_dim_xy(ni,nj,istatus)
        if (istatus .ne. 1) then
@@ -32,8 +20,27 @@
            stop
        endif
 
+       call get_c8_project(c8_project,istatus)
+       if (istatus .ne. 1) then
+          write (6,*) 'Error getting c8_project'
+          go to 999
+       endif
+
        call get_laps_cycle_time(laps_cycle_time,istatus)
        if(istatus .ne. 1)stop
+
+       call get_systime(i4time_sys,a9_time,istatus)
+       if(istatus .ne. 1)stop
+       write(6,*)' systime = ',a9_time
+
+       call get_snd_parms(path_to_raw_raob,path_to_local_raob
+     1                   ,path_to_raw_drpsnd
+     1                   ,path_to_raw_satsnd
+     1                   ,path_to_raw_tower
+     1                   ,path_to_raw_radiometer
+     1                   ,istatus)       
+
+       if(istatus .ne. 1)goto999
 
        lun_out = 21
 
@@ -41,7 +48,7 @@
            c8_raob_format = c8_project
            write(6,*)
            write(6,*)' Call ingest_raob, format=',c8_raob_format
-           call ingest_raob(path_to_raw_raob,c8_raob_format)
+           call ingest_raob(path_to_raw_raob,c8_raob_format,lun_out)
 
            if(c8_project .eq. 'CWB')then
                c8_raob_format = c8_project
@@ -63,12 +70,12 @@
            c8_raob_format = 'WFO'
            write(6,*)
            write(6,*)' Call ingest_raob, format=',c8_raob_format
-           call ingest_raob(path_to_raw_raob,c8_raob_format)
+           call ingest_raob(path_to_raw_raob,c8_raob_format,lun_out)
 
            c8_raob_format = 'RSA'
            write(6,*)
            write(6,*)' Call ingest_raob, format=',c8_raob_format
-           call ingest_raob(path_to_local_raob,c8_raob_format)
+           call ingest_raob(path_to_local_raob,c8_raob_format,lun_out)
 
            write(6,*)
            write(6,*)' Call tower_driver_sub'
@@ -89,14 +96,29 @@
        endif ! RSA project
  
 
+!      Satellite soundings
        write(6,*)
-       write(6,*)' Call ingest_satsnd'
+       write(6,*)' Call ingest_satsnd...'
        call ingest_satsnd(path_to_raw_satsnd)
 
+
+!      Radiometer data
        write(6,*)
-       write(6,*)' Call get_radiometer_data (under construction)'
-!      call get_radiometer_data(path_to_raw_radiometer,laps_cycle_time       
-!     1                        ,ni,nj,lun_out)
+       write(6,*)' Call get_radiometer_data...'
+       i4time_rad = (i4time_sys/3600) * 3600
+ 900   filename13 = cvt_i4time_wfo_fname13(i4time_rad)
+       call s_len(path_to_raw_radiometer,len_path)
+       call get_radiometer_data(i4time_rad,laps_cycle_time,ni,nj
+     1                         ,i4time_sys - 855/2 ! Radiometer cycle time / 2
+     1                         ,i4time_sys + 855/2 ! Radiometer cycle time / 2
+     1                         ,path_to_raw_radiometer(1:len_path)
+     1                                           //'/'//filename13
+     1                         ,lun_out,istatus)
+       if(istatus .ne. 1 .and. i4time_sys - i4time_rad .lt. 3600)then
+           write(6,*)' Trying radiometer for an hour earlier'
+           i4time_rad = i4time_rad - 3600
+           go to 900
+       endif
 
        close(lun_out) ! Output SND file
 
@@ -143,3 +165,52 @@
        return
 
        end
+
+      subroutine convert_array(array_in,array_out,n,string
+     1                        ,r_missing_data,istatus)       
+
+!     QC the observation array and convert units if needed
+!     If 'string' is 'none', then do just the QC without conversion
+
+      character*(*) string
+
+      real*4 k_to_c
+
+      real*4 array_in(n),array_out(n)
+
+      do i = 1,n
+          if(abs(array_in(n)) .ge. 1e10 .or. 
+     1           array_in(n)  .eq. r_missing_data )then
+              array_out(n) = r_missing_data
+          elseif(string .eq. 'k_to_c')then
+              array_out(n) = k_to_c(array_in(n))
+          elseif(string .eq. 'pa_to_mb')then
+              array_out(n) = array_in(n) / 100.
+          elseif(string .eq. 'none')then
+              array_out(n) = array_in(n)
+          else
+              write(6,*)' Unknown operator in convert_array: ',string
+              istatus = 0
+              return
+          endif
+      enddo ! i
+     
+      istatus = 1
+
+      return
+      end
+
+
+      subroutine get_nlevels_snd(pressure_mb,height_m,r_missing_data
+     1                          ,nlevels_raw,nlevels_snd)      
+
+      do i = 1,nlevels_raw
+          if(abs(pressure_mb(i)) .le. 2000. .or.
+     1       abs(height_m(i)) .le. 1000000.)then
+              nlevels_snd = i
+          endif
+      enddo ! i
+
+      return
+      end
+
