23056403632b36f69d640899d9025b5fe89b1f8f
Steve Albers <Steve.Albers@noaa.gov>
2004-01-26 21:37:01 +0000

    this version will write out daemon command file on /exchange

--- etc/followup_ncarg_remote.sh
+++ etc/followup_ncarg_remote.sh
@@ -4,14 +4,11 @@ REMOTE_NODE=$1
 REMOTE_DATA_ROOT=$2
 DOMAIN_NAME=$3
 
-echo "start followup_ncarg_remote.sh to generate web images"
-date
+echo "Running followup_ncarg.com on $REMOTE_NODE via do it yourself daemon...."
 
-echo "Running followup_ncarg.com on $REMOTE_NODE via ssh..."
-
-ssh $REMOTE_NODE /usr/nfs/common/lapb/www/followup_ncarg.com $REMOTE_DATA_ROOT $DOMAIN_NAME /usr/nfs/lapb/builds/laps
-
-date
+echo "command = /usr/nfs/common/lapb/www/followup_ncarg.com $REMOTE_DATA_ROOT $DOMAIN_NAME /usr/nfs/lapb/builds/laps"
+echo           "/usr/nfs/common/lapb/www/followup_ncarg.com $REMOTE_DATA_ROOT $DOMAIN_NAME /usr/nfs/lapb/builds/laps" >> /exchange/tmp/usfsfire/queue.$REMOTE_NODE
+chmod +x /exchange/tmp/usfsfire/queue.$REMOTE_NODE
 
 echo "followup_ncarg_remote.sh finished..."
 
