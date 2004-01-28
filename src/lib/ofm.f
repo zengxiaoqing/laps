aad14694e47eefecf6641ab27226800624ca7a29
Daniel Birkenheuer <daniel.l.birkenheuer@noaa.gov>
2004-01-28 21:18:29 +0000

    changed auto-data statement to explicit data statement for Solaris build

--- src/lib/ofm.f
+++ src/lib/ofm.f
@@ -122,7 +122,7 @@ c     satellite number
       integer ichan
       
       Integer Channels(Mchan) 
-     &     / 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18 /
+      data channels/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18/
       
       integer i
 
