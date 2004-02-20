01b76dbc8157bf14f755438f55f9a0ad3933f6d9
Steve Albers <Steve.Albers@noaa.gov>
2004-02-20 20:51:53 +0000

    added 'l_hybrid_first_gate' parameter

--- src/ingest/radar/remap/remap.f
+++ src/ingest/radar/remap/remap.f
@@ -521,12 +521,12 @@ cdis
 
        character*3 c3_radar_subdir
 
-       logical l_line_ref_qc
+       logical l_line_ref_qc,l_hybrid_first_gate
 
        namelist /remap_nl/ n_radars_remap,max_times,path_to_radar_a ! ,c4_radarname_a
      1                    ,laps_radar_ext_a,path_to_vrc_nl
      1                    ,ref_min,min_ref_samples,min_vel_samples,dgr
-     1                    ,abs_vel_min,l_line_ref_qc
+     1                    ,abs_vel_min,l_line_ref_qc,l_hybrid_first_gate       
        character*150 static_dir,filename
 
        call get_directory('nest7grid',static_dir,len_dir)
@@ -583,9 +583,11 @@ cdis
        write(6,*)' dgr             = ',dgr
        write(6,*)' abs_vel_min     = ',abs_vel_min
        write(6,*)' l_line_ref_qc   = ',l_line_ref_qc
+       write(6,*)' l_hybrid_first_gate = ',l_hybrid_first_gate
 
        namelist_parms%abs_vel_min = abs_vel_min
        namelist_parms%l_line_ref_qc = l_line_ref_qc 
+       namelist_parms%l_hybrid_first_gate = l_hybrid_first_gate
 
        istatus = 1
        return
