f54ee48b515f5ded08d7a23e30ca8dc4ebbf6635
smart <>
2004-02-24 22:09:54 +0000

    Mods for case reruns - added new module "sub systime" previously in sched.pl. Sub systime now accomodates both laps_driver.pl and sched.pl and has new input "$write_systime_dat".  This variable is used with sched to write the systime.dat file.  Also adjusted test for computing time for the nearest previous cycle time that is only active when $archive_time > 0.

--- etc/laps_tools.pm
+++ etc/laps_tools.pm
@@ -287,7 +287,7 @@ sub mkdatadirs{
   if(! -e $LAPS_SRC_ROOT) {die "$LAPS_SRC_ROOT  does not exist\n";}
 
   if($domain_type ne "laps" && $domain_type ne "wrfsi"){
-     die "Unknown domain type input to mkdatadirs = $domain_type\n";
+     die "Stop: Unknown domain type input to mkdatadirs = $domain_type\n";
   }
 
   my ($datadirs, $lapsprddirs);
@@ -828,3 +828,80 @@ sub get_julian_day {
  
 }
 1;
+#
+# ------------------------------------------------------------------------
+#
+sub systime{
+    use Time::Local;
+    my($DATAROOT,$delay,$cycle_time,$archive_time,$write_systime_dat) = @_;
+
+#   See if -t option was used to pass in archive time in calling routine
+    my $ctime = 0;    # Initial declaration outside the scope of the if test
+    if($archive_time > 0) {
+#       print "Archive data time is being set to a ctime of $archive_time...\n";
+        $ctime = $archive_time;
+    }else{
+#       print "Setting ctime based on clock time and delay of $delay in hours...\n";
+        $ctime = time - $delay*3600;
+    }
+
+#   print "ctime = $ctime\n";
+
+    my @MON = qw(JAN FEB MAR APR MAY JUN JUL AUG SEP OCT NOV DEC);
+    my($sec,$min,$hour,$mday,$mon,$year,$yyyy); 
+    if($archive_time > 0){
+      ($year,$mon,$mday,$hour,$min,$sec) = &laps_tools::i4time_to_date($archive_time);
+       $yyyy=$year;
+       $year=substr($year,2,2);
+       $mon=$mon-1;
+    }else{
+       ($sec,$min,$hour,$mday,$mon,$year) = gmtime($ctime);
+        $yyyy = 1900+$year;
+        $year= $year-100 if($year>99);
+        $year='0'.$year if(length($year)<2);
+        $mday = '0'.$mday if(length($mday)<2);
+    }
+#
+# This resets to the top of the cycle
+# ---------------------------------------
+    if($archive_time==0){
+       my $minute=$min;
+       my $i=0;
+       while($i<60){
+       my $mod = $i%int($cycle_time/60);
+        $min = $i if($i<$minute && $mod==0);
+        $i++;
+        #     print "$i $mod\n";
+       }
+    }
+
+    $ctime = timegm(0,$min,$hour,$mday,$mon,$yyyy-1900);
+
+    my $ftime = $ctime +  315619200;
+    $min = '0'.$min if(length($min)<2);
+    $hour = '0'.$hour if(length($hour)<2);
+
+    my $jjj = &laps_tools::julian($year,$mon+1,$mday);
+
+    $jjj="0".$jjj while(length($jjj)< 3);
+
+    my $yyjjjhhmm = "$year$jjj$hour$min";
+
+    if(defined $write_systime_dat){
+       open(TFILE,">$DATAROOT/time/c_time.dat");
+       print TFILE " $year$jjj$hour$min\n";
+       print TFILE "   $ctime\n";
+       close(TFILE);
+
+       open(TFILE,">$DATAROOT/time/systime.dat");
+       print TFILE "  $ftime\n";
+       print TFILE " $year$jjj$hour$min\n";
+       print TFILE "$hour\n";
+       print TFILE "$min\n";
+       print TFILE "$mday-$MON[$mon]-$yyyy $hour$min\n";
+       print TFILE "$year$jjj\n";
+       close(TFILE);
+    }
+
+    return ($yyjjjhhmm);
+}
