d3e10fc30a82d805a0c937d85c838edda3795736
Steve Albers <Steve.Albers@noaa.gov>
2004-03-01 19:01:38 +0000

    count and index the good profile levels to help filter out              missing data values in the input arrays

--- src/ingest/profiler/write_pro.f
+++ src/ingest/profiler/write_pro.f
@@ -27,43 +27,63 @@
       real dir_deg(maxpro,maxlvl)
       real spd_mps(maxpro,maxlvl)
 
+      logical l_good_level(maxlvl)
 !............................................................................
+
+      call get_r_missing_data(r_missing_data,istatus)
 
       rms = 1.0
 
       do ipro = 1,npro
 
+!       Count and index the good levels
+        n_good_levels = 0
+        do ilvl = 1,nlvl(ipro)
+            if(dir_deg(ipro,ilvl) .ne. r_missing_data .and.
+     1         spd_mps(ipro,ilvl) .ne. r_missing_data       )then
+                n_good_levels = n_good_levels + 1
+                l_good_level(ilvl) = .true.
+            else
+                l_good_level(ilvl) = .false.
+            endif
+        enddo ! ilvl
+
 !       Write Sounding Header
 
         call filter_string(c6_staid(ipro))
 
-        write(6      ,401)iwmostanum,nlvl(ipro),stalat(ipro)
+        write(6      ,401)iwmostanum,n_good_levels,stalat(ipro)
      1                   ,stalon(ipro),staelev(ipro),c6_staid(ipro)
      1                   ,a9time_ob(ipro),c8_obstype(ipro)
-        write(lun_out,401)iwmostanum,nlvl(ipro),stalat(ipro)
+        write(lun_out,401)iwmostanum,n_good_levels,stalat(ipro)
      1                   ,stalon(ipro),staelev(ipro),c6_staid(ipro)
      1                   ,a9time_ob(ipro),c8_obstype(ipro)
 401     format(i12,i12,f11.3,f15.3,f15.0,5x,a6,3x,a9,1x,a8)
 
-        do lvl = 1,nlvl(ipro)
+        do ilvl = 1,nlvl(ipro)
 
-!         Write Profile Level
-          write(lun_out,301,err=303)height_m(ipro,lvl)
-     1                             ,dir_deg(ipro,lvl)
-     1                             ,spd_mps(ipro,lvl)
-     1                             ,rms       
-301       format(1x,f6.0,f6.0,2f6.1,3f7.1)
-303       continue
+          if(l_good_level(ilvl))then
 
-          if(ipro .le. 100)then
-              write(6,301,err=313)height_m(ipro,lvl)
-     1                           ,dir_deg(ipro,lvl)
-     1                           ,spd_mps(ipro,lvl)
-     1                           ,rms       
-313           continue
-          endif
+!             Write Profile Level
+              write(lun_out,301,err=303)height_m(ipro,ilvl)
+     1                                 ,dir_deg(ipro,ilvl)
+     1                                 ,spd_mps(ipro,ilvl)
+     1                                 ,rms       
+301           format(1x,f6.0,f6.0,2f6.1,3f7.1)
+303           continue 
 
-        enddo ! lvl
+              if(ipro .le. 100)then
+                  write(6,301,err=313)height_m(ipro,ilvl)
+     1                               ,dir_deg(ipro,ilvl)
+     1                               ,spd_mps(ipro,ilvl)
+     1                               ,rms       
+313               continue
+              endif
+
+          endif ! good level
+
+        enddo ! ilvl
+
       enddo ! ipro
 
       go to 999
