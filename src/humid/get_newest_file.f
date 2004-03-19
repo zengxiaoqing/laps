f859ec7e704ef70e74e35b2b75c3edbb26293aae
Daniel Birkenheuer <daniel.l.birkenheuer@noaa.gov>
2004-03-19 19:48:55 +0000

    Revised to return the nearest file and not the newest file for ihop

--- src/humid/get_newest_file.f
+++ src/humid/get_newest_file.f
@@ -42,13 +42,13 @@ cdis
 c     parameters
       
       integer maxfiles
-      parameter (maxfiles = 3000)
+      parameter (maxfiles = 5000)
       
 c     parameter list variables
 
       character*9 filename 
       integer time_diff
-      character*512 path
+      character*256 path
       integer pi
       character*9 file
       character*120 ext
@@ -75,9 +75,13 @@ c     code
 
       
 c     get the  filenames available from get_filenames
-
+      write (6,*) 'Calling get_file_names with path:'
+      write (6,*) path(1:pi)
       call get_file_names (path(1:pi), numoffiles, filenames,
      1     maxfiles, istatus)
+      write (6,*) 'Get_file_names returns with:'
+      write (6,*) 'numoffiles: ',numoffiles
+      write (6,*) 'getfilenames istatus = ',istatus
 
 
       if (istatus.ne.1) then    !failure, return with failure code
@@ -90,43 +94,32 @@ c     get the  filenames available from get_filenames
       do i = 1,numoffiles
          hashpoint = index (filenames(i),de(1:dei))
          if (hashpoint .ne. 0) hash = filenames(i)
-         if (hash.gt.maxhash) maxhash = hash
-      enddo                     ! i
-      
-      hashpoint = index (hash,'.')
-      
-      if (hashpoint .eq. 0) then
-         istatus = 0
-         return
-      endif
-      
-      hash = maxhash
-      
-      file_new = hash(hashpoint-file_name_length:hashpoint)
-      ext = hash(hashpoint+1:256)
-      
-      call s_len(ext, ei, istatus)
+         hashpoint = index (hash,'.')
+         if (hashpoint .eq. 0) go to 15 ! try next file
+         file_new = hash(hashpoint-file_name_length:hashpoint)
+         ext = hash(hashpoint+1:256)
+         call s_len(ext, ei, istatus)
       
 c     determine i4 times of pertenant files
       
-      call i4time_fname_lp (file_new, i4time_found, istatus)
-      call i4time_fname_lp (filename, i4time_input, istatus)
+         call i4time_fname_lp (file_new, i4time_found, istatus)
+         call i4time_fname_lp (filename, i4time_input, istatus)
       
 c     compute time difference
       
-      i4time_difference = i4time_input-i4time_found
-      i4time_difference  = abs(i4time_difference)
+         i4time_difference = i4time_input-i4time_found
+         i4time_difference  = abs(i4time_difference)
       
-      if (i4time_difference .lt. time_diff) then !success in finding file
-         file = file_new
-         write(6,*) 'Found file ', file_new//'.'//ext(1:ei)
-         write(6,*) 'Age of file found is ',i4time_difference,' seconds'
-         return
-      else
-         istatus = 0 
-         write(6,*)'File not found'
-         return
-      endif
-      
+         if (i4time_difference .lt. time_diff) then !success in finding file
+            file = file_new
+            write(6,*) 'Found file ', file_new//'.'//ext(1:ei)
+            write(6,*) 
+     1    'Age of file found is ',i4time_difference,' seconds'
+            return
+         endif
+ 15      continue
+      enddo                     ! i done with all files
+      istatus  = 0
+      write (6,*) 'File not found in Get_newest_file'
       return
       end
