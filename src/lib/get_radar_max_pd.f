2a19574ea27cef7a9701e5d1a9e2808604f19beb
Steve Albers <Steve.Albers@noaa.gov>
2004-05-10 17:53:03 +0000

    pass 'i4_tol' and 'i4_ret' into/out of 'read_radar_3dref'

--- src/lib/get_radar_max_pd.f
+++ src/lib/get_radar_max_pd.f
@@ -147,7 +147,11 @@ c       rmax_so_far = 0.
               call get_ref_base(ref_base,istatus)
               if(istatus .ne. 1)return
 
-              call read_radar_3dref(i4time_radar,.true.,ref_base,
+              i4_tol = 1200
+
+              call read_radar_3dref(i4time_radar,
+     1                 i4_tol,i4_ret,                                   ! I/O
+     1                 .true.,ref_base,
      1                 imax,jmax,kmax,ext_radar,
      1                 lat,lon,topo,.false.,.false.,
      1                 heights_3d,
