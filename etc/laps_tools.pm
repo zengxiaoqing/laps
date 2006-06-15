92d0a0eb84add446aee925b9c0bb6ad87bcdf4f6
Steve Albers <Steve.Albers@noaa.gov>
2006-06-15 17:31:57 +0000

    corrected error message in "die" branch
                 added 'rdr/wideband' subdirectory

--- etc/laps_tools.pm
+++ etc/laps_tools.pm
@@ -27,7 +27,7 @@ sub get_nl_value{
    defined $namelist_var || die "namelist variable required input\n";
 
    if(!defined $template){
-      open(NLF,"$LAPS_DATA_ROOT/static/$namelist_file") or die "Can't open $LAPS_DATA_ROOT/$namelist_file";
+      open(NLF,"$LAPS_DATA_ROOT/static/$namelist_file") or die "Can't open $LAPS_DATA_ROOT/static/$namelist_file";
    }else{
       open(NLF,"$LAPS_DATA_ROOT/$namelist_file") or die "Can't open $LAPS_DATA_ROOT/$namelist_file";
    }
@@ -316,6 +316,7 @@ rdr/001 rdr/002 rdr/003 rdr/004 rdr/005 rdr/006 rdr/007 rdr/008 rdr/009
 rdr/001/vrc rdr/001/raw rdr/002/vrc rdr/002/raw rdr/003/vrc rdr/003/raw
 rdr/004/vrc rdr/004/raw rdr/005/vrc rdr/005/raw rdr/006/vrc rdr/006/raw
 rdr/007/vrc rdr/007/raw rdr/008/vrc rdr/008/raw rdr/009/vrc rdr/009/raw 
+rdr/wideband
 ls2 lapsprep lapsprep/mm5 lapsprep/rams lapsprep/wrf lapsprep/cdf dprep stats 
 balance balance/lt1 balance/lw3 balance/lh3 balance/lq3 balance/air balance/pln
 grid ram rsf lsq tmg lst pbl model model/varfiles model/output model/sfc
