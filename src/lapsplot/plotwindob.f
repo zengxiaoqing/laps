ed6b9bfa380aa0e65517cee0b8a8d49ff0389937
Steve Albers <Steve.Albers@noaa.gov>
2007-03-01 20:29:10 +0000

    pass 'aspect' variable into subroutine 'plotwindob'
                 Introduce and utilize 'aspect' variable passed into subroutine
                 'barbs'. For the 'latlon' grid, the barbs will be stretched in
                 the X direction both in length of line segments and in line
                 thickness.

--- src/lapsplot/plotwindob.f
+++ src/lapsplot/plotwindob.f
@@ -38,7 +38,7 @@ cdis
 cdis
 
       subroutine plot_windob(dir,spd,ri,rj,lat,lon,imax,jmax,relsize
-     1                      ,c4_rot)
+     1                      ,aspect,c4_rot)
 
       real*4 lat(imax,jmax),lon(imax,jmax)
       character*4 c4_rot
@@ -79,7 +79,9 @@ cdis
           else
               sense = -1.0
           endif
-          call barbs(spd,dir,ri,rj,du,rot,-1e10,+1e10,-1e10,+1e10,sense)
+
+          call barbs(spd,dir,ri,rj,du,rot,-1e10,+1e10,-1e10,+1e10
+     1                                               ,sense,aspect)
       endif
 
     1 continue
@@ -175,7 +177,7 @@ cdis
 
 c
         SUBROUTINE BARBS(SPD,DIR,U,V,DU,PROJROT,umin,umax,vmin,vmax
-     1                                                        ,sense)
+     1                                              ,sense,aspect)
 C
 C---Wind barb plotter from barbs_gp.
 C
@@ -188,13 +190,17 @@ C---DIR,SPD     WIND DIRECTION AND SPEED
 C---U,V         NCAR COORDINATES OF OBS
 C---DU          ARBITRARILY CHOSEN INCREMENT OF SCREEN SPACE
 C---PROJROT     ROTATION OF BARB DUE TO MAP PROJECTION
+C---ASPECT      RATIO of DU scaling divided by DV scaling (stretch in U dir)
 C
         DATA DEGRAD/.01745329/
+C
 C
 C---RETURN IF MISSING SPEED OR DIRECTION.
 C
         IF (DIR .LT. 0. .OR. SPD .LT. 0.) RETURN
         IF (DIR .GT. 360. .OR. SPD .GT. 200.) RETURN
+
+        thk_base = 1
 C
 C---DIRECTIONS:
 C
@@ -204,6 +210,16 @@ C
         SIND1=SIN(DR1)
         COSD=COS(DR)
         COSD1=COS(DR1)
+
+        thk =sqrt(  sind**2          + (cosd *aspect)**2 )
+
+        thk1=sqrt(  sind1**2         + (cosd1*aspect)**2 )
+
+        thkf=sqrt( (sind*aspect)**2  + cosd**2)
+
+!       write(6,11)aspect,dr*180./3.14159265,dr1*180./3.14159265
+!    1            ,thk,thk1,thkf
+!11     format(' aspect,dr,dr1,thk,thk1,thkf ',f6.3,2f8.1,3f9.3)
 C
 C---LENGTHS:
 C
@@ -229,15 +245,21 @@ C
         IF (SPD .LT. 2.5) THEN
         X1=U
         Y1=V
-        X2=X1+SIND*STAFF
+        X2=X1+SIND*STAFF*ASPECT
         Y2=Y1+COSD*STAFF
 
         if(x2 .lt. umin .or. x2 .gt. umax .or. y2 .lt. vmin .or. y2 .gt.
-     1 vmax)
-     1                                                  return
+     1 vmax)then
+            call GSLWSC(thk_base)
+            return
+        endif
 
+        call GSLWSC(thk)
         CALL LINE(X1,Y1,X2,Y2)
+
+        call GSLWSC(thk_base)
         return
+
         endif
 C
         SP=SPD+2.5
@@ -256,34 +278,43 @@ C---DRAW STAFF
 C
         X1=U
         Y1=V
-        X2=X1+SIND*STAFF
+        X2=X1+SIND*STAFF*ASPECT
         Y2=Y1+COSD*STAFF
 
         if(x2 .lt. umin .or. x2 .gt. umax .or. y2 .lt. vmin .or. y2 .gt.
-     1 vmax)
-     1                                                  return
+     1 vmax)then
+            return
+        endif
 
+        call GSLWSC(thk)
         CALL LINE(X1,Y1,X2,Y2)
+        call GSLWSC(thk_base)
 C
 C---PLOT HALF-BARB, IF NECESSARY
 C
         IF (SP .GE. 5.) THEN
-        X1=X2+SIND1*ADD
+        X1=X2+SIND1*ADD*ASPECT
         Y1=Y2+COSD1*ADD
 
 !       if(x1 .le. umin .or. x1 .ge. umax .or. y1 .le. vmin .or. y1 .ge. vmax)
 !       1                                                       return
 
+        call GSLWSC(thk1)
         CALL LINE(X1,Y1,X2,Y2)
+
         IF (N50 .NE. 0 .OR. N10 .NE. 0) GO TO 40
-        X1=X2+SIND*ADD
+        X1=X2+SIND*ADD*ASPECT
         Y1=Y2+COSD*ADD
 
 !       if(x1 .le. umin .or. x1 .ge. umax .or. y1 .le. vmin .or. y1 .ge. vmax)
 !       1                                                       return
 
+        call GSLWSC(thk)
         CALL LINE(X1,Y1,X2,Y2)
+
+        call GSLWSC(thk_base)
         RETURN
+
         END IF
 C
 40      X1=X2
@@ -292,25 +323,30 @@ C
 C---PLOT BARBS, IF NECESSARY
 C
         DO 50 I=1,N10
-        X2=X1+SIND*ADD
+        X2=X1+SIND*ADD*ASPECT
         Y2=Y1+COSD*ADD
-        X3=X2+SIND1*BARB
+        X3=X2+SIND1*BARB*ASPECT
         Y3=Y2+COSD1*BARB
 
 !       if(x1 .le. umin .or. x1 .ge. umax .or. y1 .le. vmin .or. y1 .ge. vmax)
 !       1                                                       return
 
-        CALL FRSTPT(X1,Y1)
+!       CALL FRSTPT(X1,Y1)
 
 !       if(x2 .le. umin .or. x2 .ge. umax .or. y2 .le. vmin .or. y2 .ge. vmax)
 !       1                                                       return
 
-        CALL VECTOR(X2,Y2)
+!       CALL VECTOR(X2,Y2)
+
+        call GSLWSC(thk)
+        CALL LINE(X1,Y1,X2,Y2)
 
 !       if(x3 .le. umin .or. x3 .ge. umax .or. y3 .le. vmin .or. y3 .ge. vmax)
 !       1                                                       return
 
-        CALL VECTOR(X3,Y3)
+        call GSLWSC(thk1)
+!       CALL VECTOR(X3,Y3)
+        CALL LINE(X2,Y2,X3,Y3)
         X1=X2
         Y1=Y2
 50      CONTINUE
@@ -318,34 +354,41 @@ C
 C---PLOT FLAGS, IF NECESSARY
 C
         DO 60 I=1,N50
-        X2=X1+SIND*ADD
+        X2=X1+SIND*ADD*ASPECT
         Y2=Y1+COSD*ADD
-        X3=X2+SIND1*BARB
+        X3=X2+SIND1*BARB*ASPECT
         Y3=Y2+COSD1*BARB
 
 !       if(x1 .le. umin .or. x1 .ge. umax .or. y1 .le. vmin .or. y1 .ge. vmax)
 !       1                                                       return
 
-        CALL FRSTPT(X1,Y1)
+        call GSLWSC(thk)
+!       CALL FRSTPT(X1,Y1)
 
 !       if(x2 .le. umin .or. x2 .ge. umax .or. y2 .le. vmin .or. y2 .ge. vmax)
 !       1                                                       return
 
-        CALL VECTOR(X2,Y2)
+!       CALL VECTOR(X2,Y2)
+        CALL LINE(X1,Y1,X2,Y2)
 
 !       if(x3 .le. umin .or. x3 .ge. umax .or. y3 .le. vmin .or. y3 .ge. vmax)
 !       1                                                       return
 
-        CALL VECTOR(X3,Y3)
+        call GSLWSC(thk1)
+!       CALL VECTOR(X3,Y3)
+        CALL LINE(X2,Y2,X3,Y3)
 
 !       if(x1 .le. umin .or. x1 .ge. umax .or. y1 .le. vmin .or. y1 .ge. vmax)
 !       1                                                       return
 
-        CALL VECTOR(X1,Y1)
+        call GSLWSC(thkf)
+!       CALL VECTOR(X1,Y1)
+        CALL LINE(X3,Y3,X1,Y1)
         X1=X2
         Y1=Y2
 60      CONTINUE
 C
+        call GSLWSC(thk_base)
         RETURN
         END
 c
