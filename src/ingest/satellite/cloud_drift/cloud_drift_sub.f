d570c7d240b68df914cee367b654ff3274e1cc4a
Steve Albers <Steve.Albers@noaa.gov>
2008-03-14 16:44:02 +0000

    added support for new ASCII format we see with newer satellites              such as GOES11 (and GOES12)
                 added support for water vapor (WV) based measurements

--- src/ingest/satellite/cloud_drift/cloud_drift_sub.f
+++ src/ingest/satellite/cloud_drift/cloud_drift_sub.f
@@ -49,12 +49,19 @@ cdis
       character*10 a10_time
       character*9 a9_timeObs,a10_to_a9 
       character*6 a6_time
+      character*5 a5_time
       character*4 a4_time
       character*2 c2_sat_type
 
       real lat_a(NX_L,NY_L)
       real lon_a(NX_L,NY_L)
       real topo_a(NX_L,NY_L)
+
+      logical l_new_fmt, l_parse
+      
+      l_new_fmt = .false.
+      if(l_parse(filename,'goes11'))l_new_fmt = .true.
+      if(l_parse(filename,'goes12'))l_new_fmt = .true.
 
       call get_domain_perimeter(NX_L,NY_L,'nest7grid',lat_a,lon_a, 
      1            topo_a,1.0,rnorth,south,east,west,istatus)
@@ -67,10 +74,28 @@ cdis
       i = 0
 
 !     Read the file...
-      read(1,*)               ! Skip header line
- 40   read(1,11,err=50,end=990)c2_sat_type,a6_time,a4_time
-     1                        ,rlat,rlon,ipres,spd,idir
- 11   format(a2,15x,a6,2x,a4,f8.2,f9.2,i6,f7.1,i5)
+      if(l_new_fmt)then
+          write(6,*)' Reading ASCII file in new format ',filename
+          nheader = 2
+      else
+          write(6,*)' Reading ASCII file in old format ',filename
+          nheader = 1
+      endif
+
+      do iheader = 1,nheader
+          read(1,*)               ! Skip header lines
+      enddo ! iheader
+
+ 40   if(.not. l_new_fmt)then
+          read(1,11,err=50,end=990)c2_sat_type,a6_time,a4_time
+     1                            ,rlat,rlon,ipres,spd,idir
+ 11       format(a2,15x,a6,2x,a4,f8.2,f9.2,i6,f7.1,i5)
+      else ! new format
+          read(1,12,err=50,end=990)c2_sat_type,a5_time,a4_time
+     1                            ,rlat,rlon,ipres,spd,idir
+ 12       format(a2,17x,a5,4x,a4,2x,f10.0,f10.0,i10,f10.0,i10)
+      endif
+
  50   continue
 
       i = i+1
@@ -88,16 +113,21 @@ cdis
           goto 900
       endif
 
-      if(c2_sat_type .ne. 'IR' .and. c2_sat_type .ne. 'VI')then       
+      if(c2_sat_type .ne. 'IR' .and. c2_sat_type .ne. 'VI'
+     1                         .and. c2_sat_type .ne. 'WV')then       
           write(6,*)' Bad Sat Type ',c2_sat_type
           goto 900
       endif
 
-      a10_time = a6_time//a4_time
-      a9_timeObs = a10_to_a9(a10_time,istatus)
-      if(istatus .ne. 1)then
-          write(6,*)' Bad observation time - reject ',a10_time       
-          goto 900
+      if(l_new_fmt)then
+          a9_timeObs = a5_time//a4_time
+      else
+          a10_time = a6_time//a4_time
+          a9_timeObs = a10_to_a9(a10_time,istatus)
+          if(istatus .ne. 1)then
+              write(6,*)' Bad observation time - reject ',a10_time       
+              goto 900
+          endif
       endif
 
       call cv_asc_i4time(a9_timeObs,i4time_ob)
