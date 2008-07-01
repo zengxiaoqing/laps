766925a42bf66ed2da620ab4f5b9dded04796eb1
Steve Albers <Steve.Albers@noaa.gov>
2008-07-01 18:51:52 +0000

    added 'lapsprep/wps' to list of directories that are created

--- etc/laps_tools.pm
+++ etc/laps_tools.pm
@@ -328,7 +328,7 @@ rdr/001/vrc rdr/001/raw rdr/002/vrc rdr/002/raw rdr/003/vrc rdr/003/raw
 rdr/004/vrc rdr/004/raw rdr/005/vrc rdr/005/raw rdr/006/vrc rdr/006/raw
 rdr/007/vrc rdr/007/raw rdr/008/vrc rdr/008/raw rdr/009/vrc rdr/009/raw 
 rdr/wideband
-ls2 lapsprep lapsprep/mm5 lapsprep/rams lapsprep/wrf lapsprep/cdf dprep stats 
+ls2 lapsprep lapsprep/mm5 lapsprep/rams lapsprep/wrf lapsprep/wps lapsprep/cdf dprep stats 
 balance balance/lt1 balance/lw3 balance/lh3 balance/lq3 balance/air balance/pln
 grid ram rsf lsq tmg lst pbl model model/varfiles model/output model/sfc
 verif verif/noBal verif/Bal verif/Bkgd gr2);
