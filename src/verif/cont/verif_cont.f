826b1e1e9c833dfba235cb1dd75af28f4c427f9e
Steve Albers <Steve.Albers@noaa.gov>
2009-01-09 22:23:45 +0000

    Added some arrays and loops to allow the the possibility of              verifying more than one field. This is now set to just one              field however (3-D reflecitivity)
                 Some variable names were changed to make the software more
                 general and extensible to have multiple types of variables
    
                 thresh_dbz to thresh_var
                 ndbz to maxthr
                 dbz_anal to var_anal
                 dbz_fcst to var_fcst

--- src/verif/cont/verif_cont.f
+++ src/verif/cont/verif_cont.f
@@ -42,8 +42,8 @@
      1                  r_missing_data,
      1                  j_status)
 
-        real dbz_anal_3d(NX_L,NY_L,NZ_L)
-        real dbz_fcst_3d(NX_L,NY_L,NZ_L)
+        real var_anal_3d(NX_L,NY_L,NZ_L)
+        real var_fcst_3d(NX_L,NY_L,NZ_L)
         logical lmask_and_3d(NX_L,NY_L,NZ_L)
         logical lmask_or_3d(NX_L,NY_L,NZ_L)
         logical lmask_all_3d(NX_L,NY_L,NZ_L)
@@ -61,21 +61,26 @@
         character*150 hist_dir, cont_dir, verif_dir
         character*150 hist_file
 
-!       integer n_models
-!       parameter (n_models=2)
-!       character*10 c_model_a(n_models)
+        integer n_fields
+        parameter (n_fields=1)
+        character*10 ext_anal_a(n_fields), ext_fcst_a(n_fields)
+        character*10 var_a(n_fields)
+        integer nthr_a(n_fields) ! number of thresholds for each field
 
-!       data c_model_a /'wrf-lin','wrf-wsm6'/        
+        data ext_fcst_a /'fua'/        
+        data ext_anal_a /'lps'/        
+        data var_a      /'REF'/        
+        data nthr_a     /3/        
 
         integer contable(0:1,0:1)
 
-        integer ndbz
-        parameter (ndbz=3)
+        integer maxthr
+        parameter (maxthr=3)
 
-        real cont_4d(NX_L,NY_L,NZ_L,ndbz)
+        real cont_4d(NX_L,NY_L,NZ_L,maxthr)
 
         lmask_all_3d = .true.
-        thresh_dbz = 20.
+        thresh_var = 20. ! lowest threshold for this variable
 
         i4_initial = i4time_sys
 
@@ -85,7 +90,12 @@
         write(6,*)' n_fdda_models = ',n_fdda_models
         write(6,*)' c_fdda_mdl_src = ',c_fdda_mdl_src
 
-        do imodel = 1,n_fdda_models
+        do ifield = 1,n_fields
+
+         var_2d = var_a(ifield)
+         call s_len(var_2d,lenvar)
+
+         do imodel = 1,n_fdda_models
 
           c_model = c_fdda_mdl_src(imodel)
 
@@ -97,14 +107,16 @@
 
             call get_directory('verif',verif_dir,len_verif)
 
-            hist_dir = verif_dir(1:len_verif)//'radar/hist/'
+            hist_dir = verif_dir(1:len_verif)//var_2d(1:lenvar)
+     1                                       //'/hist/'
      1                                       //c_model(1:len_model)
-            len_hist = len_verif + 11 + len_model
+            len_hist = len_verif + 6 + lenvar + len_model
 
-            cont_dir = verif_dir(1:len_verif)//'radar/cont/'
+            cont_dir = verif_dir(1:len_verif)//var_2d(1:lenvar)
+     1                                       //'/cont/'
      1                                       //c_model(1:len_model)
      1                                       //'/'
-!           len_cont = len_verif + 11 + len_model
+!           len_cont = len_verif + 6 + lenvar + len_model
 
             do ihr_fcst = 0,12
 
@@ -126,25 +138,22 @@
               open(11,file=hist_file,status='unknown')
 
 !             Read analyzed reflectivity
-              var_2d = 'REF'
-              ext = 'lps'
+              ext = ext_anal_a(ifield)
               call get_laps_3d(i4_valid,NX_L,NY_L,NZ_L
-     1            ,ext,var_2d,units_2d,comment_2d,dbz_anal_3d,istatus)
+     1            ,ext,var_2d,units_2d,comment_2d,var_anal_3d,istatus)
               if(istatus .ne. 1)then
                   write(6,*)' Error reading 3D REF Analysis'
                   return
               endif
 
 !             Read forecast reflectivity
-              var_2d = 'REF'
-              ext = 'fua'
-
+              ext = ext_fcst_a(ifield)
               call get_directory(ext,directory,len_dir)
               DIRECTORY=directory(1:len_dir)//c_model(1:len_model)//'/'
 
               call get_lapsdata_3d(i4_initial,i4_valid,NX_L,NY_L,NZ_L       
      1                          ,directory,var_2d
-     1                          ,units_2d,comment_2d,dbz_fcst_3d
+     1                          ,units_2d,comment_2d,var_fcst_3d
      1                          ,istatus)
               if(istatus .ne. 1)then
                   write(6,*)' Error reading 3D REF Forecast'
@@ -156,10 +165,10 @@
               do i = 1,NX_L
               do j = 1,NY_L
                 lmask_and_3d(i,j,k) = .false.
-                if(dbz_anal_3d(i,j,k) .ne. r_missing_data .and. 
-     1             dbz_anal_3d(i,j,k) .ge. thresh_dbz .and.
-     1             dbz_fcst_3d(i,j,k) .ne. r_missing_data .and.
-     1             dbz_fcst_3d(i,j,k) .ge. thresh_dbz           )then
+                if(var_anal_3d(i,j,k) .ne. r_missing_data .and. 
+     1             var_anal_3d(i,j,k) .ge. thresh_var .and.
+     1             var_fcst_3d(i,j,k) .ne. r_missing_data .and.
+     1             var_fcst_3d(i,j,k) .ge. thresh_var           )then
                     lmask_and_3d(i,j,k) = .true.
                 endif
               enddo ! j
@@ -171,10 +180,10 @@
               do i = 1,NX_L
               do j = 1,NY_L
                 lmask_or_3d(i,j,k) = .false.
-                if((dbz_anal_3d(i,j,k) .ne. r_missing_data .and. 
-     1              dbz_anal_3d(i,j,k) .ge. thresh_dbz) .OR.
-     1             (dbz_fcst_3d(i,j,k) .ne. r_missing_data .and.
-     1              dbz_fcst_3d(i,j,k) .ge. thresh_dbz)          )then       
+                if((var_anal_3d(i,j,k) .ne. r_missing_data .and. 
+     1              var_anal_3d(i,j,k) .ge. thresh_var) .OR.
+     1             (var_fcst_3d(i,j,k) .ne. r_missing_data .and.
+     1              var_fcst_3d(i,j,k) .ge. thresh_var)          )then       
                     lmask_or_3d(i,j,k) = .true.
                 endif
               enddo ! j
@@ -187,32 +196,32 @@
               write(lun_out,*)
               write(lun_out,*)' Calling radarhist for analysis at '
      1                        ,a9time_valid
-              call radarhist(NX_L,NY_L,NZ_L,dbz_anal_3d,lmask_all_3d
+              call radarhist(NX_L,NY_L,NZ_L,var_anal_3d,lmask_all_3d
      1                     ,lun_out)       
 
               write(lun_out,*)
               write(lun_out,*)' Calling radarhist for',ihr_fcst
      1               ,' hr forecast valid at ',a9time_valid
-              call radarhist(NX_L,NY_L,NZ_L,dbz_fcst_3d,lmask_all_3d
+              call radarhist(NX_L,NY_L,NZ_L,var_fcst_3d,lmask_all_3d
      1                    ,lun_out)
 
               write(lun_out,*)
               write(lun_out,*)
-     1    ' 3-D AND mask is in place with dbz threshold of ',thresh_dbz       
+     1    ' 3-D AND mask is in place with dbz threshold of ',thresh_var       
 
               write(lun_out,*)
               write(lun_out,*)' Calling radarhist for analysis at '
      1                      ,a9time_valid
-              call radarhist(NX_L,NY_L,NZ_L,dbz_anal_3d,lmask_and_3d
+              call radarhist(NX_L,NY_L,NZ_L,var_anal_3d,lmask_and_3d
      1                      ,lun_out)
 
               write(lun_out,*)
               write(lun_out,*)' Calling radarhist for',ihr_fcst
      1               ,' hr forecast valid at ',a9time_valid
-              call radarhist(NX_L,NY_L,NZ_L,dbz_fcst_3d,lmask_and_3d
+              call radarhist(NX_L,NY_L,NZ_L,var_fcst_3d,lmask_and_3d
      1                    ,lun_out)
 
-              nthr = 3
+              nthr = nthr_a(ifield)
 
 !             Calculate contingency tables
               do idbz = 1,nthr
@@ -221,7 +230,7 @@
                 write(lun_out,*)
                 write(lun_out,*)' Calculate contingency table for '
      1                         ,rdbz,' dbz'
-                call contingency_table(dbz_anal_3d,dbz_fcst_3d
+                call contingency_table(var_anal_3d,var_fcst_3d
      1                                ,NX_L,NY_L,NZ_L,rdbz,lun_out
      1                                ,contable)
 
@@ -231,7 +240,7 @@
 
 !               Calculate Contingency Table (3-D)
                 call calc_contable(i4_initial,i4_valid
-     1                       ,dbz_anal_3d,dbz_fcst_3d
+     1                       ,var_anal_3d,var_fcst_3d
      1                       ,rdbz,contable,NX_L,NY_L,NZ_L
      1                       ,cont_4d(1,1,1,idbz))
 
@@ -247,7 +256,9 @@
 
           endif ! c_model .ne. lga
 
-        enddo ! model
+         enddo ! model
+
+        enddo ! fields
 
  999    write(6,*)' End of subroutine verif_radar'
 
