11ac722e5dc235be1359fd7e79d2db32deab8882
Steve Albers <Steve.Albers@noaa.gov>
2009-09-16 00:10:58 +0000

    updates to better match the conventions used in the PSD papers              on the flux-tool

--- src/lib/up_mflux.f
+++ src/lib/up_mflux.f
@@ -31,59 +31,103 @@ cdis
 cdis 
 c
 c
-	subroutine up_mflux(ni,nj,nk,topo,u_3d,v_3d,q_3d,upslope_flux
-     1                     ,pres_3d,r_missing_data)
+	subroutine up_mflux(ni,nj,nk,topo,dx,dy
+     1                     ,u_3d,v_3d,tpw_2d,upslope_flux
+     1                     ,ht_3d,r_missing_data)
 
-c       Compute upslope moisture flux 
+c       Compute upslope moisture flux (using conventions in the PSD flux tool) 
 
-	real topo(ni,nj)                 ! I
-        real u_3d(ni,nj,nk)              ! I
-        real v_3d(ni,nj,nk)              ! I
-        real q_3d(ni,nj,nk)              ! I
-        real pres_3d(ni,nj,nk)           ! I
+	real topo(ni,nj)                 ! I Terrain elevation (m)
+	real dx(ni,nj)                   ! I Grid spacing in X direction (m)
+	real dy(ni,nj)                   ! I Grid spacing in Y direction (m)
+        real u_3d(ni,nj,nk)              ! I U wind component in Grid X direction
+        real v_3d(ni,nj,nk)              ! I V wind component in Grid Y direction
+        real ht_3d(ni,nj,nk)             ! I
+        real tpw_2d(ni,nj)               ! I
         real upslope_flux(ni,nj)         ! O
 
         real u_2d(ni,nj)
         real v_2d(ni,nj)
-        real q_2d(ni,nj)
-
-        value = 900. * 100.
-
-!       Interpolate 3D fields to 900mb pressure level
-	do j=1,nj
-	do i=1,ni
-            rk = rlevel_of_field(value,pres_3d,ni,nj,nk,i,j,istatus)
-            level = nint(rk)
-            u_2d = u_3d(i,j,level)
-            v_2d = v_3d(i,j,level)
-            q_2d = q_3d(i,j,level)
-        enddo ! i
-        enddo ! j
+        
+!       Controlling layer
+        ht_lo  = 1200.
+        ht_hi  = 3200.
 
 	do j=2,nj-1
 	do i=2,ni-1
-	   dterdx = (topo(i,j)+topo(i,j-1)-topo(i-1,j)-topo(i-1,j-1)
+          if(topo(i,j) .le. ht_low)then
+
+!           Calculate mean wind over the layer       
+            rk_lo = rlevel_of_field(ht_lo,ht_3d,ni,nj,nk,i,j,istatus) 
+            rk_hi = rlevel_of_field(ht_hi,ht_3d,ni,nj,nk,i,j,istatus)  
+
+            k_lo = int(rk_lo)
+            k_hi = int(rk_hi)
+
+!           Lower part
+            frac_lo = rk_lo - float(k_lo)
+            u_lo = u_3d(i,j,k_lo)*(1.-frac_lo)+u_3d(i,j,k_lo+1)*frac_lo
+            v_lo = v_3d(i,j,k_lo)*(1.-frac_lo)+v_3d(i,j,k_lo+1)*frac_lo     
+
+            ubar_lyr = (u_lo + u_3d(i,j,k_lo+1)) / 2.
+            vbar_lyr = (v_lo + v_3d(i,j,k_lo+1)) / 2.
+ 
+            ubar_sum = ubar_lyr
+            vbar_sum = vbar_lyr
+
+            sumk = 1. - frac_lo
+
+!           Middle part
+            do k = k_lo+1,k_hi
+                ubar_lyr = (u_3d(i,j,k) + u_3d(i,j,k+1)) / 2.
+                vbar_lyr = (v_3d(i,j,k) + v_3d(i,j,k+1)) / 2. 
+                ubar_sum = ubar_sum + ubar_lyr
+                vbar_sum = vbar_sum + vbar_lyr
+                sumk = sumk + 1.0
+            enddo ! k
+
+!           Upper part  
+            frac_hi = rk_hi - float(k_hi)
+            u_hi = u_3d(i,j,k_hi)*(1.-frac_hi)+u_3d(i,j,k_hi+1)*frac_hi
+            v_hi = v_3d(i,j,k_hi)*(1.-frac_hi)+v_3d(i,j,k_hi+1)*frac_hi     
+
+            ubar_lyr = (u_3d(i,j,k_hi) + u_hi) / 2.
+            vbar_lyr = (v_3d(i,j,k_hi) + v_hi) / 2.
+
+            ubar_sum = ubar_sum + ubar_lyr
+            vbar_sum = vbar_sum + vbar_lyr
+
+            sumk = sumk + frac_hi
+
+!           Divide to get the means
+            ubar = ubar_sum / sumk
+            vbar = vbar_sum / sumk            
+
+!           Determine terrain slope
+	    dterdx = (topo(i,j)+topo(i,j-1)-topo(i-1,j)-topo(i-1,j-1)
      1                ) * .5 / dx(i,j)
-	   dterdy = (topo(i,j)+topo(i-1,j)-topo(i-1,j-1)-topo(i,j-1)
+	    dterdy = (topo(i,j)+topo(i-1,j)-topo(i-1,j-1)-topo(i,j-1)
      1                ) * .5 / dy(i,j)
 
-	   ubar = u_2d(i,j)
-	   vbar = v_2d(i,j)
+            terrain_slope = sqrt(dterdx**2 + dterdy**2)
 
-           if(ubar .ne. r_missing_data .and. 
-     1        vbar .ne. r_missing_data      )then     
+            if(terrain_slope .gt. .001)then ! machine/terrain epsilon threshold
+!             Calculate upslope wind component (m/s)
+!             This is normalized by the terrain slope
+              dvh = (ubar * dterdx + vbar * dterdy) / terrain_slope
 
-!              Calculate upslope wind component (m/s)
-	       dvh = ubar * dterdx + vbar * dterdy
+!             Calculate upslope moisture flux (m**2/s)
+	      upslope_flux(i,j) = dvh * tpw_2d(i,j)
 
-!              Calculate upslope moisture flux (m/s)
-	       upslope_flux(i,j) = dvh * q(i,j)
+            else
+              upslope_flux(i,j) = r_missing_data
 
-           else
-	       upslope_flux(i,j) = r_missing_data
+            endif
 
-           endif
+          else
+            upslope_flux(i,j) = r_missing_data
 
+          endif
 	enddo !i
 	enddo !j
 
