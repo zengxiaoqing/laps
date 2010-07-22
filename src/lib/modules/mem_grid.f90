ac8ab1a6f42029178ceffdfb8ae0100543261e1e
Steve Albers <Steve.Albers@noaa.gov>
2010-07-22 18:52:55 +0000

    update from Jacques Middlecoff for parallelized wind analysis

--- src/lib/modules/mem_grid.f90
+++ src/lib/modules/mem_grid.f90
@@ -1,5 +1,8 @@
 Module mem_grid
 
-real, allocatable, dimension(:,:) :: lat, lon, topo, ldf
+real   , allocatable, dimension(:,:) :: lat, lon, topo, ldf
+integer, allocatable, dimension(:,:) :: IstartIend
+integer, allocatable, dimension(:)   :: recvcounts,displs
+integer                              :: nPEs,rank
 
 end Module
