e830c54bff518715d7ff336678fd7effe511d81c
Steve Albers <Steve.Albers@noaa.gov>
2010-12-29 19:59:19 +0000

    Argument list for 'sfcbkgd' now has three moisture variables              'q_sfc_i', 'td_sfc_i', and 'td_sfc_o' instead of one ('tdsfc') so              the data flow of moisture is clearer.
                 Argument list for 'compute_sfc_bgfields' now has two moisture
                 variables 'q_sfc' and 'td_sfc' instead of one ('tdsfc') so
                 the data flow of moisture is clearer. A local array 'qsfc_l'
                 was also added.

--- src/lib/bgdata/sfcbkgd.f
+++ src/lib/bgdata/sfcbkgd.f
@@ -1,5 +1,5 @@
-      SUBROUTINE SFCBKGD(bgmodel,t,q,height,tsfc,tdsfc,ter,p,
-     &IMX,JMX,KX,psfc)
+      SUBROUTINE SFCBKGD(bgmodel,t,q,height,tsfc,qsfc_i,
+     &                   tdsfc_i,tdsfc_o,ter,p,IMX,JMX,KX,psfc)
 
 c inputs are from laps analyzed 2- 3d fields
 C     INPUT       t        ANALYZED TEMPERATURE     3D
@@ -10,7 +10,6 @@ C                 IMX      DIMENSION E-W
 C                 JMX      DIMENSION N-S
 C                 KX       NUMBER OF VERTICAL LEVELS
 C                 p        LAPS Pressure levels     1D
-C                 tdsfc    LAPS Dew Point Temp      2D
 c
 c Compute surface variables on hi-res terrain using first 3D model level
 c above the LAPS terrain.
@@ -32,7 +31,7 @@ c
       INTEGER    JMX
       INTEGER    K
       INTEGER    KX
-      INTEGER    it
+      INTEGER    it, idebug
       INTEGER    bgmodel
 
       LOGICAL    lfndz
@@ -44,7 +43,9 @@ c
       REAL       Q           ( IMX , JMX , KX )
       REAL       rh3d        ( IMX , JMX , KX )
       REAL       rh2d        ( IMX , JMX )
-      REAL       tdsfc       ( imx,  jmx )
+      REAL       qsfc_i      ( imx,  jmx )
+      REAL       tdsfc_i     ( imx,  jmx )
+      REAL       tdsfc_o     ( imx,  jmx )
       REAL       T           ( IMX , JMX , KX )
       REAL       esat
       REAL       TER         ( IMX , JMX )
@@ -53,12 +54,12 @@ c
       REAL       make_ssh
       REAL       t_ref,badflag
 c
-c if bgmodel = 6 or 8 then tdsfc is indeed td sfc
-c if bgmodel = 4      then tdsfc is rh (WFO - RUC)
-c if bgmodel = 3      then tdsfc is rh and q is rh
+c if bgmodel = 6 or 8 then td_sfc_i is used         
+c if bgmodel = 4      then qsfc_i is rh (WFO - RUC)
+c if bgmodel = 3      then qsfc_i is rh and q is rh
 c if bgmodel = 9      then no surface fields input. Compute all from 3d
 c                     fields. q3d used. (NOS - ETA)
-c otherwise tdsfc is input with q
+c otherwise qsfc_i is used directly              
 c 
       write(6,*)' Subroutine sfcbkgd, bgmodel = ',bgmodel
 
@@ -74,43 +75,34 @@ c
          enddo
 
          badflag=0.
-         write(6,*)' Interp 3D T and RH to hi-res terrain, tdsfc is RH?'
+         write(6,*)
+     1        ' Interp 3D T and RH to hi-res terrain, qsfc_i is RH?'
          call interp_to_sfc(ter,rh3d,height,imx,jmx,kx,
-     &                      badflag,tdsfc) ! Here tdsfc temporarily is RH
+     &                      badflag,tdsfc_o) ! Here tdsfc_o temporarily is RH
          call interp_to_sfc(ter,t,height,imx,jmx,kx,badflag,
      &                      tsfc)
       endif
 
       write(6,*)' Compute sfc fields using 3D model data'
 
+      idebug = 1
+
       do j=1,jmx
       do i=1,imx
-
          k=0
          lfndz=.false.
          do while(.not.lfndz)
             k=k+1
             if(height(i,j,k).gt.ter(i,j))then
                lfndz=.true.
-c
-c
-c recompute psfc with moisture consideration. snook's orig code.
-c              it=tdsfc(i,j)*100.
-c              it=min(45000,max(15000,it))
-c              xe=esat(it)
-c              qsfc=0.622*xe/(p_sfc-xe)
-c              qsfc=qsfc/(1.+qsfc)
-c --------------------------------------------------
 
                if(k.gt.0)then
 
                   call compute_sfc_bgfields(bgmodel,imx,jmx,kx,i,j,k
-     &,ter(i,j),height,t,p,q,t_ref,psfc(i,j),tsfc(i,j),tdsfc(i,j))
+     &                ,ter(i,j),height,t,p,q,t_ref,psfc(i,j),tsfc(i,j)
+     &                ,qsfc_i(i,j),tdsfc_o(i,j),idebug)
 
-c              else
-c                 tsfc(i,j)=t(i,j,k)
-c                 tdsfc(i,j)=make_td(p(k),t(i,j,k)-273.15,q(i,j,k)*1000.
-c    &,t_ref)+273.15
+                  idebug = 0
 
                endif
 
@@ -126,7 +118,7 @@ c
 c----------------------------------------------------------------------------
 c
       subroutine compute_sfc_bgfields(bgm,nx,ny,nz,i,j,k,ter,height
-     &,t,p,q,t_ref,psfc,tsfc,tdsfc)
+     &,t,p,q,t_ref,psfc,tsfc,qsfc,tdsfc,idebug)
 c
 c J. Smart 11/18/99 put existing code in subroutine for other process use in laps
 c
@@ -135,7 +127,7 @@ c
       integer nx,ny,nz
       integer i,j,k            !I, i,j,k coordinate of point for calculation
       integer bgm              !I, model type {if = 0, then tdsfc input = qsfc}
-      integer istatus
+      integer istatus, idebug
 
       real   p(nz)             !I, pressure of levels
       real   ter               !I, Terrain height at i,j
@@ -145,10 +137,11 @@ c
       real   t(nx,ny,nz)       !I, temperatures 3d 
       real   q(nx,ny,nz)       !I, specific humidity 3d
       real   psfc              !O, output surface pressure, pa
-      real   tsfc              !I/O input model T, output recomputed T
-      real   tdsfc             !I/O input model Td, output  recomputed Td
+      real   tsfc              !I/O input sfc T, output recomputed T
+      real   qsfc              !I   input sfc Q                  
+      real   tdsfc             !O   hi-res Td            
 
-      real   qsfc              !I/O surface spec hum, Input as q or computed internally
+      real   qsfc_l            !L   surface spec hum, Input as q or computed internally
 
       real   tbar
       real   td1,td2
@@ -164,8 +157,9 @@ c
 c if first guess values are missing data then return missing data
 c
        call get_r_missing_data(r_missing_data,istatus)
-       if(tsfc.lt.500.0.and.t(i,j,k).lt.500.0.and.
-     .tdsfc.lt.500.0)then
+
+       if(tsfc.lt.500.0.and.t(i,j,k).lt.500.0) then 
+!    .    qsfc.lt.500.0)then
 c
 c first guess psfc without moisture consideration
 c
@@ -174,26 +168,30 @@ c
           dz=height(i,j,k)-ter
           psfc=p(k)*exp(G/(R*tbar)*dz)
 
+!         Calculate qsfc_l according to model
           if(bgm.eq.0.or.bgm.eq.6.or.
-     &       bgm.eq.8.or.bgm.eq.12)then      !Td
-             qsfc=ssh2(psfc,tsfc-273.15
-     &                  ,tdsfc-273.15,t_ref)*.001  !kg/kg
+     &       bgm.eq.8.or.bgm.eq.12)then                 ! qsfc is Td
+             qsfc_l=ssh2(psfc,tsfc-273.15
+     &                  ,qsfc-273.15,t_ref)*.001   !kg/kg
 
-          elseif(bgm.eq.3.or.bgm.eq.4.or.bgm.eq.9)then  !RH
-             qsfc=make_ssh(psfc,tsfc-273.15
-     &                      ,tdsfc/100.,t_ref)*.001  !kg/kg
+          elseif(bgm.eq.3.or.bgm.eq.4.or.bgm.eq.9)then  ! qsfc is RH
+             qsfc_l=make_ssh(psfc,tsfc-273.15
+     &                      ,qsfc/100.,t_ref)*.001 !kg/kg
 
-          else                              !q
-             qsfc=tdsfc
+          else                                          !q
+             if(idebug .eq. 1)then
+                 write(6,*)' compute_sfc_bgfields: qsfc_l = qsfc ',qsfc
+             endif
+             qsfc_l=qsfc
           endif
 
 c pressure
-          tvsfc=tsfc*(1.+0.608*qsfc)
+          tvsfc=tsfc*(1.+0.608*qsfc_l)
           tvk=t(i,j,k)*(1.+0.608*q(i,j,k))
           tbarv=(tvsfc+tvk)*.5
           psfc=(p(k)*exp(G/(R*tbarv)*dz))*100.  !return units = pa
 
-          if(k.gt.1)then
+          if(k.gt.1)then ! calculate tsfc,qsfc
 
              dzp=height(i,j,k)-height(i,j,k-1)
 c temp
@@ -205,8 +203,12 @@ c dew point temp
              td1=make_td(p(k-1),t(i,j,k-1)-273.15,q(i,j,k-1)*1000.
      .,t_ref)
              tdsfc=td2+((td1-td2)/dzp)*dz+273.15
+
+             if(idebug .eq. 1)then
+                 write(6,*)' k/tdsfc = ',k,tdsfc                                    
+             endif
 c
-          else
+          else ! k=1: calculate tsfc,qsfc and psfc
 
              dzp=height(i,j,k+1)-height(i,j,k)
              td2=make_td(p(k),t(i,j,k)-273.15
@@ -219,11 +221,16 @@ c
              psfc=p(k)*exp(G/(R*tbar)*dz)
              tdsfc=td2+((td1-td2)/dzp)*dz
 
+             if(idebug .eq. 1)then
+                 write(6,*)' k/tdsfc = ',k,tdsfc                                    
+             endif
+
           endif
        else
           psfc =r_missing_data
           tsfc =r_missing_data
           tdsfc=r_missing_data
        endif
+
        return
        end
