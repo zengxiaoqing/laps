99f6ad9ce362188fe0aa554b8212891e22bd042a
Steve Albers <Steve.Albers@noaa.gov>
2011-03-21 19:38:37 +0000

    reworked subroutine 'read_snd_metadata' with emphasis on returning              cloud liquid obs from the sounding headers

--- src/lib/read_snd_data.f
+++ src/lib/read_snd_data.f
@@ -330,12 +330,11 @@ cdoc    Returns sounding wind, T, Td data from the SND file
 
         subroutine read_snd_metadata(lun,i4time_snd,ext                ! I
      1                         ,MAX_PR,MAX_PR_LEVELS                   ! I
-     1                         ,lat,lon,imax,jmax,kmax                 ! I
+     1                         ,lat,lon,imax,jmax                      ! I
      1                         ,n_profiles                             ! O
      1                         ,nlevels_obs_pr,lat_pr,lon_pr,elev_pr   ! O
      1                         ,c5_name,i4time_ob_pr,obstype           ! O
-     1                         ,ob_pr_ht_obs,ob_pr_pr_obs              ! O
-!    1                         ,ob_pr_u_obs,ob_pr_v_obs                ! O
+     1                         ,cloud_base_temp,cloud_liquid           ! O
      1                         ,istatus)                               ! O
 
 cdoc    Returns sounding metadata from the SND file
@@ -345,12 +344,8 @@ cdoc    Returns sounding metadata from the SND file
         real lon_pr(MAX_PR)
         real elev_pr(MAX_PR)
 
-        real ob_pr_ht_obs(MAX_PR,MAX_PR_LEVELS)                        ! O
-        real ob_pr_pr_obs(MAX_PR,MAX_PR_LEVELS)                        ! O
-        real ob_pr_di_obs(MAX_PR_LEVELS)                               ! L
-        real ob_pr_sp_obs(MAX_PR_LEVELS)                               ! L
-        real ob_pr_u_obs(MAX_PR,MAX_PR_LEVELS)                         ! O
-        real ob_pr_v_obs(MAX_PR,MAX_PR_LEVELS)                         ! O
+        real cloud_base_temp(MAX_PR)
+        real cloud_liquid(MAX_PR)
 
         integer i4time_ob_pr(MAX_PR)
         integer nlevels_obs_pr(MAX_PR)
@@ -359,6 +354,7 @@ cdoc    Returns sounding metadata from the SND file
         character*8 obstype(MAX_PR)
         character*9 a9time_ob
         character ext*(*)
+        character*200 line
 
         real lat(imax,jmax)
         real lon(imax,jmax)
@@ -380,7 +376,7 @@ cdoc    Returns sounding metadata from the SND file
           GO TO 600
       endif
 
-      DO i_pr = n_profiles+1,max_pr
+      DO i_pr = 1,max_pr
 
         if(i_pr .le. 200 .or. i_pr .eq. (i_pr/10)*10)then
             iwrite = 1
@@ -388,12 +384,27 @@ cdoc    Returns sounding metadata from the SND file
             iwrite = 0
         endif
 
-        read(lun,511,err=530,end=550)ista,nlevels_in,
-     1       lat_pr(i_pr),lon_pr(i_pr),elev_pr(i_pr),
-     1       c5_name(i_pr),a9time_ob,obstype(i_pr)
-  511   format(i12,i12,f11.4,f15.4,f15.0,1x,a5,3x,a9,1x,a8)
+        read(lun,501,err=530,end=550)line
+ 501    format(a)
+ 
+        read(line,511,err=530,end=550)ista,nlevels_in,
+     1           lat_pr(i_pr),lon_pr(i_pr),elev_pr(i_pr),
+     1           c5_name(i_pr),a9time_ob,obstype(i_pr)
+ 
+        if(obstype(i_pr) .eq. 'RADIOMTR')then
+            read(line,511,err=530)ista,nlevels_in,
+     1           lat_pr(i_pr),lon_pr(i_pr),elev_pr(i_pr),
+     1           c5_name(i_pr),a9time_ob,obstype(i_pr),
+     1           cloud_base_temp(i_pr),cloud_liquid(i_pr)
+        else
+            read(line,511,err=530)ista,nlevels_in,
+     1           lat_pr(i_pr),lon_pr(i_pr),elev_pr(i_pr),
+     1           c5_name(i_pr),a9time_ob,obstype(i_pr)
+            cloud_base_temp(i_pr) = r_missing_data     
+            cloud_liquid(i_pr) = r_missing_data     
+        endif
 
-!       obstype(i_pr) = 'RAOB'
+  511   format(i12,i12,f11.4,f15.4,f15.0,1x,a5,3x,a9,1x,a8,f8.0,f8.0)
 
         call i4time_fname_lp(a9time_ob,i4time_ob,istatus)
         i4time_ob_pr(i_pr) = i4time_ob
@@ -408,68 +419,15 @@ cdoc    Returns sounding metadata from the SND file
 
         DO level = 1,nlevels_in
 
-          read(lun,*,err=515)ht_in,pr_in,t_in,td_in,di_in,sp_in ! (sp = m/s)
+          read(lun,*,end=515)
 
 !         Test this by deliberately setting ht_in to missing
 !         ht_in = r_missing_data
 
-!         Determine whether we need to supply our own height (only pres given)
-          if(ht_in .eq. r_missing_data .and. 
-     1       pr_in .ne. r_missing_data                      )then
-
-              call latlon_to_rlapsgrid(lat_pr(i_pr),lon_pr(i_pr),lat,lon       
-     1                              ,imax,jmax,ri,rj,istatus)
-
-              if(istatus .ne. 1)goto505
-
-              i_ob = nint(ri)
-              j_ob = nint(rj)
-              if(i_ob .ge. 1 .and. i_ob .le. imax .and.
-     1           j_ob .ge. 1 .and. j_ob .le. jmax      )then
-                  pr_in_pa = pr_in * 100.
-                  call pressure_to_height(pr_in_pa,heights_3d
-     1                     ,imax,jmax,kmax,i_ob,j_ob,ht_buff,istatus)
-                  if(istatus .ne. 1)goto505
-                  ht_in = ht_buff
-                  if(iwrite .eq. 1)
-     1            write(6,*)' Pressure was given, height was derived:'       
-     1                     ,pr_in,ht_in
-              endif
-          endif
-
- 505      if(ht_in .ne. r_missing_data .and.
-     1       di_in .ne. r_missing_data .and.
-     1       sp_in .ne. r_missing_data          )then           
-
-              n_good_levels = n_good_levels + 1
-
-              if(n_good_levels .gt. MAX_PR_LEVELS)then
-                  write(6,*)' ERROR: too many sounding (.snd) levels '       
-     1                     ,i_pr,n_good_levels,MAX_PR_LEVELS
-                  goto515
-              endif
-
-              nlevels_obs_pr(i_pr) = n_good_levels
-              ob_pr_ht_obs(i_pr,n_good_levels) = ht_in
-              ob_pr_pr_obs(i_pr,n_good_levels) = pr_in
-              ob_pr_di_obs(n_good_levels) = di_in
-              ob_pr_sp_obs(n_good_levels) = sp_in
-
-              call disp_to_uv(ob_pr_di_obs(n_good_levels),
-     1                        ob_pr_sp_obs(n_good_levels),
-     1                        ob_pr_u_obs(i_pr,n_good_levels),
-     1                        ob_pr_v_obs(i_pr,n_good_levels))
-
-!             write(6,311,err=312)ista,i_pr
-!    1        ,ob_snd_ht_obs(i_pr,level)
-!    1        ,ob_snd_t_obs(i_pr,level)
-!311          format(1x,i6,i4,5f8.1)
-
-          endif ! Good data at the level
 
           go to 516
 
-  515     write(6,*)' Error reading RAOB, raw level # =',level
+  515     write(6,*)' Error reading level # =',level
           write(6,*)' While reading sounding # ',(i_pr-n_profiles)
           n_profiles=i_pr-1
           istatus = 0
@@ -478,7 +436,7 @@ cdoc    Returns sounding metadata from the SND file
   516   END DO                   ! level
       END DO ! i_pr
 
-      write(6,*)' ERROR: # of soundings+profilers has reached'
+      write(6,*)' ERROR: # of soundings has reached'
      1         ,' dimensions of MAX_PR ',MAX_PR
 
       n_profiles=MAX_PR
