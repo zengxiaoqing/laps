15809f0f28944779ce158fa4a5a63f2f2e95a0a8
Steve Albers <Steve.Albers@noaa.gov>
2012-01-25 19:46:06 +0000

    changed variable name from 'sh_sfc' to 'td_sfc'
                 added check for TD missing values in subroutine 'tdcheck'

--- src/lib/bgdata/bkgd_stn_interp.f
+++ src/lib/bgdata/bkgd_stn_interp.f
@@ -1,14 +1,16 @@
 c
 c---------------------------------------------------------------------------
 c
-      subroutine tdcheck(nx_laps,ny_laps,sh_sfc,tp_sfc,
+      subroutine tdcheck(nx_laps,ny_laps,td_sfc,tp_sfc,
      &icnt,i_mx,j_mx,dmax,dmin)
 
       integer icnt,i_mx,j_mx
 
-      real sh_sfc(nx_laps,ny_laps)
+      real td_sfc(nx_laps,ny_laps)
       real tp_sfc(nx_laps,ny_laps)
       real dmax,dmin
+
+      call get_r_missing_data(r_missing_data,istatus)
 
       icnt=0
       diff_mx = -1.e30
@@ -19,9 +21,10 @@ c
       j_mn = 0
       do j=1,ny_laps
       do i=1,nx_laps
-         if(sh_sfc(i,j).gt.tp_sfc(i,j))then
-            diff = sh_sfc(i,j) - tp_sfc(i,j)
-            sh_sfc(i,j)=tp_sfc(i,j)
+         if(td_sfc(i,j) .gt. tp_sfc(i,j) .AND. 
+     1      td_sfc(i,j) .ne. r_missing_data)then
+            diff = td_sfc(i,j) - tp_sfc(i,j)
+            td_sfc(i,j)=tp_sfc(i,j)
             icnt=icnt+1
             if(diff .gt. diff_mx) then
                diff_mx = diff
