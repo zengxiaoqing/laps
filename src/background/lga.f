86a2e43dbd700b688e2dd0870bbe86716adf3b76
Steve Albers <Steve.Albers@noaa.gov>
2012-01-25 19:47:42 +0000

    removed redundant version of 'tdcheck'

--- src/background/lga.f
+++ src/background/lga.f
@@ -201,6 +201,8 @@ c
       else
           ntmin = -1
           ntmax = +1
+!         ntmin = 0
+!         ntmax = +6
       endif
 
       ltime=.true.
@@ -423,42 +425,3 @@ c
 c
       end
 c
-c
-      subroutine tdcheck(nx_laps,ny_laps,sh_sfc,tp_sfc,
-     &icnt,i_mx,j_mx,i_mn,j_mn,dmax,dmin)
-
-      integer icnt,i_mx,j_mx
-
-      real sh_sfc(nx_laps,ny_laps)
-      real tp_sfc(nx_laps,ny_laps)
-      real dmax,dmin
-
-      icnt=0
-      dmax = -1.e30
-      dmin =  1.e30
-      i_mx = 0
-      j_mx = 0
-      i_mn = 0
-      j_mn = 0
-      do j=1,ny_laps
-      do i=1,nx_laps
-         if(sh_sfc(i,j).gt.tp_sfc(i,j))then
-            diff = sh_sfc(i,j) - tp_sfc(i,j)
-            sh_sfc(i,j)=tp_sfc(i,j)
-            icnt=icnt+1
-            if(diff .gt. dmax) then
-               dmax = diff
-               i_mx = i
-               j_mx = j
-            endif
-            if(diff .lt. dmin) then
-               dmin = diff
-               i_mn = i
-               j_mn = j
-            endif
-         endif
-      enddo
-      enddo
-
-      return
-      end
