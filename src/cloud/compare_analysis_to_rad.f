d0107b1a2c9797ace590452fd3cc080343b34a4e
Steve Albers <Steve.Albers@noaa.gov>
2012-06-01 21:03:52 +0000

    adjusted downward the solar radiation assumed based on clear sky             scattering/absorption

--- src/cloud/compare_analysis_to_rad.f
+++ src/cloud/compare_analysis_to_rad.f
@@ -78,7 +78,7 @@
 
         solar_constant = 1353.
         solar_irradiance = solar_constant * rad_dist_factor
-        transmittance = 0.800 ! catchall for various types of absorption / scattering
+        transmittance = 0.730 ! catchall for various types of absorption / scattering
 
         model = 1
         if(model .eq. 1)then
