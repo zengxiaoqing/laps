460c0c73cfdd14392da67a0a5680afc53d7b3b82
Steve Albers <Steve.Albers@noaa.gov>
2012-09-07 22:24:06 +0000

    dded 'exchange_tar' option

--- etc/copyremote.sh
+++ etc/copyremote.sh
@@ -4,7 +4,7 @@
 
 # Argument 2: Destination directory
 
-# Argument 3: Copy method (bbcp, exchange, rsync)
+# Argument 3: Copy method (bbcp, exchange, rsync, exchange_tar)
 
 # .................................................................................
 
@@ -24,6 +24,31 @@ if test "$3" == "bbcp"; then
 elif test "$3" == "exchange"; then
     echo "scp -r $FILENAME jetscp.rdhpcs.noaa.gov:$TEMPDIR/$TEMPFILE" 
           scp -r $FILENAME jetscp.rdhpcs.noaa.gov:$TEMPDIR/$TEMPFILE  
+
+    if test -d /exchange/tmp/fab/$TEMPFILE; then # move individual files between the two directories
+        echo " "
+        echo "second hop of directory"
+        echo "ssh $REMOTE_NODE mv $TEMPDIR/$TEMPFILE/* $DESTDIR/$FILENAME"
+              ssh $REMOTE_NODE mv $TEMPDIR/$TEMPFILE/* $DESTDIR/$FILENAME 
+    else
+        echo "second hop of file"
+        echo "ssh $REMOTE_NODE mv $TEMPDIR/$TEMPFILE $DESTDIR/$FILENAME"
+              ssh $REMOTE_NODE mv $TEMPDIR/$TEMPFILE $DESTDIR/$FILENAME 
+    fi
+
+elif test "$3" == "exchange_tar"; then
+    if test -d $FILENAME; then                                                       
+        echo " "
+        echo "first hop of directory"
+        cd $FILENAME
+        echo 'tar cvf - . | ssh jetscp.rdhpcs.noaa.gov "mkdir -p $TEMPDIR/$TEMPFILE; cd $TEMPDIR/$TEMPFILE; tar xpvf -"'
+              tar cvf - . | ssh jetscp.rdhpcs.noaa.gov "mkdir -p $TEMPDIR/$TEMPFILE; cd $TEMPDIR/$TEMPFILE; tar xpvf -"
+    else
+        echo " "
+        echo "first hop of file"        
+        echo "scp -r $FILENAME jetscp.rdhpcs.noaa.gov:$TEMPDIR/$TEMPFILE" 
+              scp -r $FILENAME jetscp.rdhpcs.noaa.gov:$TEMPDIR/$TEMPFILE  
+    fi
 
     if test -d /exchange/tmp/fab/$TEMPFILE; then # move individual files between the two directories
         echo " "
