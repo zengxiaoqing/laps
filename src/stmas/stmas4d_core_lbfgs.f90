b5c63919008ef12ee703e9726be1aa98a7953a60
Yuanfu Xie <yuanfu.xie@noaa.gov>
2012-10-12 15:06:22 +0000

    Applying bound to the last two levels instead of 3 by Yuanfu

--- src/stmas/stmas4d_core_lbfgs.f90
+++ src/stmas/stmas4d_core_lbfgs.f90
@@ -464,7 +464,7 @@ print*,'minvalue of bk: ',minval(grdbkgnd(1:numgrid(1),1:numgrid(2),1:numgrid(3)
             CALL PSTN2NUMB(5,NN,NG,NC)
 
             NB(NC) = 0
-            IF ((MAXGRID(1)-1)/4+1 .LE. NUMGRID(1)) NB(NC) = 1
+            IF ((MAXGRID(1)-1)/2+1 .LE. NUMGRID(1)) NB(NC) = 1
             
             ! LOW BOUND FROM REFLECTIVITY DID NOT GET SCALED AND SO HERE IT DOES:
             LB(NC) = -GRDBKGND(I,J,K,T,5)+GRDBKGND(I,J,K,T,NUMSTAT+1)
