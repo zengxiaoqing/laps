de8a2e334b34ea9f3bc1e4a50b8438aae1c49f09
Steve Albers <Steve.Albers@noaa.gov>
2012-11-08 21:07:48 +0000

    added exchange_tarfilelist option

--- etc/copyremote.sh
+++ etc/copyremote.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Argument 1: filename
+# Argument 1: filename (individual file, subdirectory, or file list)
 
 # Argument 2: Destination directory
 
-# Argument 3: Copy method (bbcp, exchange, rsync, exchange_tar)
+# Argument 3: Copy method (bbcp, exchange, rsync, exchange_tar, exchange_tarfilelist)
 
 # .................................................................................
 
@@ -60,6 +60,58 @@ elif test "$3" == "exchange_tar"; then
         echo "ssh $REMOTE_NODE mv $TEMPDIR/$TEMPFILE $DESTDIR/$FILENAME"
               ssh $REMOTE_NODE mv $TEMPDIR/$TEMPFILE $DESTDIR/$FILENAME 
     fi
+
+elif test "$3" == "exchange_tarfilelist"; then
+    if test -e $FILENAME; then                                                       
+        echo " "
+        echo "make local tar file from filelist"
+        echo  tar -T $FILENAME -cvf $TEMPFILE.tar    
+              tar -T $FILENAME -cvf $TEMPFILE.tar                                                                         
+        ls -l $TEMPFILE.tar
+
+        echo  gzip $TEMPFILE.tar 
+              gzip $TEMPFILE.tar 
+
+        ls -l $TEMPFILE.tar.gz
+
+        date -u
+
+        echo " "
+        echo "first hop of tar file"        
+        echo  scp -r $TEMPFILE.tar.gz jetscp.rdhpcs.noaa.gov:$TEMPDIR              
+              scp -r $TEMPFILE.tar.gz jetscp.rdhpcs.noaa.gov:$TEMPDIR               
+
+        date -u
+
+        TEMPFILE2=`basename $TEMPFILE`                        
+
+        echo " "
+        echo "zcat from $TEMPDIR into second directory"
+        echo  ssh $REMOTE_NODE "cd $DESTDIR; zcat $TEMPDIR/$TEMPFILE2.tar.gz | tar -xvf -"
+              ssh $REMOTE_NODE "cd $DESTDIR; zcat $TEMPDIR/$TEMPFILE2.tar.gz | tar -xvf -"
+       
+        date -u
+
+    fi
+
+    exit
+
+    if test -d /exchange/tmp/fab/$TEMPFILE; then # move individual files between the two directories
+        echo " "
+        echo "second hop of directory"
+        echo "ssh $REMOTE_NODE mv $TEMPDIR/$TEMPFILE/* $DESTDIR/$FILENAME"
+              ssh $REMOTE_NODE mv $TEMPDIR/$TEMPFILE/* $DESTDIR/$FILENAME 
+    else
+        echo "second hop of file"
+        echo "ssh $REMOTE_NODE mv $TEMPDIR/$TEMPFILE $DESTDIR/$FILENAME"
+              ssh $REMOTE_NODE mv $TEMPDIR/$TEMPFILE $DESTDIR/$FILENAME 
+    fi
+
+elif test "$3" == "exchange_tar_filelist2"; then
+    echo " "
+    echo "first hop of file list"
+    echo "tar -T $FILENAME -cvf - . | ssh jetscp.rdhpcs.noaa.gov mkdir -p $TEMPDIR/$TEMPFILE; cd $TEMPDIR/$TEMPFILE; tar xpvf -"
+          tar -T $FILENAME -cvf - . | ssh jetscp.rdhpcs.noaa.gov mkdir -p $TEMPDIR/$TEMPFILE; cd $TEMPDIR/$TEMPFILE; tar xpvf -
 
 elif test "$3" == "rsync"; then
     echo "rsync $RSH $RSYNCARGS $FILENAME $REMOTE_NODE:$DESTDIR"                                    
