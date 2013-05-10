8ba1e4884b5be9ed86786ee2f7f9e66d6f6c968a
Steve Albers <Steve.Albers@noaa.gov>
2013-05-10 20:16:31 +0000

    routine for all sky plots, called from lapsplot

---
+++ src/lib/cloud/allsky.f90
@@ -0,0 +1,42 @@
+
+       subroutine cyl_to_polar(cyl,polar,ni_cyl,nj_cyl,alt_a,azi_a,ni_polar,nj_polar)
+
+       use mem_namelist, ONLY: r_missing_data
+
+       real cyl(0:ni_cyl,0:nj_cyl)
+       real polar(ni_polar,nj_polar)
+       real alt_a(ni_polar,nj_polar)
+       real azi_a(ni_polar,nj_polar)
+       real delti,deltj,atan3d
+
+       ri_polar_mid = ((ni_polar - 1.0) / 2.0) + 1.0
+       rj_polar_mid = ((nj_polar - 1.0) / 2.0) + 1.0
+
+       write(6,*)'cyl_to_polar ',ni_polar,nj_polar,ri_polar_mid,rj_polar_mid
+
+       alt_a = r_missing_data
+       azi_a = r_missing_data
+       polar = r_missing_data
+
+       do ip = 1,ni_polar
+       do jp = 1,nj_polar
+           rip = ip; rjp = jp
+           delti = rip-ri_polar_mid
+           deltj = rjp-rj_polar_mid
+           zenith_angle = sqrt(delti**2 + deltj**2) * (90. / ri_polar_mid)
+           alt = 90. - zenith_angle
+           if(alt .ge. 0.)then
+               azi = atan3d(deltj,delti)
+               i_cyl = nint(alt)    
+               j_cyl = nint(azi)    
+               polar(ip,jp) = cyl(i_cyl,j_cyl)
+               alt_a(ip,jp) = alt
+               azi_a(ip,jp) = azi 
+           endif
+       enddo ! jp
+       enddo ! ip
+
+       write(6,*)' alt(1,1) = ',alt_a(1,1)       
+
+       return
+       end
