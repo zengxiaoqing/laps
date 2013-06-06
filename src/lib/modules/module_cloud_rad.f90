dfe92b23e243c4cf523d1a7f7926245b9b79da0e
Steve Albers <Steve.Albers@noaa.gov>
2013-06-06 20:02:46 +0000

    updated 'albedo_to_clouds' routine and made it PUBLIC

--- src/lib/modules/module_cloud_rad.f90
+++ src/lib/modules/module_cloud_rad.f90
@@ -15,20 +15,34 @@ MODULE cloud_rad
       real, parameter :: reff_rain = .001000 ! m
       real, parameter :: reff_snow = .004000 ! m
 
+      PUBLIC albedo_to_clouds
+
 CONTAINS
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
      subroutine albedo_to_clouds(albedo                                  & ! I
                                 ,cloud_rad_trans,cloud_od,cloud_opacity)   ! O
 
+        use mem_namelist, ONLY: r_missing_data
+
         clear_albedo = .2097063
         cloud_albedo = .4485300
 !       cloud_albedo = .40
+
+        if(albedo .eq. r_missing_data)then
+            cloud_rad_trans = r_missing_data
+            cloud_od = r_missing_data
+            cloud_opacity = r_missing_data
+            return
+        endif
+
+!       Uniform clouds are presently assumed in the grid box or cloud layer
+        cloud_frac = 1.0
 
         arg = albedo
 
         call stretch2(clear_albedo,cloud_albedo,0.,1.,arg)
 
-        cloud_albedo = arg
+        cloud_albedo = min(arg,0.99999)
 
         cloud_rad_trans = 1.0 - cloud_albedo
 
