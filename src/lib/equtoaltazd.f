89cd4d367612df03032db516605122c6ba992cb5
Steve Albers <Steve.Albers@noaa.gov>
2013-06-13 21:05:55 +0000

    corrected azimuth calculation

--- src/lib/equtoaltazd.f
+++ src/lib/equtoaltazd.f
@@ -14,7 +14,7 @@
         alt=ASIND (sinphi*sindec+cosphi*cosdec*cosha)
         az =ACOSD((cosphi*sindec-sinphi*cosdec*cosha)/cosd(alt))
 
-        if(ha .gt. 0)az = 6.2831853071796E0 - az
+        if(ha .gt. 0)az = 360.0 - az
 
         return
         end
