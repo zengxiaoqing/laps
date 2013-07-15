3201c0c58245d1c81f73a603d6a915c2b3e8d8a1
Steve Albers <Steve.Albers@noaa.gov>
2013-07-15 17:27:05 +0000

    call 'bilinear_laps_2d' for better cyl to polar interpolation

--- src/lib/cloud/allsky.f90
+++ src/lib/cloud/allsky.f90
@@ -9,6 +9,9 @@
        real azi_a(ni_polar,nj_polar)
        real delti,deltj,atan3d
 
+       real ri_a(ni_polar,nj_polar)
+       real rj_a(ni_polar,nj_polar)
+
        ri_polar_mid = ((ni_polar - 1.0) / 2.0) + 1.0
        rj_polar_mid = ((nj_polar - 1.0) / 2.0) + 1.0
 
@@ -27,14 +30,21 @@
            alt = 90. - zenith_angle
            if(alt .ge. 0.)then
                azi = atan3d(+deltj,delti) ! minus sign on deltj flips left/right
-               i_cyl = nint(alt)    
-               j_cyl = nint(azi)    
-               polar(ip,jp) = cyl(i_cyl,j_cyl)
+!              i_cyl = nint(alt)    
+!              j_cyl = nint(azi) 
+!              polar(ip,jp) = cyl(i_cyl,j_cyl)
+               ri_a(ip,jp) = alt + 1.0   
+               rj_a(ip,jp) = azi + 1.0   
                alt_a(ip,jp) = alt
                azi_a(ip,jp) = azi 
            endif
        enddo ! jp
        enddo ! ip
+
+       imax = ni_cyl+1
+       jmax = nj_cyl+1
+
+       call bilinear_laps_2d(ri_a,rj_a,imax,jmax,ni_polar,nj_polar,cyl,polar)
 
        write(6,*)' alt(1,1) = ',alt_a(1,1)       
 
