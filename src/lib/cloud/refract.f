02d0e039299b992cb59fdec02eda272a29889060
Steve Albers <Steve.Albers@noaa.gov>
2013-08-01 19:23:03 +0000

    first version of 'refract' routine

---
+++ src/lib/cloud/refract.f
@@ -0,0 +1,13 @@
+
+        subroutine refract(true,apparent,pres)
+
+        real*8 true,apparent,pres
+
+        if(true .gt. -1.0d0)then
+            r = 1.02d0/ dtand(true+10.3d0/(true+5.11d0))
+            apparent = true + (r/60. * pres/1013.d0)
+        else
+            apparent = 0.
+        endif
+
+        end
