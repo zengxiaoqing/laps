67fd200f19cdd216eb72cebbc223ceb3d2dd5820
Steve Albers <Steve.Albers@noaa.gov>
2013-08-01 19:21:00 +0000

    fill up 'aod_3d' array with extinction coefficients that decrease              exponentially with a 1.5km scale height

--- src/lib/cloud/get_aod_3d.f90
+++ src/lib/cloud/get_aod_3d.f90
@@ -1,22 +1,40 @@
 
-       subroutine get_aod_3d(pres_3d,ni,nj,nk,aod_3d)
+       subroutine get_aod_3d(pres_3d,heights_3d,topo_2d,ni,nj,nk,aod_3d)
 
        use mem_namelist, ONLY: aod
 
-       real aod_3d(ni,nj,nk)
+       real aod_3d(ni,nj,nk) ! aerosol extinction coefficient
        real pres_3d(ni,nj,nk)
+       real heights_3d(ni,nj,nk)
+       real topo_2d(ni,nj)
 
-       do k = 1,nk
+       gas_scale_height = 8000.
+       aero_scale_height = 1500.
+
+       pref = 85000.
+
+       write(6,*)' subroutine get_aod_3d: aod = ',aod
+
        do i = 1,ni
        do j = 1,nj
-           if(pres_3d(i,j,k) .gt. 45000.)then
-               aod_3d(i,j,k) = aod * 2.0
-           else
-               aod_3d(i,j,k) = aod * 0.0
+         sum_aod = 0.
+         do k = 1,nk
+           pratio = pres_3d(i,j,k) / pref
+           h_agl = heights_3d(i,j,k) - topo_2d(i,j)
+           aod_3d(i,j,k) = aod/aero_scale_height * exp(-h_agl/aero_scale_height)
+
+           if(i .eq. ni/2 .and. j .eq. nj/2)then
+               if(h_agl .gt. 0.)then
+                   ave_aod = 0.5 * (aod_3d(i,j,k)+aod_3d(i,j,k-1))
+                   sum_aod = sum_aod + ave_aod       & 
+                           * (heights_3d(i,j,k) - heights_3d(i,j,k-1))
+               endif
+               write(6,101)k,h_agl,aod_3d(i,j,k),sum_aod
+101            format('k,agl,aod_3d,sum',i5,f9.1,e15.8,f9.5)
            endif
+         enddo ! k
        enddo ! j
        enddo ! i
-       enddo ! k
 
        return
        end
