4e2340901b7c79e64d57c8462577000c27dcb456
Steve Albers <Steve.Albers@noaa.gov>
2013-09-05 19:53:05 +0000

    further changes to facilitate dynamic grid adjustments

--- src/lib/cloud/allsky.f90
+++ src/lib/cloud/allsky.f90
@@ -1,5 +1,5 @@
 
-       subroutine cyl_to_polar(cyl,polar,minalt,maxalt,nj_cyl,alt_a,azi_a,ni_polar,nj_polar)
+       subroutine cyl_to_polar(cyl,polar,minalt,maxalt,nj_cyl,alt_scale,azi_scale,alt_a,azi_a,ni_polar,nj_polar)
 
        use mem_namelist, ONLY: r_missing_data
 
@@ -12,10 +12,18 @@
        real ri_a(ni_polar,nj_polar)
        real rj_a(ni_polar,nj_polar)
 
+       altmin = float(minalt) * alt_scale
+
        ri_polar_mid = ((ni_polar - 1.0) / 2.0) + 1.0
        rj_polar_mid = ((nj_polar - 1.0) / 2.0) + 1.0
 
        write(6,*)'cyl_to_polar ',ni_polar,nj_polar,ri_polar_mid,rj_polar_mid
+       write(6,*)'minalt,maxalt,nj_cyl',minalt,maxalt,nj_cyl
+       write(6,*)'alt_scale,azi_scale',alt_scale,azi_scale
+
+       do iaz = 0,nj_cyl,20
+           write(6,*)'iaz,cyl(maxalt/2,iaz)',iaz,cyl(maxalt/2,iaz)
+       enddo ! iaz
 
        alt_a = r_missing_data
        azi_a = r_missing_data
@@ -33,8 +41,11 @@
 !              i_cyl = nint(alt)    
 !              j_cyl = nint(azi) 
 !              polar(ip,jp) = cyl(i_cyl,j_cyl)
-               ri_a(ip,jp) = (alt-float(minalt)) + 1.0 ! real I index in CYL array, offset to start at 1
-               rj_a(ip,jp) =  azi                + 1.0 ! real J index in CYL array, offset to start at 1
+               ri_a(ip,jp) = (alt-altmin) / alt_scale + 1.0 ! real I index in CYL array, offset to start at 1
+               rj_a(ip,jp) =  azi         / azi_scale + 1.0 ! real J index in CYL array, offset to start at 1
+!              if(jp .eq. nint(rj_polar_mid))then
+!                  write(6,*)'ip,jp,ri,rj',ip,jp,ri_a(ip,jp),rj_a(ip,jp)
+!              endif
                alt_a(ip,jp) = alt
                azi_a(ip,jp) = azi 
            endif
