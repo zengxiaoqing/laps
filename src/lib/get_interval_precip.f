7bfaae6fb5ace6c7157021fa101a0237aede7aca
Steve Albers <Steve.Albers@noaa.gov>
2013-09-25 19:15:36 +0000

    improvements for reading analysis fields
                 added exceptions for NAM-NH 00Z and 12Z model runs

--- src/lib/get_interval_precip.f
+++ src/lib/get_interval_precip.f
@@ -5,8 +5,8 @@
      1                                ,r_missing_data
      1                                ,ni,nj,nf,pcp_2d,istatus)
 
-!       This is written for just one field at present, though it can be
-!       readily expanded to two fields. 
+!       Works for analysis, forecast, or Stage IV                        
+!       Special exception for 'nam-nh'  
 
         real pcp_2d(ni,nj,nf)
         real pcp_buf_2d(ni,nj,nf)
@@ -27,31 +27,48 @@
         call make_fnam_lp(i4time_end  ,a9_end  ,istatus)   
 
         pcp_2d = 0.
+        c_model = ' '
 
         if(trim(ext) .eq. 'st4')then
             var_2d(1) = 'ppt'       
             ipcp_cycle_time = 3600         
-            i4_loop_start = i4time_start+ipcp_cycle_time
-            i4_loop_end   = i4time_end
-            i4_loop_int   = ipcp_cycle_time
         elseif(nf .eq. 2)then
             var_2d(1) = 'R01'
             var_2d(2) = 'S01'
             ipcp_cycle_time = laps_cycle_time
-            i4_loop_start = i4time_start+ipcp_cycle_time
-            i4_loop_end   = i4time_end
-            i4_loop_int   = ipcp_cycle_time
-        else ! e.g. fsf
+        elseif(trim(ext) .eq. 'l1s')then
+            var_2d(1) = c_pcp(1:1)//'01'
+            ipcp_cycle_time = laps_cycle_time 
+        else ! i.e. fsf
             c_model = ext
             ext = 'fsf'
             var_2d(1) = c_pcp(1:1)//'01'
             ipcp_cycle_time = laps_cycle_time ! from model_fcst_intvl
-            i4_loop_start = i4time_start+ipcp_cycle_time
-            i4_loop_end   = i4time_end
-            i4_loop_int   = ipcp_cycle_time
         endif
 
-        write(6,*)' get_interval_precip ext/var: ',ext,' ',var_2d(1)
+        i4_loop_start = i4time_start+ipcp_cycle_time
+        i4_loop_end   = i4time_end
+        i4_loop_int   = ipcp_cycle_time
+
+!       Exception for 'nam-nh' (precip convention depends on file time)
+        if(trim(c_model) .eq. 'nam-nh')then
+            i4_fcst_end = i4time_end - i4_model_init
+            if(mod(i4_fcst_end,43200) .eq. 0 .AND. 
+     1         mod(i4_model_init,43200) .eq. 0 .AND.
+     1         i4time_end-i4time_start .eq. 21600)then
+                write(6,*)' nam-nh exception 1'
+                i4_loop_start = i4time_start
+                i4_loop_end   = i4time_end
+                i4_loop_int   = ipcp_cycle_time
+            else
+                write(6,*)' nam-nh situation not handled...'
+                istatus = 0
+                return
+            endif
+        endif
+
+        write(6,*)' get_interval_precip ext_in/ext/var: '
+     1                     ,trim(ext_in),' ',trim(ext),' ',var_2d(1)
         write(6,*)' get_interval_precip times: ',a9_start,' ',a9_end
 
         i4time_interval = i4time_end - i4time_start
@@ -111,17 +128,25 @@ C    abdel
             enddo ! i
 
             do i = 1,nf
-                call add_miss(pcp_2d(1,1,nf),pcp_buf_2d(1,1,nf)
-     1                       ,pcp_2d(1,1,nf),ni,nj)
-!               call add_miss(pcp_2d,pcp_buf_2d
-!    1                       ,pcp_2d,ni,nj)
+                if(trim(c_model) .eq. 'nam-nh')then
+                    if(mod(i4_fcst_end,43200)   .eq. 0  .AND. 
+     1                 mod(i4_model_init,43200) .eq. 0  .AND.
+     1                 i4time_end-i4time_start  .eq. 21600 .AND.
+     1                 i4time .eq. i4time_start                )then
+                        write(6,*)' nam-nh exception 2: negate precip'
+                        pcp_buf_2d(:,:,i) = -pcp_buf_2d(:,:,i)
+                    endif
+                endif
+                call add_miss(pcp_2d(1,1,i),pcp_buf_2d(1,1,i)
+     1                       ,pcp_2d(1,1,i),ni,nj)
             enddo ! i
 
         enddo ! i4time
 
         do i = 1,nf
-            call array_range(pcp_2d,ni,nj,rmin,rmax,r_missing_data)
-            write(6,*)' get_interval_precip total range: ',nf,rmin,rmax
+            call array_range(pcp_2d(1,1,i),ni,nj,rmin,rmax
+     1                                          ,r_missing_data)
+            write(6,*)' get_interval_precip total range: ',i,rmin,rmax
         enddo ! i
 
         return
