f1b7c0a9a91e639080afaa8c3ef61b523282e1c6
Steve Albers <Steve.Albers@noaa.gov>
2014-02-21 19:34:02 +0000

    initial setup of routine to write out PNG files

---
+++ src/lapsplot/hsect_img.f
@@ -0,0 +1,11 @@
+
+        subroutine hsect_img(i4time_ref,lun,NX_L,NY_L,NZ_L
+     1                      ,r_missing_data,laps_cycle_time
+     1                      ,plot_parms,namelist_parms,ifield_found)
+
+        real field_2d(NX_L,NY_L)
+
+        write(6,*)' Subroutine hsect_img'
+
+        return
+        end
