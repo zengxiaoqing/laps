371d913d456a2aab39416c011d52b8517f2d0cb6
Steve Albers <Steve.Albers@noaa.gov>
2014-03-27 22:21:00 +0000

    improved filtering for resolutions < 750m

--- src/ingest/radar/mosaic/clean_radar.f
+++ src/ingest/radar/mosaic/clean_radar.f
@@ -14,18 +14,26 @@
           return
       else
           write(6,*)' Clean radar data from small-scale noise'
+          if(grid_spacing_m .gt. 750.)then
+              ib = 1
+          else
+              ib = 2
+          endif
       endif
 
       nclean = 0
+      rnumbox = float((2*ib+1)**2 - 1)
 
       do k = 1,nk
-      do j = 2,nj-1
-      do i = 2,ni-1
-          sum9 = sum(ref(i-1:i+1,j-1:j+1,k))
+      do j = 1+ib,nj-ib
+      do i = 1+ib,ni-ib
+          sum9 = sum(ref(i-ib:i+ib,j-ib:j+ib,k))
           if(abs(sum9) .le. 1e6)then ! screen out summed missing data values
               sum8 = sum9 - ref(i,j,k)
-              ave8 = sum8 / 8.
-              if(ref(i,j,k) .gt. ave8 + 20.)then
+              ave8 = sum8 / rnumbox
+              if(ref(i,j,k) .gt. (ave8 + 20.) .OR.
+     1           (ave8 .eq. ref_base .and. grid_spacing_m .le. 750.) 
+     1                                                             )then        
                   ref_buf(i,j,k) = ave8 ! ref_base
                   nclean = nclean + 1
               endif
