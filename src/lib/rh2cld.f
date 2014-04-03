1f52ca846419fa92d150dfc16baeaf462f52b90b
Steve Albers <Steve.Albers@noaa.gov>
2014-04-03 23:21:37 +0000

    increased start of humidity/cloud ramp to 80% to reduce the              coverage of clouds seen in the global laps run

--- src/lib/rh2cld.f
+++ src/lib/rh2cld.f
@@ -9,7 +9,7 @@
 !       It is recommended that the input RH be calculated with subroutine
 !       'make_rh' using a 't_ref' between 0C and -10C.
 
-        ramp_thresh = 0.70
+        ramp_thresh = 0.80
 
         if(rh .lt. ramp_thresh)then
             rh_to_cldcv = 0.0
