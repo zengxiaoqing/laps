5ac98c3e1d853d75f27181a47b2f808e561e34f8
Steve Albers <Steve.Albers@noaa.gov>
2014-04-03 23:22:50 +0000

    added call to 'writeppm3Matrix' for cloud albedo PPM image

--- src/lapsplot/hsect_img.f
+++ src/lapsplot/hsect_img.f
@@ -3,7 +3,10 @@
      1                      ,r_missing_data,laps_cycle_time
      1                      ,plot_parms,namelist_parms,ifield_found)
 
+        use ppm
+
         real field_2d(NX_L,NY_L)
+        integer isky_rgb_cyl(3,NX_L,NY_L)
         character*31 ext
         character*4 var_2d
         character*20 units_2d
@@ -18,11 +21,19 @@
      1                       ext,var_2d,units_2d,comment_2d,
      1                       NX_L,NY_L,field_2d,0,istatus)
 
+        if(istatus .ne. 1)then
+            write(6,*)' Skipping image write in hsect_img'
+        endif
+
+        do ic = 1,3
+            isky_rgb_cyl(ic,:,:) = nint(field_2d(:,:) * 255.)
+        enddo ! ic
+
 !       Write as PNG
-        write(6,*)' Write all sky cyl ppm file '
-!       call writeppm3Matrix(
-!    1             isky_rgb_cyl(0,:,:),isky_rgb_cyl(1,:,:)
-!    1            ,isky_rgb_cyl(2,:,:),'allsky_rgb_cyl_'//clun)
+        write(6,*)' Write cloud albedo ppm file '
+        call writeppm3Matrix(
+     1             isky_rgb_cyl(1,:,:),isky_rgb_cyl(2,:,:)
+     1            ,isky_rgb_cyl(3,:,:),'cloud_albedo')
 
 
         return
