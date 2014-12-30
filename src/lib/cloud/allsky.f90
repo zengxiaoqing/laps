d98b4ef5d768c5afc7e964c39683ec0f27bee592
albers <steve.albers@noaa.gov>
2014-12-30 18:48:25 +0000

    added bounded azimuth support to subroutine 'cyl_to_polar'

--- src/lib/cloud/allsky.f90
+++ src/lib/cloud/allsky.f90
@@ -1,9 +1,9 @@
 
-       subroutine cyl_to_polar(cyl,polar,minalt,maxalt,nj_cyl,alt_scale,azi_scale,alt_a,azi_a,ni_polar,nj_polar)
+       subroutine cyl_to_polar(cyl,polar,minalt,maxalt,minazi,maxazi,alt_scale,azi_scale,alt_a,azi_a,ni_polar,nj_polar)
 
        use mem_namelist, ONLY: r_missing_data
 
-       real cyl(minalt:maxalt,0:nj_cyl)
+       real cyl(minalt:maxalt,minazi:maxazi)
        real polar(ni_polar,nj_polar)
        real alt_a(ni_polar,nj_polar)
        real azi_a(ni_polar,nj_polar)
@@ -13,17 +13,21 @@
        real rj_a(ni_polar,nj_polar)
 
        altmin = float(minalt) * alt_scale
+       azimin = float(minazi) * azi_scale
 
        ri_polar_mid = ((ni_polar - 1.0) / 2.0) + 1.0
        rj_polar_mid = ((nj_polar - 1.0) / 2.0) + 1.0
 
        write(6,*)'cyl_to_polar ',ni_polar,nj_polar,ri_polar_mid,rj_polar_mid
-       write(6,*)'minalt,maxalt,nj_cyl',minalt,maxalt,nj_cyl
+       write(6,*)'minalt,maxalt,minazi,maxazi',minalt,maxalt,minazi,maxazi
        write(6,*)'alt_scale,azi_scale',alt_scale,azi_scale
+       write(6,*)'altmin,azimin',altmin,azimin
 
-       do iaz = 0,nj_cyl,20
-           write(6,*)'iaz,cyl(maxalt/2,iaz)',iaz,cyl(maxalt/2,iaz)
-       enddo ! iaz
+!      do iaz = minazi,maxazi,20
+!          write(6,*)'iaz,cyl(maxalt/2,iaz)',iaz,cyl(maxalt/2,iaz)
+!      enddo ! iaz
+
+       write(6,*)' cyl_to_polar: shape of cyl is ',shape(cyl)
 
        alt_a = r_missing_data
        azi_a = r_missing_data
@@ -37,15 +41,25 @@
            zenith_angle = sqrt(delti**2 + deltj**2) * (90. / ri_polar_mid)
            alt = 90. - zenith_angle
            if(alt .ge. 0.)then
+               azi_last = azi
                azi = atan3d(+deltj,delti) ! minus sign on deltj flips left/right
 !              i_cyl = nint(alt)    
 !              j_cyl = nint(azi) 
 !              polar(ip,jp) = cyl(i_cyl,j_cyl)
                ri_a(ip,jp) = (alt-altmin) / alt_scale + 1.0 ! real I index in CYL array, offset to start at 1
-               rj_a(ip,jp) =  azi         / azi_scale + 1.0 ! real J index in CYL array, offset to start at 1
+               rj_a(ip,jp) = (azi-azimin) / azi_scale + 1.0 ! real J index in CYL array, offset to start at 1
 !              if(jp .eq. nint(rj_polar_mid))then
-!                  write(6,*)'ip,jp,ri,rj',ip,jp,ri_a(ip,jp),rj_a(ip,jp)
-!              endif
+!              if((azi-50.) * (azi_last-50.) .lt. 0. .OR. ip .eq. 257)then ! cross 50.
+               if((azi-50.) * (azi_last-50.) .lt. 0. .AND. jp .gt. 265)then ! cross 50.
+                 icyl = nint(ri_a(ip,jp)) + minalt
+                 jcyl = nint(rj_a(ip,jp)) + minazi
+!                if(icyl .ge. minalt .and. icyl .le. maxalt .and. jcyl .ge. minazi .and. jcyl .le. maxazi)then
+!                  write(6,1)ip,jp,alt,azi,ri_a(ip,jp),rj_a(ip,jp),icyl,jcyl,cyl(icyl,jcyl)
+!                else
+!                  write(6,1)ip,jp,alt,azi,ri_a(ip,jp),rj_a(ip,jp),icyl,jcyl
+!                endif
+ 1               format('ip/jp/alt/azi/ri/rj/icyl/jcyl/cyl',2i5,2f9.2,2f9.2,2i5,f9.1)
+               endif
                alt_a(ip,jp) = alt
                azi_a(ip,jp) = azi 
            endif
@@ -53,9 +67,12 @@
        enddo ! ip
 
        imax = maxalt-minalt+1
-       jmax = nj_cyl+1
+       jmax = maxazi-minazi+1
+       write(6,*)' imax/jmax = ',imax,jmax
 
        call bilinear_laps_2d(ri_a,rj_a,imax,jmax,ni_polar,nj_polar,cyl,polar)
+
+!      write(6,*)' polar array',polar(ni_polar/2,1:nj_polar)
 
        write(6,*)' alt(1,1) = ',alt_a(1,1)       
 
