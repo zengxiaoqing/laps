4f8847242069b334223e5aa34aa7be66f43678c5
albers <steve.albers@noaa.gov>
2015-02-02 22:55:03 +0000

    'get_airmass' split off from 'get_sky_rgb_routines.f90' and              improved for high vantage points, as well as for ozone

---
+++ src/lib/cloud/get_airmass.f90
@@ -0,0 +1,160 @@
+
+        subroutine get_airmass(alt,htmsl,patm &          ! I (add htagl?)
+                              ,aero_refht,aero_scaleht & ! I
+                              ,earth_radius,iverbose &   ! I
+                              ,ag,ao,aa)                 ! O
+
+!       Airmasses relative to zenith at sea level pressure (ag - gas)
+!       Airmasses relative to zenith at sea level pressure (ao - ozone)
+!       Airmasses relative to zenith at aero refht         (aa - aerosol)
+
+        use mem_namelist, ONLY: r_missing_data, earth_radius
+
+        include 'trigd.inc'
+        include 'rad.inc'
+
+        zapp = 90. - alt
+        zappi = 90. + alt
+
+        ztrue = zapp + refractd_app(alt ,patm)
+        ztruei = zappi + refractd_app(abs(alt) ,patm)
+
+        if(iverbose .eq. 1)then
+          write(6,11)alt,htmsl,patm
+11        format(/' start get_airmass: alt/htmsl/patm' &
+                 ,f8.2,f11.1,f9.4)
+        endif
+
+        call get_htmin(alt,patm,htmsl,earth_radius,iverbose & ! I
+                      ,patm2,htmin)                           ! O
+
+!       Gas component for Rayleigh Scattering
+        if(alt .lt. -3.)then ! high looking down
+          if(htmin .lt. 0.0)then ! hit ground
+            patm_refht = ztopsa(aero_refht) / 1013.25
+            ag = airmassf(zappi,(patm_refht-patm))
+            if(iverbose .eq. 1)write(6,21)zappi,patm_refht,patm,ag
+21          format('  high gas   hitting ground',4f9.4)
+          else ! grazes atmosphere
+            ztrue0 = 90. + refractd_app(0.,patm2)
+!           alttrue0 = -refractd_app(0.,patm2)
+            ag = (airmassf(ztruei,patm2)-airmassf(ztruei,patm)) &
+                + airmassf(ztrue0,patm2)
+            if(iverbose .eq. 1)then
+              write(6,*)' high gas   looking down at htmin',htmin,ag
+            endif
+          endif
+        else ! standard situation
+          ag = airmassf(ztrue,patm)
+          if(iverbose .eq. 1)then
+            write(6,*)' standard gas   situation',htmsl,alt,ag
+          endif
+        endif
+
+!       Ozone component
+        if(alt .lt. 0.)then ! high looking down
+          if(htmin .lt. 0.0)then ! hit ground
+            patm_refht = patm_o3(aero_refht)
+            ao = airmasso(zappi,aero_refht)  &
+               * (patm_o3(aero_refht)-patm_o3(htmsl))
+            if(iverbose.eq.1)write(6,31)zappi,patm_refht,patm_o3(htmsl),ao
+31          format('  high ozone hitting ground',4f9.4)
+          else ! grazes atmosphere
+            patmo  = patm_o3(htmsl)
+            patm2o = patm_o3(htmin)
+            ztrue0 = 90. + refractd_app(0.,patm2o)
+!           alttrue0 = -refractd_app(0.,patm2o)
+            ao1 = airmasso(ztruei,patm2o) * patm2o
+            ao2 = airmasso(ztruei,patmo)  * patmo
+            ao3 = airmasso(ztrue0,patm2o) * patm2o
+            ao = (ao1-ao2) + ao3
+            if(iverbose .eq. 1)then
+              write(6,*)' high ozone looking down at htmin',htmin,ao
+            endif
+          endif
+        else
+          ao = airmasso(zapp,htmsl) * patm_o3(htmsl)
+          if(iverbose .eq. 1)then
+            write(6,*)' standard ozone situation',htmsl,alt,ao
+          endif
+        endif
+
+!       Aerosol component
+
+!       Note that near the horizon the aerosol airmass should
+!       exceed the gas component by the inverse square root of the
+!       respective scale heights.
+
+        patm_aero = exp(-((htmsl-aero_refht) / aero_scaleht))
+        if(alt .lt. 0.)then ! high looking down
+          if(htmin .lt. 0.0)then ! hit ground
+            ZZ = zappi * rpd
+            aa=1./(COS(ZZ)+.0123*EXP(-24.5*COS(ZZ))) * (1.0-patm_aero)
+            if(iverbose .eq. 1)then
+              write(6,41)zappi,htmin,patm_aero,aa    
+41            format('  high aero  hitting ground',4f9.4)           
+            endif
+          else ! grazes atmosphere
+            ZZ = zappi * rpd
+            patm2_aero = exp(-((htmin-aero_refht) / aero_scaleht))
+            aa1=1./(COS(ZZ)+.0123*EXP(-24.5*COS(ZZ))) * patm2_aero
+            aa2=1./(COS(ZZ)+.0123*EXP(-24.5*COS(ZZ))) * patm_aero
+            ZZ = 90. * rpd
+            aa3=1./(COS(ZZ)+.0123*EXP(-24.5*COS(ZZ))) * patm2_aero
+            aa = (aa1-aa2) + aa3
+            if(iverbose .eq. 1)then
+              write(6,*)' high aero  looking down at htmin',htmin,aa 
+            endif
+          endif
+        else
+          ZZ = (min(zapp,90.)) * rpd
+          aa=1./(COS(ZZ)+.0123*EXP(-24.5*COS(ZZ))) * patm_aero
+          if(iverbose .eq. 1)then
+            write(6,*)' standard aero  situation',htmsl,alt,aa       
+          endif
+        endif
+
+        if(iverbose .eq. 1)write(6,*)' returning from get_airmass'
+
+        return
+        end
+
+        subroutine get_htmin(alt,patm,htmsl,earth_radius,iverbose & ! I
+                            ,patm2,htmin)                           ! O
+
+!       Find minimum MSL height of a light ray compared to Earth sfc
+!       Refraction is taken into account
+
+        parameter (pi=3.14159265)
+        parameter (rpd=pi/180.)
+
+        if(alt .lt. 0)then
+          patm2 = patm
+          niter = 3; iter = 1
+          do while (iter .le. niter)
+!           rk and htmin could use more accuracy when htmsl is high
+            wtm = 0.5 + 0.5 * (1.-patm/patm2)
+            wtm2 = (1.-wtm)
+            rk = (patm*wtm+patm2*wtm2) * 0.13 ! curvature at midpoint of ray
+            erad_eff = earth_radius / (1. - rk)
+            htmin_s = htmsl - erad_eff * (alt*rpd)**2 / 2.
+            htmin_l = ((erad_eff+htmsl) * cosd(alt)) - erad_eff    
+            if(alt .gt. -0.25)then ! small angle forumla
+              htmin = htmin_s
+            else                 ! large angle formula
+              htmin = htmin_l  
+            endif
+            htmin = max(htmin,-500.)
+            patm2 = ztopsa(htmin) / 1013.25
+            if(iverbose .eq. 1)then
+              write(6,1)iter,htmsl,alt,cosd(alt),htmin_s,htmin_l,htmin,patm,patm2,rk
+            endif
+1           format('    iter/htmsl/alt/cosd/htmin/patm/patm2/rk = ',i4,f10.1,2f9.3,2f11.0,f11.1,2f10.7,f9.5)
+            iter = iter + 1
+          enddo ! while
+        else
+          htmin = htmsl
+        endif
+
+        return
+        end
