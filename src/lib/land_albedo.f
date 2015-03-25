f601276ce8adeba2bb1804c52e30ce846017b68a
albers <steve.albers@noaa.gov>
2015-03-25 22:08:20 +0000

    refined land albedo values

--- src/lib/land_albedo.f
+++ src/lib/land_albedo.f
@@ -18,45 +18,45 @@
           elseif(lu(i,j) .eq. 2.)then  ! Dryland Cropland/Pasture  (.17 Brown)
               albedo(1,i,j) = 0.17
               albedo(2,i,j) = 0.17
-              albedo(3,i,j) = 0.01
+              albedo(3,i,j) = 0.10
           elseif(lu(i,j) .eq. 3.)then  ! Irrigtd Cropland/Pasture  (.18 Green)
               albedo(1,i,j) = 0.13
               albedo(2,i,j) = 0.18
               albedo(3,i,j) = 0.09
           elseif(lu(i,j) .eq. 4.)then  ! Mixed                     (.18 Green)
-              albedo(1,i,j) = 0.13
+              albedo(1,i,j) = 0.14
               albedo(2,i,j) = 0.18
               albedo(3,i,j) = 0.09
           elseif(lu(i,j) .eq. 5.)then  ! Cropland Grassland Mosaic (.18 Green)
-              albedo(1,i,j) = 0.13
+              albedo(1,i,j) = 0.14
               albedo(2,i,j) = 0.18
               albedo(3,i,j) = 0.09
           elseif(lu(i,j) .eq. 6.)then  ! Cropland Woodland Mosaic  (.16 Green)
-              albedo(1,i,j) = 0.12
+              albedo(1,i,j) = 0.13
               albedo(2,i,j) = 0.16
               albedo(3,i,j) = 0.08
           elseif(lu(i,j) .eq. 7.)then  ! Grassland                 (.19 Brown)
               albedo(1,i,j) = 0.19
               albedo(2,i,j) = 0.19
-              albedo(3,i,j) = 0.01
+              albedo(3,i,j) = 0.10
           elseif(lu(i,j) .eq. 8.)then  ! Shrubland                 (.22 Brown)
               albedo(1,i,j) = 0.22
               albedo(2,i,j) = 0.22
-              albedo(3,i,j) = 0.01
+              albedo(3,i,j) = 0.10
           elseif(lu(i,j) .eq. 9.)then  ! Mixed Shrubland/Grassland (.20 Brown)
               albedo(1,i,j) = 0.20
               albedo(2,i,j) = 0.20
-              albedo(3,i,j) = 0.01
+              albedo(3,i,j) = 0.10
           elseif(lu(i,j) .eq. 10.)then ! Savanna                   (.20 Brown)
               albedo(1,i,j) = 0.20
               albedo(2,i,j) = 0.20
-              albedo(3,i,j) = 0.01
+              albedo(3,i,j) = 0.10
           elseif(lu(i,j) .eq. 11.)then ! Deciduous Broadleaf       (.16 Green)
-              albedo(1,i,j) = 0.12
+              albedo(1,i,j) = 0.13
               albedo(2,i,j) = 0.16
               albedo(3,i,j) = 0.08
           elseif(lu(i,j) .eq. 12.)then ! Deciduous Needleleaf      (.14 Green)
-              albedo(1,i,j) = 0.10
+              albedo(1,i,j) = 0.11
               albedo(2,i,j) = 0.14
               albedo(3,i,j) = 0.07
           elseif(lu(i,j) .eq. 13.)then ! Evergreen Broadleaf       (.12 Green)
@@ -72,9 +72,9 @@
               albedo(2,i,j) = 0.10
               albedo(3,i,j) = 0.05
           elseif(lu(i,j) .eq. 16.)then ! water (.08)
-              albedo(1,i,j) = 0.04
-              albedo(2,i,j) = 0.04
-              albedo(3,i,j) = 0.12
+              albedo(1,i,j) = 0.08
+              albedo(2,i,j) = 0.08
+              albedo(3,i,j) = 0.08
           elseif(lu(i,j) .eq. 19.)then ! Barren or Sparsely Vegetated (.25 Brown)
               albedo(1,i,j) = 0.25
               albedo(2,i,j) = 0.15
@@ -82,7 +82,7 @@
           else ! default
               albedo(1,i,j) = 0.19
               albedo(2,i,j) = 0.19
-              albedo(3,i,j) = 0.01
+              albedo(3,i,j) = 0.10
           endif                        
       enddo ! j
       enddo ! i
