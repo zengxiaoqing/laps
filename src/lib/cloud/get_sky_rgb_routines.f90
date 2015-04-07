8a561e6abccfb5e204cab1b505b6b97285549340
albers <steve.albers@noaa.gov>
2015-04-07 19:09:18 +0000

    elaborated on airglow in 'get_clr_rad_nt'
                 call more general purpose 'angcoord_2d' routine for coordinate
                 conversions
    
                 added zodiacal light and galactic glow to 'get_starglow'
    
                 calculate 'rad_stars', though not yet passed out
    
                 turned off extinction in 'get_glow_obj'
    
                 only calculate 'sky_rad_ave' above the horizontal
    
                 added models for zodiacal light (including F corona), along with
                 galactic light from the Milky Way

--- src/lib/cloud/get_sky_rgb_routines.f90
+++ src/lib/cloud/get_sky_rgb_routines.f90
@@ -1,10 +1,10 @@
 
-        subroutine get_clr_rad_nt(alt,azi,obs_glow_zen,clear_rad_c_nt)
+        subroutine get_clr_rad_nt(alt,azi,obs_glow_zen,patm,htmsl,clear_rad_c_nt)
 
         include 'trigd.inc'
 
-        real clear_rad_c_nt(3)      ! HSV night sky brightness
-                                    ! Nanolamberts
+        real clear_rad_c_nt(3)      ! night sky brightness
+                                    ! 3 color radiance (Nanolamberts)
 
         z = min(90. - alt,91.)        
         airmass = 1. / (cosd(z) + 0.025 * exp(-11 * cosd(z)))
@@ -13,12 +13,19 @@
         sat_ramp = 1.0
         sat_twi_ramp = 0.4
 
-        rint_alt_ramp = sqrt(airmass)
-
-        airglow = 75.  ! nL (range from ~60-90 with solar cycle)
+        if(htmsl .lt. 100000.)then ! below airglow
+          airglow = 75.  ! nL (range from ~60-90 with solar cycle)
+          rint_alt_ramp = sqrt(airmass)
+        elseif(alt .lt. -12)then ! atmosphere from above airglow
+          airglow = 75.
+          rint_alt_ramp = 1.
+        else
+          airglow = 0.
+          rint_alt_ramp = 1.
+        endif
 
 !       glow_lp = 500. ! from city lights (nL)
-        glow_lp = obs_glow_zen + airglow ! from city lights + airglow (nL)
+        glow_lp = obs_glow_zen * patm + airglow ! from city lights + airglow (nL)
         glow_alt = glow_lp * rint_alt_ramp
 
 !       HSI
@@ -26,32 +33,61 @@
         clear_rad_c_nt(1) = hue                             ! Hue
         clear_rad_c_nt(2) = abs(hue-0.5) * 0.8 * sat_ramp   &                        
                                              * sat_twi_ramp ! Sat
-        clear_rad_c_nt(3) = glow_alt                        ! Int
+
+!       Now returned as radiance (nL)
+        clear_rad_c_nt(:) = glow_alt                        ! Int
 
         return
         end
        
-        subroutine get_starglow(i4time,alt_a,azi_a,minalt,maxalt,minazi,maxazi,rlat,rlon,alt_scale,azi_scale,glow_stars)
+        subroutine get_starglow(i4time,alt_a,azi_a,minalt,maxalt,minazi,maxazi,rlat,rlon,alt_scale,azi_scale,horz_dep,glow_stars)
+
+        include 'rad.inc'
+
+        wm2srum_to_s10(x) = x / 1.261e-8 ! 550nm
+        s10_to_wm2srum(x) = x * 1.261e-8 ! 550nm
+!       wm2sr_to_nl(x) = 2145.707e9 * x
+        wm2_to_wm2nm(x)     = 0.00136 * x ! solar 550nm approx
+        wm2sr_to_wm2srnm(x) = 0.00136 * x ! solar 550nm approx
+        wm2sr_to_wm2srum(x) = x * 1.36 ! solar 550nm approx
+        wm2srum_to_wm2sr(x) = x / 1.36 ! solar 550nm approx
+!       wm2srum_to_nl(x) = wm2sr_to_nl(wm2srum_to_wm2sr(x)) ! sol 550nm
+!       s10_to_nl(x) = wm2srum_to_nl(s10_to_wm2srum(x)) ! solar 550nm
+
 
         real alt_a(minalt:maxalt,minazi:maxazi)
         real azi_a(minalt:maxalt,minazi:maxazi)
         real glow_stars(3,minalt:maxalt,minazi:maxazi) ! log nL
+        real rad_stars(3,minalt:maxalt,minazi:maxazi) ! nL
+
+!       Local arrays
+        real dec_a(minalt:maxalt,minazi:maxazi)
+        real ha_a(minalt:maxalt,minazi:maxazi)
+        real ra_a(minalt:maxalt,minazi:maxazi)
+        real gallat_a(minalt:maxalt,minazi:maxazi)
+        real gallon_a(minalt:maxalt,minazi:maxazi)
+        real helioeclipticlat_a(minalt:maxalt,minazi:maxazi)
+        real helioeclipticlon_a(minalt:maxalt,minazi:maxazi)
+
+        character*20 c_conv
 
         parameter (nstars = 2000)
         real dec_d(nstars),ra_d(nstars),mag_stars(nstars),bmv(nstars)
         real alt_stars(nstars),azi_stars(nstars),ext_mag(nstars),lst_deg
-        real*8 angdif,jed,r8lon,lst,has(nstars),phi,als,azs,ras,x,y,decr
+        real angdif
+        real*8 dangdif,jed,r8lon,lst,has(nstars),phi,als,azs,ras,x,y,decr
+        real*8 dalt,dazi,dha,ddec,dra,sol_meanlon,sol_meananom
 
         character*20 starnames(nstars)
 
-        ANGDIF(X,Y)=DMOD(X-Y+9.4247779607694D0,6.2831853071796D0)-3.1415926535897932D0
+        DANGDIF(X,Y)=DMOD(X-Y+9.4247779607694D0,6.2831853071796D0)-3.1415926535897932D0
+        ANGDIFD(X,Y)=MOD(X-Y+540.,360.)-180.
 !       addmags(a,b)=log10(10.**(-a*0.4) + 10.**(-b*0.4)) * (-2.5)
         addlogs(x,y) = log10(10.**x + 10.**y)
 
         write(6,*)' subroutine get_starglow...'
         write(6,*)' lat/lon = ',rlat,rlon             
 
-        rpd = 3.14159265 / 180.
         phi = rlat * rpd
         r8lon = rlon          
 
@@ -68,7 +104,7 @@
           RAS = ra_d(is) * rpd
           DECR=dec_d(is) * rpd
 
-          HAS(is)=angdif(lst,RAS)
+          HAS(is)=dangdif(lst,RAS)
           call equ_to_altaz_r(DECR,HAS(is),PHI,ALS,AZS)
 
           alt_stars(is) = als / rpd
@@ -107,22 +143,129 @@
           endif
         enddo ! is
 
+!       http://www.ing.iac.es/Astronomy/observing/conditions/skybr/skybr.html
+!       http://aas.aanda.org/articles/aas/pdf/1998/01/ds1449.pdf
+!       http://download.springer.com/static/pdf/80/art%253A10.1186%252FBF03352136.pdf?auth66=1427760212_d8fc6d84b219c957974dd2b221bc226a&ext=.pdf
         glow_stars = 1.0 ! cosmic background level
 
         sqarcsec_per_sqdeg = 3600.**2
 
+!       Coordinate conversions
+        nalt = (maxalt-minalt) + 1
+        nazi = (maxazi-minazi) + 1
+        c_conv = 'altaz2decra'
+        call angcoord_2d(c_conv,nalt,nazi,lst_deg,arg2,alt_a,azi_a,rlat,dec_a,ra_a)
+
+        c_conv = 'decra2galactic'
+        call angcoord_2d(c_conv,nalt,nazi,arg1,arg2,dec_a,ra_a,argphi,gallat_a,gallon_a)
+
+!       http://en.wikipedia.org/wiki/Position_of_the_Sun
+        sol_meanlon  = 280.460 + 0.9856474 * (jed-2451545.0D0)
+        sol_meananom = 357.528 + 0.9856003 * (jed-2451545.0D0)
+        sollon = sol_meanlon + 1.915 * sind(sol_meananom) + .02 * sind(2. * sol_meananom)
+        sollon = mod(sollon,360.)
+        write(6,*)' sollon (computed) ',sollon
+        c_conv = 'decra2helioecliptic'
+        call angcoord_2d(c_conv,nalt,nazi,sollon,arg2,dec_a,ra_a,argphi,helioeclipticlat_a,helioeclipticlon_a)
+
         do ialt = minalt,maxalt
         do jazi = minazi,maxazi
+
+            alt = alt_a(ialt,jazi)
+            azi = azi_a(ialt,jazi)
 
             size_glow_sqdg = 0.3  ! empirical middle ground 
             size_glow_sqdg = 0.5  ! empirical middle ground 
             size_glow_sqdg = 0.0625  ! alt/az grid
             size_glow_sqdg = 0.1  ! final polar kernel size?
 
-            alt = alt_a(ialt,jazi)
-            azi = azi_a(ialt,jazi)
+            if(alt .ge. -horz_dep)then
 
-            if(alt .ge. -2.)then
+!             Milky Way (mainly from stars from 6-16 magnitude)
+              explatterm = (abs(gallat_a(ialt,jazi)/20.))**1.5
+              explonterm = (angdifd(gallon_a(ialt,jazi),0.)/140.)**2
+              s10gal = 25. + 250. * exp(-explatterm) * exp(-explonterm) 
+              if(s10gal .gt. 0.)then
+                vgal = s10_to_magsecsq(s10gal)
+                glow_gal = log10(v_to_b(vgal))
+              else
+                glow_gal = 0.
+              endif
+
+!             Zodiacal Light S10 units 140 - 90 sin(beta) ecliptic latitude
+              elat = helioeclipticlat_a(ialt,jazi)
+              elon = helioeclipticlon_a(ialt,jazi)
+              if(elon .gt. 180.)elon = 360. - elon
+              s10zod_pole = 60. ! high ecliptic latitude
+              s10geg_ecliptic =   40. * cosd((elon-180.)/2.)**150.0
+              s10zod_ecliptic =  900. * cosd((elon)/2.0001)**7.7 + 140. + s10geg_ecliptic
+!             s10zod_ecliptic = 4000. * cosd((elon)/2.)**16.0 + 150. +
+!             s10geg_ecliptic
+!             Note the gegenschein of s10 = 40 can be added near
+!             helioecliptic 180.
+              eclp = 1.9 - 0.9*abs(elon/180.)
+              eclp = 1.0  ! control ecliptic sharpness
+              ecllatterm = abs(sind(elat))**eclp
+              s10zod = 10.**(log10(s10zod_pole) * ecllatterm + &
+                             log10(s10zod_ecliptic) * (1.-ecllatterm))
+!             s10zod =            (s10zod_pole) * ecllatterm + &
+!                                 (s10zod_ecliptic) * (1.-ecllatterm) 
+!             s10zod = s10zod*10. ! debug for visibility
+
+!             F corona section (inner)
+!             http://arxiv.org/pdf/0909.1722.pdf (far corona in Fig 1.)
+!             "Interplanetary Dust" edited by B. Grun
+              sqarcsec_per_sqdeg = 3600.**2
+              size_glow_sqdg = 0.2    ! sun/moon area           
+              delta_mag = log10(size_glow_sqdg*sqarcsec_per_sqdeg)*2.5
+              distr = sqrt(elat**2 + elon**2)
+              distr2 = max(distr,0.40) ! account for pixel size
+              srad = distr2/0.25
+              spowerk =  5.7 +       srad  * 2.0
+              spowerf = 10.0 + log10(srad) * 2.0
+              spowerf2 = 8.3 +       srad  * 0.16
+              smag_eff = -26.7 + 2.5 * min(spowerk,spowerf)
+              rmag_per_sqarcsec = smag_eff + delta_mag
+              elong = min(sqrt(elon**2 + elat**2),180.)
+              coeff = 3.5 - 2.5*cosd(elong/2)**180.
+              if(elong .gt. 0.)then
+!               pcoeff = 1.0 - elon**2/(elon**2 + elat**2)
+!               pcoeff = abs(elat) / sqrt(elon**2 + elat**2)
+!               The last exponent controls ecliptic sharpness
+                pcoeff = (abs(elat) / sqrt(elon**2 + elat**2))**1.3 
+              else
+                pcoeff = 0.
+              endif
+              fexp = -2.5*(1.-pcoeff) + -2.8*pcoeff
+              f_wm2srum = coeff * srad**fexp
+              f_s10 = wm2srum_to_s10(f_wm2srum)
+!             f_nl  = wm2srum_to_nl(f_wm2srum)
+
+              s10zod2 = max(s10zod,f_s10)
+
+              sbu = s10zod2 * 1.25
+              vzod = s10_to_magsecsq(s10zod2)
+              glow_zod = log10(v_to_b(vzod))
+
+              do ic = 1,3
+                glow_stars(ic,ialt,jazi) = log10(10.**glow_stars(ic,ialt,jazi) + 10.**glow_zod + 10.**glow_gal)             
+!               glow_stars(ic,ialt,jazi) = log10(10.**glow_stars(ic,ialt,jazi))             
+              enddo ! ic
+
+              if((azi .eq. 0.0 .or. azi .eq. 180.) .AND. &
+                  (alt .eq. 0.0 .or. alt .eq. 10. .or. alt .eq. 20. .or. alt .eq. 30. .or. alt .eq. 40. .or. alt .eq. 60. .or. alt .eq. 90.) &
+                                                                                                                            )then
+                write(6,71)alt,azi,dec_a(ialt,jazi),ra_a(ialt,jazi) &
+                          ,gallat_a(ialt,jazi),gallon_a(ialt,jazi) &
+                          ,helioeclipticlat_a(ialt,jazi),helioeclipticlon_a(ialt,jazi)
+71              format(' alt-azi/dec-ra/gallat-lon/helioecllat-lon',4(2x,2f8.2))
+                write(6,*)'angdif/explonterm/s10gal',angdifd(gallon_a(ialt,jazi),0.),explonterm,s10gal
+                write(6,72)elat,elon,srad,spowerk,spowerf,spowerf2,f_wm2srum,s10zod,f_s10,s10zod2,f_wm2srum*1e8,coeff,fexp
+72              format(' elat/elon',6f9.2,f10.6,4f10.0,2f9.2)
+
+                write(6,*)'s10geg_ecl/s10zod_ecl/s10zod',s10geg_ecliptic,s10zod_ecliptic,s10zod
+                write(6,*)'s10zod/s10gal/glowstars',s10zod2,s10gal,glow_stars(2,ialt,jazi)
+              endif
 
               alt_cos = min(alt,89.)
               alt_dist = alt_scale / 2.0          * 1.3
@@ -134,24 +277,23 @@
                     rmag_per_sqarcsec = mag_stars(is) + ext_mag(is) + delta_mag                  
 
 !                   Convert to nanolamberts
-!                   glow_stars(:,ialt,jazi) = 5.0 - (mag_stars(is)+ext_mag(is))*0.4
-
                     glow_nl = log10(v_to_b(rmag_per_sqarcsec))
                     do ic = 1,3
-!                     glow_stars(:,ialt,jazi) = log10(glow_nl)             
-!                     glow_stars(:,ialt,jazi) = addlogs(glow_stars(ic,ialt,jazi),glow_nl)             
                       glow_stars(ic,ialt,jazi) = log10(10.**glow_stars(ic,ialt,jazi) + 10.**glow_nl)             
                     enddo ! ic
 
                     if(is .le. 50 .AND. abs(azi_stars(is)-azi) .le. 0.5)then
                         write(6,91)is,rmag_per_sqarcsec,delta_mag,glow_nl,glow_stars(2,ialt,jazi)
-91                      format(' rmag_per_sqarcsec/dmag/glow_nl/glow_stars = ',i4,4f10.3)             
+91                      format( &
+                         ' rmag_per_sqarcsec/dmag/glow_nl/glow_stars = ',i4,4f10.3)             
                     endif
                 endif ! within star kernel
               enddo ! is
-            endif ! alt > -2.
+            endif ! alt > -20.
         enddo ! jazi
         enddo ! ialt
+
+        rad_stars(:,:,:) = 10.**glow_stars(:,:,:)
  
         return
         end 
@@ -280,7 +422,7 @@
        
         subroutine get_glow_obj(i4time,alt_a,azi_a,minalt,maxalt,minazi,maxazi &
                                ,alt_scale,azi_scale &
-                               ,alt_obj,azi_obj,mag_obj,diam_deg,glow_obj)
+                               ,alt_obj,azi_obj,mag_obj,diam_deg,horz_dep,glow_obj)
 
         real alt_a(minalt:maxalt,minazi:maxazi)
         real azi_a(minalt:maxalt,minazi:maxazi)
@@ -310,6 +452,7 @@
         else
             ext_mag = 0.0
         endif
+        ext_mag = 0. ! test of attenuation in 'get_sky_rgb'
 
         write(6,7)dec_d,ra_d,has/rpd,alt_obj,azi_obj,mag_obj,ext_mag
 7       format('dec/ra/ha/al/az/mg/ext',1x,f9.1,4f9.3,2f9.1)
@@ -335,7 +478,7 @@
             alt = alt_a(ialt,jazi)
             azi = azi_a(ialt,jazi)
 
-            if(alt .ge. -2.)then
+            if(alt .ge. -2. .or. alt .ge. -horz_dep)then
 
               alt_cos = min(alt,89.)
               azi_dist = alt_dist / cosd(alt_cos)         
@@ -439,11 +582,13 @@
         sum = 0.
 
         do i = 1,ni
-            cosi = cosd(alt_a(i,1))
+          cosi = cosd(alt_a(i,1))
+          if(alt_a(i,1) .ge. 0.)then ! above horizontal
             do j = 1,nj
                 cnt = cnt + (1.0      * cosi)
                 sum = sum + (rad(i,j) * cosi)     
             enddo ! j
+          endif
         enddo ! i
 
         if(cnt .gt. 0.)then
