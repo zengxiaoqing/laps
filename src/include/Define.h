b4bebb66f4571e9f964b4f95087acad246fdbeb8
albers <steve.albers@noaa.gov>
2015-04-13 21:21:43 +0000

    from AnnaBelle's severe weather probability algorithm

---
+++ src/include/Define.h
@@ -0,0 +1,58 @@
+
+#define VALEUR_BIDON    -99999
+
+
+#define 	SUCCESS		0
+#define	NO_ERROR		0
+#define 	ERROR			1
+#define	FALSE			0
+#define	TRUE			1
+
+//-------------------------------
+// Definir les variables 3D
+//-------------------------------
+#define GZ          0
+#define TT          1
+#define HU          2
+#define UV          3  
+#define WW          4
+#define WD          5
+#define QR          6
+#define ZET         7
+#define QJT1        8
+#define QIT1        9
+#define QHT1        10
+#define SLW         11
+#define DMH         12
+
+//-------------------------------
+// Definir les variables 2D
+//-------------------------------
+#define SWEAT        0
+#define LWC          1
+#define LWC_700      2
+
+//-----------------------------------------
+// Defenir les variable de temps violents 
+//-----------------------------------------
+#define Hauteur_MaxR_aboveT0                        0
+#define MaxR_aboveT0                                1
+#define Couplet_Tourbillon                          2
+#define Cisaillement_3km                            3  
+#define Cisaillement_6km                            4
+#define Couplet_Positif                             5
+#define Couplet_Negatif                             6
+#define zoneSurplomb                                7
+#define Severe_Updraft                              8
+#define Vertical_Moisture_Flux                      9
+#define Mesocyclone                                10
+#define Meso_base                                  11
+#define VEF                                        12
+#define Tornade                                    13
+#define Grosseur_Grele_Max                         14
+#define Grele_sfc                                  15
+#define Supercooled_Liquid_Water                   16
+#define Vertical_Ice_Flux                          17
+#define Vertical_Moisture_Flux_NEW                 18
+
+#define nbr_variableTV                             19
