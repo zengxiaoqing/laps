32caddbb4fde795b864cae207b7b063d3d8bb43d
albers <steve.albers@noaa.gov>
2015-04-13 21:21:58 +0000

    from AnnaBelle's severe weather probability algorithm

---
+++ src/include/TV_Algorithms.h
@@ -0,0 +1,32 @@
+int TrouverMaxR_Hauteur( float **, float **, int , int , int , float , float **, int **, int **, float **, float **);
+
+int CalculerCisaillement_3kmET6km(float **, float **, float **, int , int , int , float **, float **);
+
+int CalculerVerticalIceFlux(float **,float **, float **, float **, float **, int , int , int , float **);
+
+int CalculerVerticalMoistureFlux(float **, float **, int , int , int , float **, float **);
+
+int TrouverNiveauCongelation(float **, int , int , int , int **);
+
+int TrouverMaxRAboveT0( float **, float **, int *, int , int , int , float , float **, int **, float **);
+
+int DetecterCoupletTourbillion(float **, float *, float **, int , int , int , float [2][2], float **, float **, float **);
+
+int DetecterTornade(float **, float **, int , int , int , float, float **);
+
+int DetecterUpdraft(float **, float **, int , int , int , float [2], float **);
+
+int CalculerUU_VV( float **, float **, int , int , int , float **, float **);
+
+int ConvergenceHumidite( float **, float **, float **, int , int , int , float **, float **);
+
+int repererSurplomb(float **,float **, float *, int , int , int , float, float **, float **);
+
+int DetecterMesocyclone(float **, float **, float *, float *, int , int , int, float, float **, float **);
+
+int CalculerQteEauSurfondue( float **, int *, int , int , int , float **);
+
+int BrasseurSevereGust( float *, float *, int , int , float **, float **);
+
+int TrouverGrele( float **, float *, int , int , int , float **, float **);
+
