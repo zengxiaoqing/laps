8953bf24a16155b90ca62169f864682985d2066b
Yuanfu Xie <xiey@vlaps-ms1.fsl.noaa.gov>
2015-05-27 17:02:04 -0600

    Add Laplacian check for too small values causing noise in gradient by Yuanfu

--- src/stmas/smcostf_grad.f90
+++ src/stmas/smcostf_grad.f90
@@ -20,11 +20,15 @@ SUBROUTINE SMOTHCOST
 !*************************************************
 ! SMOOTH PENALTY TERM OF COST FUNCTION
 ! HISTORY: AUGUST 2007, CODED by WEI LI.
+!
+!          MAY 2015 MODIFIED by YUANFU XIE
+!          add a penalty parameter check
+!          and laplacian check: skip if small enough
 !*************************************************
   IMPLICIT NONE
 ! --------------------
   INTEGER  :: I,J,K,T,S
-  REAL     :: Z1,Z2,Z3,AZ,BZ,CZ
+  REAL     :: Z1,Z2,Z3,AZ,BZ,CZ,sm
   double precision :: smx,smy,smz,smt
 ! --------------------
 ! PENALTY TERM
@@ -36,40 +40,51 @@ SUBROUTINE SMOTHCOST
   smx = 0.0d0
   IF(NUMGRID(1).GE.3)THEN
     DO S=1,NUMSTAT
+      IF (PENAL_X(S) .GT. 0.0) THEN
       DO T=1,NUMGRID(4)
       DO K=1,NUMGRID(3)
       DO J=1,NUMGRID(2)
       DO I=2,NUMGRID(1)-1
         ! COSTFUN=COSTFUN+PENAL_X(S)* &
-        smx=smx+PENAL_X(S)* &
-        ((GRDANALS(I-1,J,K,T,S)-2*GRDANALS(I,J,K,T,S)+GRDANALS(I+1,J,K,T,S))/(GRDSPAC(1)*GRDSPAC(1)))**2
+        sm = &
+          ((GRDANALS(I-1,J,K,T,S)-2*GRDANALS(I,J,K,T,S)+GRDANALS(I+1,J,K,T,S))/ &
+          (GRDSPAC(1)*GRDSPAC(1)))**2
+        ! YUANFU added a check to skip to small Laplacian: May 2015
+        IF (sm .GT. PENAL_X(S)*0.1) smx=smx+PENAL_X(S)*sm
       ENDDO
       ENDDO
       ENDDO
       ENDDO
+      ENDIF
     ENDDO
   ENDIF
 
   smy = 0.0d0
   IF(NUMGRID(2).GE.3)THEN
     DO S=1,NUMSTAT
+      IF (PENAL_Y(S) .GT. 0.0) THEN
       DO T=1,NUMGRID(4)
       DO K=1,NUMGRID(3)
       DO J=2,NUMGRID(2)-1
       DO I=1,NUMGRID(1)
         ! COSTFUN=COSTFUN+PENAL_Y(S)* &
-        smy=smy+PENAL_Y(S)* &
-        ((GRDANALS(I,J-1,K,T,S)-2*GRDANALS(I,J,K,T,S)+GRDANALS(I,J+1,K,T,S))/(GRDSPAC(2)*GRDSPAC(2)))**2
+        sm = &
+          ((GRDANALS(I,J-1,K,T,S)-2*GRDANALS(I,J,K,T,S)+GRDANALS(I,J+1,K,T,S))/ &
+          (GRDSPAC(2)*GRDSPAC(2)))**2
+        ! YUANFU added a check to skip to small Laplacian: May 2015
+        IF (sm .GT. PENAL_Y(S)*0.1) smy=smy+PENAL_Y(S)*sm
       ENDDO
       ENDDO
       ENDDO
       ENDDO
+      ENDIF
     ENDDO
   ENDIF
 
   smz = 0.0d0
   IF(NUMGRID(3).GE.3)THEN
     DO S=1,NUMSTAT
+      IF (PENAL_Z(S) .GT. 0.0) THEN
       DO T=1,NUMGRID(4)
       DO K=2,NUMGRID(3)-1
       DO J=1,NUMGRID(2)
@@ -90,29 +105,37 @@ SUBROUTINE SMOTHCOST
         BZ=BZ*(Z2-Z1)*(Z3-Z2)
         CZ=CZ*(Z2-Z1)*(Z3-Z2)
         ! COSTFUN=COSTFUN+PENAL_Z(S)* &
-        smz=smz+PENAL_Z(S)* &
-        (AZ*GRDANALS(I,J,K-1,T,S)+BZ*GRDANALS(I,J,K,T,S)+CZ*GRDANALS(I,J,K+1,T,S))**2
+        sm = &
+          (AZ*GRDANALS(I,J,K-1,T,S)+BZ*GRDANALS(I,J,K,T,S)+CZ*GRDANALS(I,J,K+1,T,S))**2
+        ! YUANFU added a check to skip to small Laplacian: May 2015
+        IF (sm .GT. PENAL_Z(S)*0.1) smz=smz+PENAL_Z(S)*sm
       ENDDO
       ENDDO
       ENDDO
       ENDDO
+      ENDIF
     ENDDO
   ENDIF
 
   smt = 0.0d0
   IF(NUMGRID(4).GE.3)THEN
     DO S=1,NUMSTAT
+      IF (PENAL_T(S) .GT. 0.0) THEN
       DO T=2,NUMGRID(4)-1
       DO K=1,NUMGRID(3)
       DO J=1,NUMGRID(2)
       DO I=1,NUMGRID(1)
         ! COSTFUN=COSTFUN+PENAL_T(S)* &
-        smt=smt+PENAL_T(S)* &
-        ((GRDANALS(I,J,K,T-1,S)-2*GRDANALS(I,J,K,T,S)+GRDANALS(I,J,K,T+1,S))/(GRDSPAC(4)*GRDSPAC(4)))**2
+        sm = &
+          ((GRDANALS(I,J,K,T-1,S)-2*GRDANALS(I,J,K,T,S)+GRDANALS(I,J,K,T+1,S))/ &
+          (GRDSPAC(4)*GRDSPAC(4)))**2
+        ! YUANFU added a check to skip to small Laplacian: May 2015
+        IF (sm .GT. PENAL_T(S)*0.1) smt=smt+PENAL_T(S)*sm
       ENDDO
       ENDDO
       ENDDO
       ENDDO
+      ENDIF
     ENDDO
   ENDIF
 
@@ -135,38 +158,51 @@ SUBROUTINE SMOTHGRAD
 ! PENALTY TERM
   IF(NUMGRID(1).GE.3)THEN
     DO S=1,NUMSTAT
+      IF (PENAL_X(S) .GT. 0.0) THEN
       DO T=1,NUMGRID(4)
       DO K=1,NUMGRID(3)
       DO J=1,NUMGRID(2)
       DO I=2,NUMGRID(1)-1
-        SM=(GRDANALS(I-1,J,K,T,S)-2.0*GRDANALS(I,J,K,T,S)+GRDANALS(I+1,J,K,T,S))/(GRDSPAC(1)*GRDSPAC(1))
-            GRADINT(I-1,J,K,T,S)=GRADINT(I-1,J,K,T,S)+2.0*PENAL_X(S)/(GRDSPAC(1)*GRDSPAC(1))*SM
-            GRADINT(I  ,J,K,T,S)=GRADINT(I  ,J,K,T,S)+2.0*PENAL_X(S)/(GRDSPAC(1)*GRDSPAC(1))*(-2.0)*SM
-            GRADINT(I+1,J,K,T,S)=GRADINT(I+1,J,K,T,S)+2.0*PENAL_X(S)/(GRDSPAC(1)*GRDSPAC(1))*SM
+        SM=(GRDANALS(I-1,J,K,T,S)-2.0*GRDANALS(I,J,K,T,S)+GRDANALS(I+1,J,K,T,S))/ &
+          (GRDSPAC(1)*GRDSPAC(1))
+        ! YUANFU added a check to skip to small Laplacian: May 2015
+        IF (sm .GT. PENAL_X(S)*0.1) THEN
+          GRADINT(I-1,J,K,T,S)=GRADINT(I-1,J,K,T,S)+2.0*PENAL_X(S)/(GRDSPAC(1)*GRDSPAC(1))*SM
+          GRADINT(I  ,J,K,T,S)=GRADINT(I  ,J,K,T,S)+2.0*PENAL_X(S)/(GRDSPAC(1)*GRDSPAC(1))*(-2.0)*SM
+          GRADINT(I+1,J,K,T,S)=GRADINT(I+1,J,K,T,S)+2.0*PENAL_X(S)/(GRDSPAC(1)*GRDSPAC(1))*SM
+        ENDIF
       ENDDO
       ENDDO
       ENDDO
       ENDDO
+      ENDIF
     ENDDO
   ENDIF
   IF(NUMGRID(2).GE.3)THEN
     DO S=1,NUMSTAT
+      IF (PENAL_Y(S) .GT. 0.0) THEN
       DO T=1,NUMGRID(4)
       DO K=1,NUMGRID(3)
       DO I=1,NUMGRID(1)
       DO J=2,NUMGRID(2)-1
-        SM=(GRDANALS(I,J-1,K,T,S)-2.0*GRDANALS(I,J,K,T,S)+GRDANALS(I,J+1,K,T,S))/(GRDSPAC(2)*GRDSPAC(2))
-            GRADINT(I,J-1,K,T,S)=GRADINT(I,J-1,K,T,S)+2.0*PENAL_Y(S)/(GRDSPAC(2)*GRDSPAC(2))*SM
-            GRADINT(I,J  ,K,T,S)=GRADINT(I,J  ,K,T,S)+2.0*PENAL_Y(S)/(GRDSPAC(2)*GRDSPAC(2))*(-2.0)*SM
-            GRADINT(I,J+1,K,T,S)=GRADINT(I,J+1,K,T,S)+2.0*PENAL_Y(S)/(GRDSPAC(2)*GRDSPAC(2))*SM
+        SM=(GRDANALS(I,J-1,K,T,S)-2.0*GRDANALS(I,J,K,T,S)+GRDANALS(I,J+1,K,T,S))/ &
+          (GRDSPAC(2)*GRDSPAC(2))
+        ! YUANFU added a check to skip to small Laplacian: May 2015
+        IF (sm .GT. PENAL_Y(S)*0.1) THEN
+          GRADINT(I,J-1,K,T,S)=GRADINT(I,J-1,K,T,S)+2.0*PENAL_Y(S)/(GRDSPAC(2)*GRDSPAC(2))*SM
+          GRADINT(I,J  ,K,T,S)=GRADINT(I,J  ,K,T,S)+2.0*PENAL_Y(S)/(GRDSPAC(2)*GRDSPAC(2))*(-2.0)*SM
+          GRADINT(I,J+1,K,T,S)=GRADINT(I,J+1,K,T,S)+2.0*PENAL_Y(S)/(GRDSPAC(2)*GRDSPAC(2))*SM
+        ENDIF
       ENDDO
       ENDDO
       ENDDO
       ENDDO
+      ENDIF
     ENDDO
   ENDIF
   IF(NUMGRID(3).GE.3)THEN
     DO S=1,NUMSTAT
+      IF (PENAL_Z(S) .GT. 0.0) THEN
       DO T=1,NUMGRID(4)
       DO J=1,NUMGRID(2)
       DO I=1,NUMGRID(1)
@@ -185,29 +221,39 @@ SUBROUTINE SMOTHGRAD
         BZ=BZ*(Z2-Z1)*(Z3-Z2)
         CZ=CZ*(Z2-Z1)*(Z3-Z2)
         SM=AZ*GRDANALS(I,J,K-1,T,S)+BZ*GRDANALS(I,J,K,T,S)+CZ*GRDANALS(I,J,K+1,T,S)
-        GRADINT(I,J,K-1,T,S)=GRADINT(I,J,K-1,T,S)+2.0*PENAL_Z(S)*AZ*SM
-        GRADINT(I,J,K  ,T,S)=GRADINT(I,J,K  ,T,S)+2.0*PENAL_Z(S)*BZ*SM
-        GRADINT(I,J,K+1,T,S)=GRADINT(I,J,K+1,T,S)+2.0*PENAL_Z(S)*CZ*SM
+        ! YUANFU added a check to skip to small Laplacian: May 2015
+        IF (sm .GT. PENAL_Z(S)*0.1) THEN
+          GRADINT(I,J,K-1,T,S)=GRADINT(I,J,K-1,T,S)+2.0*PENAL_Z(S)*AZ*SM
+          GRADINT(I,J,K  ,T,S)=GRADINT(I,J,K  ,T,S)+2.0*PENAL_Z(S)*BZ*SM
+          GRADINT(I,J,K+1,T,S)=GRADINT(I,J,K+1,T,S)+2.0*PENAL_Z(S)*CZ*SM
+        ENDIF
       ENDDO
       ENDDO
       ENDDO
       ENDDO
+      ENDIF
     ENDDO
   ENDIF
   IF(NUMGRID(4).GE.3)THEN
     DO S=1,NUMSTAT
+      IF (PENAL_T(S) .GT. 0.0) THEN
       DO K=1,NUMGRID(3)
       DO J=1,NUMGRID(2)
       DO I=1,NUMGRID(1)
       DO T=2,NUMGRID(4)-1
-        SM=(GRDANALS(I,J,K,T-1,S)-2.0*GRDANALS(I,J,K,T,S)+GRDANALS(I,J,K,T+1,S))/(GRDSPAC(4)*GRDSPAC(4))
-            GRADINT(I,J,K,T-1,S)=GRADINT(I,J,K,T-1,S)+2.0*PENAL_T(S)/(GRDSPAC(4)*GRDSPAC(4))*SM
-            GRADINT(I,J,K,T  ,S)=GRADINT(I,J,K,T  ,S)+2.0*PENAL_T(S)/(GRDSPAC(4)*GRDSPAC(4))*(-2.0)*SM
-            GRADINT(I,J,K,T+1,S)=GRADINT(I,J,K,T+1,S)+2.0*PENAL_T(S)/(GRDSPAC(4)*GRDSPAC(4))*SM
+        SM=(GRDANALS(I,J,K,T-1,S)-2.0*GRDANALS(I,J,K,T,S)+GRDANALS(I,J,K,T+1,S))/ &
+          (GRDSPAC(4)*GRDSPAC(4))
+        ! YUANFU added a check to skip to small Laplacian: May 2015
+        IF (sm .GT. PENAL_T(S)*0.1) THEN
+          GRADINT(I,J,K,T-1,S)=GRADINT(I,J,K,T-1,S)+2.0*PENAL_T(S)/(GRDSPAC(4)*GRDSPAC(4))*SM
+          GRADINT(I,J,K,T  ,S)=GRADINT(I,J,K,T  ,S)+2.0*PENAL_T(S)/(GRDSPAC(4)*GRDSPAC(4))*(-2.0)*SM
+          GRADINT(I,J,K,T+1,S)=GRADINT(I,J,K,T+1,S)+2.0*PENAL_T(S)/(GRDSPAC(4)*GRDSPAC(4))*SM
+        ENDIF
       ENDDO
       ENDDO
       ENDDO
       ENDDO
+      ENDIF
     ENDDO
   ENDIF
   RETURN
