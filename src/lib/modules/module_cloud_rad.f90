f32dbe9641ddc7cec3344c32b595a8caed4923e7
albers <steve.albers@noaa.gov>
2015-05-28 19:32:58 +0000

    updated values of cloud microphysics variables

--- src/lib/modules/module_cloud_rad.f90
+++ src/lib/modules/module_cloud_rad.f90
@@ -23,8 +23,8 @@ MODULE cloud_rad
       real, parameter :: rhograupel = .50e3 ! kilograms per cubic meter
 
 !     Effective radii
-      real, parameter :: reff_clwc    = .000020 ! m
-      real, parameter :: reff_cice    = .000040 ! m
+      real, parameter :: reff_clwc    = .000007 ! m
+      real, parameter :: reff_cice    = .000013 ! m
       real, parameter :: reff_rain    = .000750 ! m
       real, parameter :: reff_snow    = .004000 ! m
       real, parameter :: reff_graupel = .010000 ! m
