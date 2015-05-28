16e194d49e5d9fcf0396cce446f5ed6e38565d8a
albers <steve.albers@noaa.gov>
2015-05-28 19:34:49 +0000

    more robust statement functions

--- src/lib/cloud/get_land_pf.f90
+++ src/lib/cloud/get_land_pf.f90
@@ -9,8 +9,8 @@
         include 'trigd.inc'
 
 !       Statement functions
-        trans(od) = exp(-od)
-        opac(od) = 1.0 - exp(-od)
+        trans(od) = exp(-min(od,80.))
+        opac(od) = 1.0 - trans(od)
         alb(bt) = bt / (1.+bt)
         rad2tau(b,r) = (1.-r)/(r*b)
         ANGDIF(X,Y)=MOD(X-Y+540.,360.)-180.
