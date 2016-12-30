bcb2dec0f8ac699692776d0c01fa5e5c127318a7
albers <steve.albers@noaa.gov>
2016-12-30 22:51:00 +0000

    added 'gcdist' calculation using haversines

--- src/lib/cloud/greatcircle.f90
+++ src/lib/cloud/greatcircle.f90
@@ -1,18 +1,17 @@
 
       subroutine great_circle(alat1,alon1,alat2,alon2,gcdist,gcbearing)
 
-
 !     http://www.dtcenter.org/met/users/docs/write_ups/gc_simple.pdf
 
 !     solve for bearing of location 2 as seen from location 1
 !     north/up is zero bearing with angles counted clockwise
 !     angles are in radians
 
+      hav(x) = sin(x/2.)**2
+      hav_inv(x) = 2. * asin(sqrt(x))
+
       pi = 3.14159265
       rpd = pi / 180.
-
-!     hav(x) = sin(x/2.)**2
-!     hav_inv(x) = ???
 
       phia = alat1 * rpd
       phib = alat2 * rpd
@@ -23,6 +22,10 @@
  
       beta = atan2(s,c)
       gcbearing = beta / rpd
+
+      hav_theta = hav(phia-phib) + cos(phia) * cos(phib) * hav(deltal)
+      theta = hav_inv(hav_theta)
+      gcdist = theta / rpd
 
       return
       end
