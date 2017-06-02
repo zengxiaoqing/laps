ed7b67d94a2a77150e2690a132b60488c71751b4
albers <steve.albers@noaa.gov>
2017-06-02 22:50:27 +0000

    added 'gnp' support for range thresholds

--- src/ingest/satellite/lvd/set_missing_sat.f
+++ src/ingest/satellite/lvd/set_missing_sat.f
@@ -116,6 +116,15 @@ c        rhigh=255.
 
         write(6,*)' Range testing thresholds set to ',rlow,rhigh
 
+      elseif(csattype.eq.'gnp')then ! Bad range thresholds
+        if(chtype .eq. 'vis')then 
+          rhigh=4096.
+        else ! ir
+          rhigh=4096.
+        endif
+
+        write(6,*)' Range testing thresholds set to ',rlow,rhigh
+
       else
         write(6,*)' Range testing thresholds set to ',rlow,rhigh
 
