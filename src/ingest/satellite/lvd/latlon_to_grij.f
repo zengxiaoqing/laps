5ff9398810fedae7659e34cd0911785f67f06c8a
albers <steve.albers@noaa.gov>
2017-06-02 22:49:00 +0000

    added faster remapping routine 'latlon_to_grij_new' for future              use

--- src/ingest/satellite/lvd/latlon_to_grij.f
+++ src/ingest/satellite/lvd/latlon_to_grij.f
@@ -553,3 +553,170 @@
 
         return
         end 
+
+
+        subroutine latlon_to_grij_new(lat_l,lon_l,nx_l,ny_l,
+     1                            lat_s,lon_s,gri,grj,nx_s,ny_s,istatus)
+
+        ANGDIF(XX,Y)=MOD(XX-Y+540.,360.)-180.
+
+!       Determine i/j satellite coordinates for each LAPS grid point
+
+        real lat_l(nx_l,ny_l)
+        real lon_l(nx_l,ny_l)
+        real lat_s(nx_s,ny_s)
+        real lon_s(nx_s,ny_s)
+
+        real sublat_l(nx_l,ny_l)
+        real sublon_l(nx_l,ny_l)
+
+        real emission_angle_d(nx_l,ny_l)
+        real phase_angle_d(nx_l,ny_l)
+        real specular_ref_angle_d(nx_l,ny_l)
+        real azimuth_d(nx_l,ny_l)
+
+        real gri(nx_l,ny_l)
+        real grj(nx_l,ny_l)
+
+        real rilaps_s(nx_s,ny_s)
+        real rjlaps_s(nx_s,ny_s)
+
+        character*1 c_loop
+
+        real bi_coeff(2,2)
+
+        xlin(a,b,p,c,d,q) = (p-a*q/c) / (b-a*d/c)
+        ylin(a,b,p,c,d,q) = (q-c*p/a) / (d-c*b/a)
+
+        itstatus=ishow_timer()
+
+        write(6,*)' Subroutine latlon_to_grij...'
+
+        istatus = 1
+
+        call get_r_missing_data(r_missing_data,istatus)
+
+!       Initialize
+        gri = r_missing_data
+        grj = r_missing_data
+
+        write(6,*)' lat_s range ',minval(lat_s),maxval(lat_s)
+        write(6,*)' lat_l range ',minval(lat_l),maxval(lat_l)
+
+        slatmax = -999.
+        slatmin = +999.
+        slonmax = -999.
+        slonmin = +999.
+
+        slatcen = lat_s(nx_s/2,ny_s/2)
+        sloncen = lon_s(nx_s/2,ny_s/2)
+
+        sublat_l = slatcen
+        sublon_l = sloncen
+
+        dist_min_thr = 0.5
+
+        epsilon = .00040 ! degrees
+
+!       Initialize other variables
+        iclo = -1
+        jclo = -1
+        dist_min_last = -999.
+
+!       Start with i/j LAPS coordinate for each satellite grid point
+        do is = 1,nx_s
+        do js = 1,ny_s
+            if(lat_s(is,js) .ne. r_missing_data .AND.
+     1         lon_s(is,js) .ne. r_missing_data       )then
+                call latlon_to_rlapsgrid(lat_s(is,js),lon_s(is,js)
+     1                                  ,lat_l,lon_l
+     1                                  ,nx_l,ny_l
+     1                                  ,rilaps_s(is,js),rjlaps_s(is,js)
+     1                                  ,istatus)
+                slatmin = min(slatmin,lat_s(is,js))
+                slatmax = max(slatmax,lat_s(is,js))
+
+                slondiff = angdif(lon_s(is,js),sloncen)
+
+                slonmin = min(slonmin,slondiff)
+                slonmax = max(slonmax,slondiff)
+            else
+                rilaps_s(is,js) = r_missing_data
+                rjlaps_s(is,js) = r_missing_data
+            endif
+        enddo ! js
+        enddo ! is
+
+        write(6,*)' Sat lon center                  : ',sloncen
+        write(6,*)' Sat lat range                   : ',slatmin,slatmax
+        write(6,*)' Sat lon range (diff from center): ',slonmin,slonmax
+        write(6,*)' Sat lon range                   : ',slonmin+sloncen
+     1                                                 ,slonmax+sloncen
+
+        itstatus=ishow_timer()
+
+!       Create buffer within sat lat/lon range
+        sat_ll_buf = 0.2
+        slatmin = slatmin + sat_ll_buf
+        slatmax = slatmax - sat_ll_buf
+        slonmin = slonmin + sat_ll_buf
+        slonmax = slonmax - sat_ll_buf
+
+!       New solution method
+        do is = 1,nx_s-1
+        do js = 1,ny_s-1
+
+!           Obtain derivatives
+            dlidsi = rilaps_s(is+1,js) - rilaps_s(is,js)
+            dlidsj = rilaps_s(is,js+1) - rilaps_s(is,js)
+            dljdsi = rjlaps_s(is+1,js) - rjlaps_s(is,js)
+            dljdsj = rjlaps_s(is,js+1) - rjlaps_s(is,js)
+
+!           Locate model grid points inside the satellite parallelogram
+            rilmn = min(rilaps_s(is,js),rilaps_s(is+1,js)
+     1                 ,rilaps_s(is,js+1),rilaps_s(is+1,js+1))
+
+            rilmx = max(rilaps_s(is,js),rilaps_s(is+1,js)
+     1                 ,rilaps_s(is,js+1),rilaps_s(is+1,js+1))
+
+            rjlmn = min(rjlaps_s(is,js),rjlaps_s(is+1,js)
+     1                 ,rjlaps_s(is,js+1),rjlaps_s(is+1,js+1))
+
+            rjlmx = max(rjlaps_s(is,js),rjlaps_s(is+1,js)
+     1                 ,rjlaps_s(is,js+1),rjlaps_s(is+1,js+1))
+
+!           Loop through model grid points inside the satellite parallelogram
+            do il = int(rilmn),int(rilmx)
+            do jl = int(rjlmn),int(rjlmx)
+
+!               Set up system of linear equations
+!               If x,y = 0 we are at the lower left of the satellite parallelogram
+!               ax + by = p
+!               cx + dy = q
+
+                a = dlidsi 
+                b = dlidsj
+                c = dljdsi
+                d = dljdsj
+
+                p = float(il)         ! model grid point of interest
+                q = float(jl)         ! model grid point of interest
+
+                if(a .ne. 0. .and. c .ne. 0.)then ! well conditioned
+                    x = xlin(a,b,p,c,d,q) ! sat fractional grid point (fis)
+                    y = ylin(a,b,p,c,d,q) ! sat fractional grid point (fjs)
+
+                    gri(il,jl) = float(is) + x
+                    grj(il,jl) = float(js) + y
+                endif
+
+            enddo ! jl
+            enddo ! il
+
+        enddo ! js
+        enddo ! is
+
+        itstatus=ishow_timer()
+        
+        return
+        end
