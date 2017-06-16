de0679e4c40a92c83e56c20f58e4cb80f7eb2560
albers <steve.albers@noaa.gov>
2017-06-16 16:07:36 +0000

    added 'smooth_box_2d' routine

--- src/lib/smooth_box.f
+++ src/lib/smooth_box.f
@@ -39,3 +39,40 @@
 
        return
        end 
+
+       subroutine smooth_box_2d(a,ni,nj,kernsize)
+
+!      2D smoother using a box filter  
+
+       real a(ni,nj) 
+
+       real a_2d(ni,nj)
+
+       idelt = kernsize / 2
+
+       do i = 1,ni   
+       do j = 1,nj   
+           il = max(i-idelt,1)
+           ih = min(i+idelt,ni)
+           jl = max(j-idelt,1)
+           jh = min(j+idelt,nj)
+
+           isum = 0
+           sum = 0.
+                
+           do ii = il,ih
+           do jj = jl,jh
+               isum = isum + 1
+               sum = sum + a(ii,jj)
+           enddo ! jj
+           enddo ! ii
+
+           a_2d(i,j) = sum / float(isum)
+
+       enddo ! j
+       enddo ! i
+
+       a(:,:) = a_2d(:,:)
+
+       return
+       end 
