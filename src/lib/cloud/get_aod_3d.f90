df643b3150862d105077a7c8ac900faa3b23314e
albers <steve.albers@noaa.gov>
2017-09-06 19:28:04 +0000

    pass in 'i_aero_synplume' and 'i_aero_1d'

--- src/lib/cloud/get_aod_3d.f90
+++ src/lib/cloud/get_aod_3d.f90
@@ -1,5 +1,6 @@
 
-       subroutine get_aod_3d(pres_3d,heights_3d,topo_2d,ni,nj,nk,aod,aod_3d)
+       subroutine get_aod_3d(pres_3d,heights_3d,topo_2d,ni,nj,nk,aod &
+                            ,i_aero_synplume,i_aero_1d,aod_3d)
 
        use mem_namelist, ONLY: redp_lvl,aero_scaleht
        use mem_allsky, ONLY: mode_aero_cld
@@ -16,22 +17,27 @@
        write(6,*)' subroutine get_aod_3d: aod/redp_lvl/aero_scaleht = ' &
                                          ,aod,redp_lvl,aero_scaleht
 
-       do i = 1,ni
-       do j = 1,nj
-         sum_aod = 0.
-         do k = 1,nk
-!          pratio = pres_3d(i,j,k) / pref
-           h_agl = heights_3d(i,j,k) - redp_lvl 
-           alpha_low = (aod/aero_scaleht) * exp(-h_agl/aero_scaleht)
-           if(heights_3d(i,j,k) .gt. h1_ha .and. & 
-              heights_3d(i,j,k) .le. h2_ha .and. aod_ha .gt. 0.)then
-               alpha_high = alpha_ha
-           else
-               alpha_high = 0.
-           endif
-           aod_3d(i,j,k) = alpha_low + alpha_high
+       write(6,11)mode_aero_cld,i_aero_1d,i_aero_synplume
+11     format(' mode_aero_cld/i_aero_1d/i_aero_synplume = ',3i5)
+
+       if(i_aero_1d .eq. 1)then
+         write(6,*)' Set aerosols from 1D parameters'
+         do i = 1,ni
+         do j = 1,nj
+           sum_aod = 0.
+           do k = 1,nk
+!            pratio = pres_3d(i,j,k) / pref
+             h_agl = heights_3d(i,j,k) - redp_lvl 
+             alpha_low = (aod/aero_scaleht) * exp(-h_agl/aero_scaleht)
+             if(heights_3d(i,j,k) .gt. h1_ha .and. & 
+                heights_3d(i,j,k) .le. h2_ha .and. aod_ha .gt. 0.)then
+                 alpha_high = alpha_ha
+             else
+                 alpha_high = 0.
+             endif
+             aod_3d(i,j,k) = alpha_low + alpha_high
          
-           if(i .eq. ni/2 .and. j .eq. nj/2 .and. k .gt. 1)then
+             if(i .eq. ni/2 .and. j .eq. nj/2 .and. k .gt. 1)then
                if(h_agl .gt. 0.)then
                    ave_aod = 0.5 * (aod_3d(i,j,k)+aod_3d(i,j,k-1))
                    sum_aod = sum_aod + ave_aod       & 
@@ -39,12 +45,13 @@
                endif
                write(6,101)k,h_agl,aod_3d(i,j,k),sum_aod
 101            format('k,agl,aod_3d,sum',i5,f9.1,e15.8,f9.5)
-           endif
-         enddo ! k
-       enddo ! j
-       enddo ! i
+             endif
+           enddo ! k
+         enddo ! j
+         enddo ! i
+       endif ! mode_aero_cld
 
-       if(mode_aero_cld .gt. 1)then ! synthetic aerosol plume
+       if(i_aero_synplume .eq. 1)then ! synthetic aerosol plume
          ext_syn = 0.001
          iplume = ni/2 + 1
          jplume = nj/2 + 1
