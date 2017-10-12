2ea3146cf4ffb92159b9a01755cf2ab9a715d6ae
albers <steve.albers@noaa.gov>
2017-10-12 20:08:21 +0000

    use bilinear interpolation for very high resolutions domains when              using 30m terrain data

--- src/lib/land_topo.f
+++ src/lib/land_topo.f
@@ -14,8 +14,11 @@
       real*8 r8lat(ni,nj), r8lon(ni,nj)
       real sfc_glow_c(3,ni,nj) ! Sfc Glow (Red, Green, Blue)
       real result(ni,nj)       ! Interpolated image channel
+      real result_tile(ni,nj)  ! Interpolated image channel
       real sfc_glow(ni,nj)     ! surface lighting intensity of clouds (nl)
       real topo(ni,nj)     ! zenithal ground lighting intensity (wm2sr)
+      real ri_img(ni,nj)
+      real rj_img(ni,nj)
       character*255 directory
       character*255 file,file_bm,file_a(maxtiles)
       character*(*) path_to_topt1s
@@ -43,6 +46,7 @@
       real, allocatable :: array_2d(:,:)                   
 
 !     ri_mdl/rj_mdl tells us the model grid point of each image pixel
+!     they are allocated on the image grid
       real, allocatable :: ri_mdl(:,:),rj_mdl(:,:)                   
       real, allocatable :: rlat_img(:,:),rlon_img(:,:) 
       u = 11
@@ -55,6 +59,9 @@
           istatus = 0
           return
       endif
+
+      call get_r_missing_data(r_missing_data,istatus)
+      result = r_missing_data ! initialize
 
       call get_directory('static',directory,len_dir)
       file_bm=trim(directory)//'topo1s_tile.dat'
@@ -152,10 +159,17 @@
 
 !       Fill arrays of image ri,rj for each LAPS grid point
         istatus_img = 1
-        do i = 1,ni     
+        do i = 1,ni ! Loop through LAPS grid points    
         do j = 1,nj     
+
+!        The dimensions of this are questionable
          rj_mdl(i,j) = (rlat_laps(i,j) - rlat_start) / pix_latlon
          ri_mdl(i,j) = (rlon_laps(i,j) - rlon_start) / pix_latlon
+
+         rj_img(i,j) = (rlat_laps(i,j) - rlat_start) / pix_latlon
+         rj_img(i,j) = 3600. - rj_img(i,j) + 1.0
+         ri_img(i,j) = (rlon_laps(i,j) - rlon_start) / pix_latlon
+
          if(rlat_laps(i,j) .gt. rlat_start .OR.
      1      rlat_laps(i,j) .lt. rlat_end   .OR.
      1      rlon_laps(i,j) .lt. rlon_start .OR. 
@@ -171,14 +185,20 @@
           write(6,*)' LAPS grid is contained within image tile'      
         endif
 
-        do i = 1,iwidth,400
+        write(6,*)' selected image topo points for tile',itile
+        write(6,*)'    i     j   j1    topo       lat      lon'
         do j = 1,iheight,400
+        do i = 1,iwidth,400
 
-          write(6,21)array_2d(i,j),i,j
-21        format(f9.2,2i6)
+          j1 = iheight+1-j  
+          rlon_img_tmp = rlon_start + float(i) * pix_latlon
+          rlat_img_tmp = rlat_start + float(j) * pix_latlon
 
-        enddo ! j
+          write(6,21)i,j,j1,array_2d(i,j),rlat_img_tmp,rlon_img_tmp
+21        format(3i6,3f9.2)
+
         enddo ! i
+        enddo ! j
 
         write(6,*)' array_2d range before interpolation (topo) = '
      1          ,minval(array_2d),maxval(array_2d)
@@ -190,12 +210,28 @@
 !       Interpolate to LAPS grid using bilinear_laps_2d
 !       array_2d(:,:) = img(1,:,:)
 
-!       if(grid_spacing_m / reslights_m .lt. 1.5)then
-        if(.false.)then
-          write(6,*)' Bilinearly Interpolate ',ic,grid_spacing_m
-     1                                           ,reslights_m
-          call bilinear_laps_2d(ri_mdl,rj_mdl,iwidth,iheight,ni,nj 
-     1                         ,array_2d,result)
+        if(grid_spacing_m / reslights_m .lt. 1.5)then
+!       if(.false.)then
+          write(6,*)' Bilinearly Interpolate Tile ',itile,grid_spacing_m
+     1                                             ,reslights_m
+          result_tile = r_missing_data
+
+!         Find interpolated image values on the model grid points that
+!         overlap the image. The output is on the model grid. 'array_2d'
+!         is the image terrain. 'ri_img' and 'rj_img' are dimensioned 
+!         on the model grid and represent ri/rj image values.          
+          call bilinear_laps_2d(ri_img,rj_img,iwidth,iheight,ni,nj 
+     1                         ,array_2d,result_tile)
+
+          write(6,*)' range of result_tile ',minval(result_tile)
+     1                                      ,maxval(result_tile)
+
+          where(result_tile(:,:) .ne. r_missing_data)
+            result(:,:) = result_tile(:,:)
+          endwhere
+
+          write(6,*)' range of result',minval(result),maxval(result)
+
         else ! pixel average interpolation
 
           write(6,*)' Calls to latlon_to_rlapsgrid'
@@ -230,6 +266,7 @@
 
           write(6,*)' Pixel Ave Interpolate ',ni,nj,grid_spacing_m
      1                                       ,reslights_m
+!         Check dimensions and purpose of each array
           call pixelave_interp_multi(ri_mdl,rj_mdl,iwidth,iheight 
      1         ,ni,nj
      1         ,itile,ntiles,pixsum,npix
@@ -332,8 +369,9 @@ cdoc   points.
      1                        / float(npix(iout,jout))
           else
             result(iout,jout) = r_missing_data
+            write(6,*)' WARNING: npix = 0 at ',iout,jout
           endif
-          if(i .eq. 30*(i/30) .and. j .eq. jtest)then
+          if(i .eq. 30*(i/30) .and. jout .eq. jtest)then
             write(6,*)'iout/jout/result',iout,jout,pixsum(iout,jout)
      1                            ,npix(iout,jout),result(iout,jout)
           endif
