7c23ef5f7e343aafbfffba1206e22c0c1cbc8551
albers <steve.albers@noaa.gov>
2018-04-16 20:46:21 +0000

    improved efficiency
                 added "Gibraltar" synthetic plume

--- src/lib/cloud/get_aod_3d.f90
+++ src/lib/cloud/get_aod_3d.f90
@@ -35,33 +35,39 @@
 
        if(i_aero_1d .eq. 1)then
          write(6,*)' Set aerosols from 1D parameters'
-         do i = 1,ni
-         do j = 1,nj
-           sum_aod = 0.
-           do k = 1,nk
+         do k = 1,nk
+           do i = 1,ni
+           do j = 1,nj
+!            sum_aod = 0.
 !            pratio = pres_3d(i,j,k) / pref
-             h_agl = heights_3d(i,j,k) - redp_lvl 
+             htarg = heights_3d(i,j,k)
+             h_agl = htarg - redp_lvl 
              alpha_low = (aod/aero_scaleht) * exp(-h_agl/aero_scaleht)
-             if(heights_3d(i,j,k) .gt. h1_ha .and. & 
-                heights_3d(i,j,k) .le. h2_ha .and. aod_ha .gt. 0.)then
+             if(htarg .gt. h1_ha .and. & 
+                htarg .le. h2_ha .and. aod_ha .gt. 0.)then
                  alpha_high = alpha_ha
              else
                  alpha_high = 0.
              endif
              aod_3d(i,j,k) = alpha_low + alpha_high
-         
-             if(i .eq. ni/2 .and. j .eq. nj/2 .and. k .gt. 1)then
-               if(h_agl .gt. 0.)then
-                   ave_aod = 0.5 * (aod_3d(i,j,k)+aod_3d(i,j,k-1))
-                   sum_aod = sum_aod + ave_aod       & 
-                           * (heights_3d(i,j,k) - heights_3d(i,j,k-1))
-               endif
-               write(6,101)k,h_agl,aod_3d(i,j,k),sum_aod
-101            format('k,agl,aod_3d,sum',i5,f9.1,e15.8,f9.5)
+           enddo ! j
+           enddo ! i
+         enddo ! k
+
+         i = ni/2
+         j = nj/2  
+         sum_aod = 0.
+         do k = 1,nk
+             h_agl = heights_3d(i,j,k) - redp_lvl 
+             if(h_agl .gt. 0.)then
+                 ave_aod = 0.5 * (aod_3d(i,j,k)+aod_3d(i,j,k-1))
+                 sum_aod = sum_aod + ave_aod       & 
+                         * (heights_3d(i,j,k) - heights_3d(i,j,k-1))
              endif
-           enddo ! k
-         enddo ! j
-         enddo ! i
+             write(6,101)k,h_agl,aod_3d(i,j,k),sum_aod
+101          format('k,agl,aod_3d,sum',i5,f9.1,e15.8,f9.5)
+         enddo ! k
+         I4_elapsed = ishow_timer()
        else
          write(6,*)' Skip aerosols from 1D parameters'
        endif ! mode_aero_cld
@@ -74,9 +80,9 @@
          write(6,*)' Adding synthetic aerosol plume of',ext_syn,iplume,jplume
          aod_3d(iplume-iwp:iplume+iwp,jplume-iwp:jplume+iwp,1:nk-8) = ext_syn
        elseif(i_aero_synplume .eq. 20)then ! synthetic aerosol gradient (CO)
-         ri_full = ni/2 + 0000. / grid_spacing_m
-         ri_none = ni/2 + 4000. / grid_spacing_m
-         write(6,*)' Adding synthetic aerosol gradient ',ni/2,ri_none,ri_full
+         ri_full = float(ni/2) + 0000. / grid_spacing_m
+         ri_none = float(ni/2) + 4000. / grid_spacing_m
+         write(6,*)' Adding synthetic aerosol I gradient ',ni/2,ri_none,ri_full
          do i = 1,ni
            aero_scale = max(scurvel(float(i),ri_none,ri_full),.0001)
            do k = 1,nk
@@ -95,9 +101,9 @@
          aod = 0.
          aod_ref = 0.
        elseif(i_aero_synplume .eq. 2)then ! synthetic aerosol gradient (Gibraltar)
-         rj_full = nj - 100000. / grid_spacing_m
-         rj_none = nj -  80000. / grid_spacing_m
-         write(6,*)' Adding synthetic aerosol gradient ',ni/2,ri_none,ri_full
+         rj_full = float(nj) - 400000. / grid_spacing_m
+         rj_none = float(nj) - 300000. / grid_spacing_m
+         write(6,*)' Adding synthetic aerosol J gradient ',ni/2,rj_none,rj_full
          do j = 1,nj
            aero_scale = max(scurvel(float(j),rj_none,rj_full),.0001)
            do k = 1,nk
