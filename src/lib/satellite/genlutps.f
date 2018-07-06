e1c5666034cb88cb34fa0260686705f5c71a8c8f
albers <steve.albers@noaa.gov>
2018-07-06 19:55:35 +0000

    improved comments

--- src/lib/satellite/genlutps.f
+++ src/lib/satellite/genlutps.f
@@ -33,6 +33,8 @@ cdis
      &nx_l,ny_l,lat,lon,ri_laps,rj_laps,istatus)
 c
 c
+c     Returns satellite pixel locations for each point on model (LAPS) domain
+c
       implicit none
 
       integer   nx_l,ny_l
@@ -64,8 +66,8 @@ c
       real    rj(nx_l+2,ny_l+2)
       real    rel_ri(nx_l+2,ny_l+2)
       real    rel_rj(nx_l+2,ny_l+2)
-      real    ri_laps(nx_l,ny_l)
-      real    rj_laps(nx_l,ny_l)
+      real    ri_laps(nx_l,ny_l)       ! Output
+      real    rj_laps(nx_l,ny_l)       ! Output
       real    u,v,u0,v0,uscale,vscale
       real    usmin,usmax,vsmin,vsmax
       real    usat,vsat
